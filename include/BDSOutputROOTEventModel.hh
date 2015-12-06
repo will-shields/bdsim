c3d939cabc81f8397709cfd02b4deb4624770655
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2015-12-06 15:25:02 +0000

    Global positions and rotations in model output
    Former-commit-id: 9d65c54fa667ecd6271756d9bfb60f62d799d4bc

--- include/BDSOutputROOTEventModel.hh
+++ include/BDSOutputROOTEventModel.hh
@@ -5,6 +5,8 @@
 #include <string>
 
 #include "TObject.h"
+#include "TVector3.h"
+#include "TRotation.h"
 
 #ifndef __MAKECINT__
 #include "BDSAcceleratorModel.hh"
@@ -14,12 +16,19 @@ class BDSOutputROOTEventModel {
 protected :
   std::vector<std::string> name;
   std::vector<float>       length;
-  
-public : 
+  std::vector<TVector3>    staPos;
+  std::vector<TVector3>    midPos;
+  std::vector<TVector3>    endPos;
+  std::vector<TRotation>   staRot;
+  std::vector<TRotation>   midRot;
+  std::vector<TRotation>   endRot;
+
+public :
   BDSOutputROOTEventModel();
   virtual ~BDSOutputROOTEventModel();
+
 #ifndef __MAKECINT__   
-  void Fill();
+  virtual void Fill();
 #endif
   ClassDef(BDSOutputROOTEventModel,1);
 };
--- src/BDSOutputROOTEventModel.cc
+++ src/BDSOutputROOTEventModel.cc
@@ -2,12 +2,41 @@
 
 ClassImp(BDSOutputROOTEventModel);
 
-BDSOutputROOTEventModel::BDSOutputROOTEventModel() {
+BDSOutputROOTEventModel::BDSOutputROOTEventModel()
+{
 }
 
-BDSOutputROOTEventModel::~BDSOutputROOTEventModel() {
+BDSOutputROOTEventModel::~BDSOutputROOTEventModel()
+{
 }
 
-void BDSOutputROOTEventModel::Fill() {
-  
+void BDSOutputROOTEventModel::Fill()
+{
+  // get accelerator model
+  BDSBeamline *beamline = BDSAcceleratorModel::Instance()->GetFlatBeamline();
+
+  // iterate over flat beamline
+  for(auto i = beamline->begin(); i != beamline->end(); ++i)
+  {
+    this->name.push_back((*i)->GetName());
+    this->length.push_back((float &&) (*i)->GetAcceleratorComponent()->GetArcLength());
+    auto p = (*i)->GetPositionStart();
+    this->staPos.push_back(TVector3(p.getX(),p.getY(),p.getZ()));
+    p = (*i)->GetPositionMiddle();
+    this->midPos.push_back(TVector3(p.getX(),p.getY(),p.getZ()));
+    p = (*i)->GetPositionEnd();
+    this->endPos.push_back(TVector3(p.getX(),p.getY(),p.getZ()));
+
+    G4RotationMatrix *gr;
+    gr = (*i)->GetRotationStart();
+    TRotation rr = TRotation();
+    rr.SetXEulerAngles(gr->getTheta(),gr->getPhi(),gr->getPsi());
+    this->staRot.push_back(rr);
+    gr = (*i)->GetRotationMiddle();
+    rr.SetXEulerAngles(gr->getTheta(),gr->getPhi(),gr->getPsi());
+    this->midRot.push_back(rr);
+    gr = (*i)->GetRotationEnd();
+    rr.SetXEulerAngles(gr->getTheta(),gr->getPhi(),gr->getPsi());
+    this->endRot.push_back(rr);
+  }
 }
