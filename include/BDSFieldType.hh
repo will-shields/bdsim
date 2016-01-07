42866f30a373246a9ffe1a9b6aed80666c9dd56f
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-01-07 17:31:14 +0000

    change enum of BType to field type to generalise for any type of field
    Former-commit-id: 2157748578deae17d3ab9fde1854d9a7df187a8c

---
+++ include/BDSFieldType.hh
@@ -0,0 +1,30 @@
+#ifndef BDSFIELDTYPE_H
+#define BDSFIELDTYPE_H
+
+#include "BDSTypeSafeEnum.hh"
+#include "globals.hh"         // geant4 globals / types
+
+/**
+ * @brief Type definition for field formats - 
+ * used for comparison in factory methods.
+ * 
+ * @author Laurie Nevay <laurie.nevay@rhul.ac.uk>
+ */
+
+struct fieldtypes_def
+{
+  enum type {zero, threed, xy, mokka, solenoid, dipole, quadrupole,
+	     sextupole, octupole, decapole, multipole, sbend};
+};
+
+typedef BDSTypeSafeEnum<fieldtypes_def,int> BDSFieldType;
+
+namespace BDS
+{
+  /// function that gives corresponding enum value for string (case-insensitive)
+  BDSFieldType DetermineFieldType(G4String fieldType);
+}
+
+#endif
+
+
