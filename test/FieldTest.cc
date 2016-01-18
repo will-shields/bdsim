493b43367be66b2efbeb5d6a8ff56e1e8b10e67c
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-01-18 00:42:27 +0000

    radial testing of field
    Former-commit-id: e5e3bef5998e50ceac72229ce3b34b58c9495aad

--- test/FieldTest.cc
+++ test/FieldTest.cc
@@ -7,6 +7,9 @@
 #include "globals.hh"
 #include "G4ThreeVector.hh"
 
+#include "CLHEP/Units/PhysicalConstants.h"
+
+#include <cmath>
 #include <fstream>
 #include <ostream>
 #include <vector>
@@ -23,7 +26,7 @@ int main(int /*argc*/, char** /*argv*/)
   (*st)["k4"] = 0.004;
   (*st)["k5"] = 0.0005;
 
-  const G4double brho = 3.456;
+  const G4double brho = 34.456;
 
   std::vector<BDSFieldType> types = {
     BDSFieldType::quadrupole,
@@ -35,25 +38,25 @@ int main(int /*argc*/, char** /*argv*/)
   std::vector<BDSField*> fields;
   for (auto t : types)
     {fields.push_back(BDSFieldFactory::Instance()->CreateFieldMagLocal(t,st,brho));}
-  
-  const G4int    nX    = 100;
-  const G4double xMin  = -5*CLHEP::cm;
-  const G4double xMax  = +5*CLHEP::cm;
-  const G4double xStep = (xMax - xMin) / (G4double) nX;
-  const G4int    nY    = 100;
-  const G4double yMin  = -5*CLHEP::cm;
-  const G4double yMax  = +5*CLHEP::cm;
-  const G4double yStep = (yMax - yMin) / (G4double) nY;
+
+
+  const G4int    nR    = 20;
+  const G4int    nPhi  = 40;
+  const G4double rMax  = 20; // mm
+  const G4double rStep = rMax / (G4double) nR;
+  const G4double pStep = CLHEP::twopi / (G4double) nPhi;
   
   for (auto field : fields)
     {
       std::ofstream file;
       file.open((field->Name()+".dat").c_str());
       file << "(x,y,z)\t\tField\n";
-      for (G4double x = xMin; x <= xMax; x+=xStep)
+      for (G4double r = 0.0; r <= rMax; r+= rStep)
 	{
-	  for (G4double y = yMin; y <= yMax; y+=yStep)
+	  for (G4double phi = 0; phi <= CLHEP::twopi; phi+=pStep)
 	    {
+	      G4double x = r*cos(phi);
+	      G4double y = r*sin(phi);
 	      G4ThreeVector position(x,y,0);
 	      file << position << "\t" << field->GetFieldValue(position) << "\n";
 	    }
