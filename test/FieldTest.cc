08bde8cbdbe1ad55834661eb3265d0be88f3df34
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-01-18 20:08:38 +0000

    Update field test.  Generate data in both cylindrical and carteasian coordinates. More realistic field strengths
    Former-commit-id: f5d513d76f607e4e7181d40291215f73b2b6ec70

--- test/FieldTest.cc
+++ test/FieldTest.cc
@@ -20,13 +20,20 @@ int main(int /*argc*/, char** /*argv*/)
   BDSMagnetStrength* st = new BDSMagnetStrength();
   (*st)["field"] = 1.3;   // T
   (*st)["angle"] = 0.014; // mrad
-  (*st)["k1"] = 1.0;
-  (*st)["k2"] = 0.2;
-  (*st)["k3"] = 0.03;
-  (*st)["k4"] = 0.004;
-  (*st)["k5"] = 0.0005;
+  (*st)["k1"] = 0.12;
+  (*st)["k2"] = 0.02;
+  (*st)["k3"] = 0.003;
+  (*st)["k4"] = 0.0004;
+  (*st)["k5"] = 0.00005;
 
-  const G4double brho = 34.456;
+  BDSMagnetStrength* st2 = new BDSMagnetStrength();
+  (*st2)["k1"] = 0.12;
+  (*st2)["k2"] = 0.02;
+  (*st2)["k3"] = 0.003;
+  (*st2)["k4"] = 0.0004;
+  (*st2)["k5"] = 0.00005;
+
+  const G4double brho = 0.3456;
 
   std::vector<BDSFieldType> types = {
     BDSFieldType::quadrupole,
@@ -37,31 +44,66 @@ int main(int /*argc*/, char** /*argv*/)
   };
   std::vector<BDSField*> fields;
   for (auto t : types)
-    {fields.push_back(BDSFieldFactory::Instance()->CreateFieldMagLocal(t,st,brho));}
+    {
+      if (t != BDSFieldType::multipole)
+	{fields.push_back(BDSFieldFactory::Instance()->CreateFieldMagLocal(t,st,brho));}
+      else
+	{fields.push_back(BDSFieldFactory::Instance()->CreateFieldMagLocal(t,st2,brho));}
+    }
 
-
+  // Angular data
   const G4int    nR    = 20;
-  const G4int    nPhi  = 40;
+  const G4int    nPhi  = 100;
   const G4double rMax  = 20; // mm
   const G4double rStep = rMax / (G4double) nR;
   const G4double pStep = CLHEP::twopi / (G4double) nPhi;
-  
+
+  // Regular carteasian grid - symmetric for x,y just now
+  const G4int    nX    = 100;
+  const G4int    nY    = nX;
+  const G4double xMin  = - rMax;
+  const G4double xMax  = std::abs(xMin);
+  const G4double xStep = (xMax - xMin) / (G4double) nX;
+   
   for (auto field : fields)
     {
-      std::ofstream file;
-      file.open((field->Name()+".dat").c_str());
-      file << "(x,y,z)\t\tField\n";
-      for (G4double r = 0.0; r <= rMax; r+= rStep)
+      G4cout << "Generating field for type \"" << field->Name() << "\"" << G4endl;
+      std::ofstream rfile;
+      rfile.open((field->Name()+"_radial.dat").c_str());
+      rfile << "> nR = "   << nR   << "\n";
+      rfile << "> nPhi = " << nPhi << "\n";
+      rfile << "> brho = " << brho << "\n";
+      rfile << "# (x,y,z)\t\tField\n";
+      G4double r, phi;
+      G4int i, j;
+      for (r=0.0, i=0; i < nR; r+= rStep, i++)
 	{
-	  for (G4double phi = 0; phi <= CLHEP::twopi; phi+=pStep)
+	  for (phi=0, j=0; j < nPhi; phi+=pStep, j++)
 	    {
 	      G4double x = r*cos(phi);
 	      G4double y = r*sin(phi);
 	      G4ThreeVector position(x,y,0);
-	      file << position << "\t" << field->GetFieldValue(position) << "\n";
+	      rfile << position << "\t" << field->GetFieldValue(position) << "\n";
 	    }
 	}
-      file.close();
+      rfile.close();
+      
+      std::ofstream cfile;
+      cfile.open((field->Name()+"_carteasian.dat").c_str());
+      cfile << "> nX = "   << nX   << "\n";
+      cfile << "> nY = "   << nX   << "\n";
+      cfile << "> brho = " << brho << "\n";
+      cfile << "# (x,y,z)\t\tField\n";
+      G4double x,y;
+      for (x=xMin, i=0; i < nX; x+=xStep, i++)
+	{
+	  for (y=xMin, j=0; j < nY ; y+=xStep, j++)
+	    {
+	      G4ThreeVector position(x,y,0);
+	      cfile << position << "\t" << field->GetFieldValue(position) << "\n";
+	    }
+	}
+      cfile.close();
     }
 
   return 0;
