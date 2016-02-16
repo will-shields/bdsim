ca84cedbf4d6a5fd7e832f964ca06c3dffcfc135
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-02-16 00:12:05 +0000

    introduce integrator type class
    Former-commit-id: 711822ad8eef5abce2d2905f40e9502720a35ed9

---
+++ include/BDSIntegratorType.hh
@@ -0,0 +1,30 @@
+#ifndef BDSINTEGRATORTYPE_H
+#define BDSINTEGRATORTYPE_H
+
+#include "BDSTypeSafeEnum.hh"
+
+/**
+ * @brief Type definition for integrators.
+ * 
+ * @author Laurie Nevay
+ */
+
+struct integratortypes_def
+{
+  enum type {solenoid, dipole, quadrupole, sextupole, octupole, decapole,
+	     g4cashkarprkf45, g4classicalrk4, g4constrk4, g4expliciteuler,
+	     g4impliciteuler, g4simpleheum, g4simplerunge,
+	     g4exacthelixstepper, g4helixexpliciteuler, g4helixheum,
+	     g4heliximpliciteuler, g4helixmixedstepper, g4helixsimplerunge,
+	     g4nystromrk4, g4rkg3stepper};
+};
+
+typdef BDSTypeSafeEnum<integratortypes_def,int> BDSIntegratorType;
+
+namespace BDS
+{
+  /// Function that determines enum from string (case-insensitive).
+  BDSIntegratorType DetermineIntegratorType(G4String integratorType);
+}
+
+#endif
