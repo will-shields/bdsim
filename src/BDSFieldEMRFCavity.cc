59a0a8aa06d82f3fc8307ee7916d18148a458a4c
swalker92 <zwap082@live.rhul.ac.uk>
2016-04-05 12:25:14 +0100

    Fixed pill box field so that it outputs the right field vectors
    Former-commit-id: 900ab4ce72c1ad7e9e68b6e2cb6b80541d2264f9

--- src/BDSFieldEMRFCavity.cc
+++ src/BDSFieldEMRFCavity.cc
@@ -1,7 +1,7 @@
 #include "BDSCavityInfo.hh"
 #include "BDSFieldEMRFCavity.hh"
 
-#include "CLHEP/Units/PhysicalConstants.h" 
+#include "CLHEP/Units/PhysicalConstants.h"
 #include "globals.hh"
 #include "G4ThreeVector.hh"
 
@@ -25,6 +25,9 @@ BDSFieldEMRFCavity::BDSFieldEMRFCavity(BDSCavityInfo const* info):
 std::pair<G4ThreeVector, G4ThreeVector> BDSFieldEMRFCavity::GetField(const G4ThreeVector &position,
                                                                      const G4double& t) const
 {
+
+  //Converting from Local Cartesian to Local Cylindrical
+  G4double phi = atan2(position.y(),position.x());
   G4double r = std::sqrt(pow(position.x(),2) +  pow(position.y(),2));
 
   G4double rNormalised = normalisedCavityRadius * r;
@@ -33,55 +36,33 @@ std::pair<G4ThreeVector, G4ThreeVector> BDSFieldEMRFCavity::GetField(const G4Thr
   if (rNormalised > j0FirstZero)
     {rNormalised = j0FirstZero - 1e-6;}
 
+
+  //Source for calculating the TM010 mode: Gerigk, Frank. "Cavity types." arXiv preprint arXiv:1111.4897 (2011).
+
   G4double J0r = TMath::BesselJ0(rNormalised);
   G4double J1r = TMath::BesselJ1(rNormalised);
 
-  /*
-  //Calculating the magnitudes of the Fields:  
-  //J0(2.405*z/cavityRadius) ..  Uses polynomial approximation suitable for J0 arg between 0 and 3
+  //Calculating free-space impedance and scale factor for Bphi:
+  G4double Z0 = std::sqrt(CLHEP::mu0/CLHEP::epsilon0);
+  G4double Bmax = -eFieldMax/Z0;
 
-  G4double factor = (2.405*r) / (3*cavityRadius);
-  
-  double J0r = 0.999999999
-    -2.249999879*pow(factor,2)
-    +1.265623060*pow(factor,4)
-    -0.316394552*pow(factor,6)
-    +0.044460948*pow(factor,8)
-    -0.003954479*pow(factor,10)
-    +0.000212950*pow(factor,12);
+  //Calculating field components.  Frequency in rad/s or /s?
+  G4double Ez = eFieldMax * J0r * cos(frequency*t);
+  G4double Bphi = Bmax * J1r * sin(frequency*t);
 
+  //Converting Bphi into cartesian coordinates:
 
-  //J0(2.405*z/cavityRadius) ..  Uses polynomial approximation suitable for J1 arg between 0 and 3 
-  double J1r = factor*
-    (0.500000000
-     -0.562499992*pow(factor,2)
-     +0.210937377*pow(factor,4)
-     -0.039550040*pow(factor,6)
-     +0.004447331*pow(factor,8)
-     -0.000330547*pow(factor,10)
-     +0.000015525*pow(factor,12));
-  */
-  //|E|
-  G4double eMagnitude = eFieldMax*J0r*cos(frequency*t);
-  //|H|
-  G4double hMagnitude = (eFieldMax/(std::sqrt(CLHEP::mu0/CLHEP::epsilon0))*J1r*sin(frequency*t));
-
-
-  //Angle of the position vector.
-  //Add pi/2 so that the vector is perpendicular to position.
-  G4double angle = atan2(position.y(),position.x()) + CLHEP::pi*0.5; 
-
-  //Normalization factor
-  G4double normalization = std::sqrt(tan(angle)*tan(angle)+1);
+  G4double Bx = Bphi*sin(phi);
+  G4double By = Bphi*cos(phi);
 
   //Local B and E fields:
-  G4ThreeVector LocalB = G4ThreeVector(tan(angle)*hMagnitude/normalization, //x
-				       hMagnitude/normalization,            //y
-				       0);                                  //z
+  G4ThreeVector LocalB = G4ThreeVector(Bx,                          //x
+				       By,                          //y
+				       0);                          //z
 
-  G4ThreeVector LocalE = G4ThreeVector(0,                                   //x
-				       0,                                   //y
-				       eMagnitude);                         //z
+  G4ThreeVector LocalE = G4ThreeVector(0,                           //x
+				       0,                           //y
+				       Ez);                         //z
 
   auto result = std::make_pair(LocalB, LocalE);
   return result;
