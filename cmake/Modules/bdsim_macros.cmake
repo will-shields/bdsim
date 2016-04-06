b6bde9cb0535c3143cd3eb9897d7703297bd891b
Jochem Snuverink <jochem.snuverink@rhul.ac.uk>
2016-04-06 11:32:33 +0200

    more efficient way of copying of example files to binary dir (only when needed)
    Former-commit-id: 10ec4c0b4b9ad3e0a6da7ffbe917d5733a544ccb

---
+++ cmake/Modules/bdsim_macros.cmake
@@ -0,0 +1,25 @@
+#from https://cmake.org/pipermail/cmake/2009-March/027892.html
+
+MACRO(COPY_FILE_IF_CHANGED in_file out_file target)
+  IF(${in_file} IS_NEWER_THAN ${out_file})    
+    #message(STATUS "Copying file: ${in_file} to: ${out_file}")
+    ADD_CUSTOM_COMMAND (
+      TARGET     ${target}
+      POST_BUILD
+      COMMAND    ${CMAKE_COMMAND}
+      ARGS       -E copy ${in_file} ${out_file}
+      #COMMENT    "Copying file: ${in_file} to: ${out_file}"
+      )
+  ENDIF(${in_file} IS_NEWER_THAN ${out_file})
+ENDMACRO(COPY_FILE_IF_CHANGED)
+
+# Copy all files and directories in in_dir to out_dir. 
+# Subtrees remain intact.
+MACRO(COPY_DIRECTORY_IF_CHANGED in_dir out_dir target)
+  message(STATUS "Copying directory ${in_dir}")
+  FILE(GLOB_RECURSE in_file_list ${in_dir}/*)
+  FOREACH(in_file ${in_file_list})
+    STRING(REGEX REPLACE ${in_dir} ${out_dir} out_file ${in_file})
+    COPY_FILE_IF_CHANGED(${in_file} ${out_file} ${target})
+  ENDFOREACH(in_file)
+ENDMACRO(COPY_DIRECTORY_IF_CHANGED)
