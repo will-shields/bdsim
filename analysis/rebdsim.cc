2e70e6669f89ba36f04d7b5db7548db4b6875a01
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2016-04-07 14:38:16 +0100

    Start main code
    Former-commit-id: 611512e5c475a09e913c808372d936111a51cbc6

--- analysis/rebdsim.cc
+++ analysis/rebdsim.cc
@@ -1,12 +1,21 @@
+#include <iostream>
+
 #include "TChain.h"
 
 #include "BDSOutputROOTEventOptions.hh"
 #include "BDSOutputROOTEventModel.hh"
 
 #include "Config.hh"
+#include "DataLoader.hh"
 
-
-int main(void) 
+int main(int argc, char *argv[])
 {
-  
+  // check input
+  if(argc != 2)
+  {
+    std::cout << "usage rebdsim <ConfigFileName>" << std::endl;
+  }
+
+  Config    *cf = Config::Instance(argv[1]);
+  DataLoader dl = DataLoader(*cf);
 }
