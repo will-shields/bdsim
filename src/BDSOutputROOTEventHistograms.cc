c02096676a63d6478c866dbda4dbc8c7ffd5f84e
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2016-04-13 16:14:06 +0100

    Fix uneven binning. bins is edges-1
    Former-commit-id: 77d9b82a57eed75dca3ca9e97ed40ab836c63964

--- src/BDSOutputROOTEventHistograms.cc
+++ src/BDSOutputROOTEventHistograms.cc
@@ -31,7 +31,7 @@ G4int BDSOutputROOTEventHistograms::Create1DHistogram(G4String name, G4String ti
     edgesD[i] = edges[i];
 //    G4cout << edgesD[i] << G4endl;
   }
-  this->histograms1D.push_back(new TH1D(name,title,(Int_t)edges.size(),edgesD));
+  this->histograms1D.push_back(new TH1D(name,title,(Int_t)edges.size()-1,edgesD));
   return (G4int)this->histograms1D.size();
 
   delete[] edgesD;
@@ -59,7 +59,7 @@ G4int BDSOutputROOTEventHistograms::Create2DHistogram(G4String name, G4String ti
     yedgesD[i] = xedges[i];
   }
 
-  this->histograms2D.push_back(new TH2D(name.data(),title.data(), (Int_t)xedges.size(), xedgesD, (Int_t)yedges.size(), yedgesD));
+  this->histograms2D.push_back(new TH2D(name.data(),title.data(), (Int_t)xedges.size()-1, xedgesD, (Int_t)yedges.size()-1, yedgesD));
   return (G4int)this->histograms2D.size();
 
   delete[] xedgesD;
