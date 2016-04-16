4305c581f70103d36909d66f8b5fbc6f33e3d636
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2016-04-16 14:07:42 +0100

    Start phase space plot function
    Former-commit-id: 570f5fa10eec5bf049e1d8fc42711329e36f91c3

--- analysis/Utility.C
+++ analysis/Utility.C
@@ -7,3 +7,14 @@ std::vector<TH1D*>& GetRun1DHistograms(TString fileName)
   t->GetEntry(0);
   return h->Get1DHistograms();
 }
+
+void DrawPhaseEllipse(TString fileName)
+{
+  TFile *f = new TFile(fileName.Data());
+  TTree *t = (TTree*)f->Get("Options");
+  BDSOutputROOTEventOptions *o = 0;
+  t->SetBranchAddress("Options.",&o);
+  t->GetEntry(0);
+
+
+}
