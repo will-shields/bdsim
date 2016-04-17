bf2391ee9edff3b2bee7c127b3c91c6f2a351b49
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2016-04-17 17:36:40 +0100

    Working outer emittance, "collimators" still in progress
    Former-commit-id: 64650b39cfddc9f650585760b3e8980b21ed499b

--- include/BDSBunchHalo.hh
+++ include/BDSBunchHalo.hh
@@ -33,6 +33,19 @@ private :
   G4double envelopeXp; 
   G4double envelopeYp; 
 
+  G4double envelopeEmitX;
+  G4double envelopeEmitY;
+
+  G4double envelopeCollMinX;
+  G4double envelopeCollMaxX;
+  G4double envelopeCollMinXp;
+  G4double envelopeCollMaxXp;
+
+  G4double envelopeCollMinY;
+  G4double envelopeCollMaxY;
+  G4double envelopeCollMinYp;
+  G4double envelopeCollMaxYp;
+
   CLHEP::RandFlat  *FlatGen;
 
   G4double weightParameter;
@@ -46,32 +59,53 @@ public :
 		       G4double& xp, G4double& yp, G4double& zp,
 		       G4double& t , G4double&  E, G4double& weight);  
 
-  G4double GetBetaX()     {return betaX;}
-  G4double GetBetaY()     {return betaY;}
-  G4double GetAlphaX()    {return alphaX;}
-  G4double GetAlphaY()    {return alphaY;} 
-  G4double GetEmitX()     {return emitX;}
-  G4double GetEmitY()     {return emitY;} 
-  G4double GetEnvelopeX() {return envelopeX;}
-  G4double GetEnvelopeY() {return envelopeY;}
-  G4double GetEnvelopeXp(){return envelopeXp;}
-  G4double GetEnvelopeYp(){return envelopeYp;}
+  G4double GetBetaX()             {return betaX;}
+  G4double GetBetaY()             {return betaY;}
+  G4double GetAlphaX()            {return alphaX;}
+  G4double GetAlphaY()            {return alphaY;}
+  G4double GetEmitX()             {return emitX;}
+  G4double GetEmitY()             {return emitY;}
+  G4double GetEnvelopeX()         {return envelopeX;}
+  G4double GetEnvelopeY()         {return envelopeY;}
+  G4double GetEnvelopeXp()        {return envelopeXp;}
+  G4double GetEnvelopeYp()        {return envelopeYp;}
+  G4double GetEnvelopeEmitX()     {return envelopeEmitX;}
+  G4double GetEnvelopeEmitY()     {return envelopeEmitY;}
+  G4double GetEnvelopeCollMinX()  {return envelopeCollMinX;}
+  G4double GetEnvelopeCollMaxX()  {return envelopeCollMaxX;}
+  G4double GetEnvelopeCollMinXp() {return envelopeCollMinXp;}
+  G4double GetEnvelopeCollMaxXp() {return envelopeCollMaxXp;}
+  G4double GetEnvelopeCollMinY()  {return envelopeCollMinY;}
+  G4double GetEnvelopeCollMaxY()  {return envelopeCollMaxY;}
+  G4double GetEnvelopeCollMinYp() {return envelopeCollMinYp;}
+  G4double GetEnvelopeCollMaxYp() {return envelopeCollMaxYp;}
+
   G4double GetWeightParamer(){return weightParameter;}
   std::string GetWeightFunction() {return weightFunction;}
 
 
 protected:
-  void     SetBetaX(double newBetaX) {betaX    = newBetaX;}
-  void     SetBetaY(double newBetaY) {betaY    = newBetaY;}
-  void     SetAlphaX(double newAlphaX) {alphaX = newAlphaX;}
-  void     SetAlphaY(double newAlphaY) {alphaY = newAlphaY;}
-  void     SetEmitX(double newEmitX)   {emitX  = newEmitX;}
-  void     SetEmitY(double newEmitY)   {emitY  = newEmitY;}   
-  void     SetEnvelopeX(G4double envelopeXIn)  {envelopeX = envelopeXIn;}
-  void     SetEnvelopeY(G4double envelopeYIn)  {envelopeY = envelopeYIn;}
-  void     SetEnvelopeXp(G4double envelopeXpIn){envelopeXp= envelopeXpIn;}
-  void     SetEnvelopeYp(G4double envelopeYpIn){envelopeYp= envelopeYpIn;}
-  void     SetWeightParameter(G4double haloPSWeightParameter) {weightParameter = haloPSWeightParameter;}
+  void     SetBetaX(double newBetaX)                          {betaX  = newBetaX;}
+  void     SetBetaY(double newBetaY)                          {betaY  = newBetaY;}
+  void     SetAlphaX(double newAlphaX)                        {alphaX = newAlphaX;}
+  void     SetAlphaY(double newAlphaY)                        {alphaY = newAlphaY;}
+  void     SetEmitX(double newEmitX)                          {emitX  = newEmitX;}
+  void     SetEmitY(double newEmitY)                          {emitY  = newEmitY;}
+  void     SetEnvelopeX(G4double envelopeXIn)                 {envelopeX  = envelopeXIn;}
+  void     SetEnvelopeY(G4double envelopeYIn)                 {envelopeY  = envelopeYIn;}
+  void     SetEnvelopeXp(G4double envelopeXpIn)               {envelopeXp = envelopeXpIn;}
+  void     SetEnvelopeYp(G4double envelopeYpIn)               {envelopeYp = envelopeYpIn;}
+  void     SetEnvelopeEmitX(G4double envelopeEmitXIn)         {envelopeEmitX = envelopeEmitXIn;}
+  void     SetEnvelopeEmitY(G4double envelopeEmitYIn)         {envelopeEmitY = envelopeEmitYIn;}
+  void     SetEnvelopeCollMinX(G4double envelopeCollMinXIn)   {envelopeCollMinX  = envelopeCollMinXIn;}
+  void     SetEnvelopeCollMaxX(G4double envelopeCollMaxXIn)   {envelopeCollMaxX  = envelopeCollMaxXIn;}
+  void     SetEnvelopeCollMinXp(G4double envelopeCollMinXpIn) {envelopeCollMinXp = envelopeCollMinXpIn;}
+  void     SetEnvelopeCollMaxXp(G4double envelopeCollMaxXpIn) {envelopeCollMaxXp = envelopeCollMaxXpIn;}
+  void     SetEnvelopeCollMinY(G4double envelopeCollMinYIn)   {envelopeCollMinY  = envelopeCollMinYIn;}
+  void     SetEnvelopeCollMaxY(G4double envelopeCollMaxYIn)   {envelopeCollMaxY  = envelopeCollMaxYIn;}
+  void     SetEnvelopeCollMinYp(G4double envelopeCollMinYpIn) {envelopeCollMinYp = envelopeCollMinYpIn;}
+  void     SetEnvelopeCollMaxYp(G4double envelopeCollMaxYpIn) {envelopeCollMaxYp = envelopeCollMaxYpIn;}
+  void     SetWeightParameter(G4double haloPSWeightParameter) {weightParameter  = haloPSWeightParameter;}
   void     SetWeightFunction(std::string haloPSWeightFunction) {
     if(haloPSWeightFunction == "flat" || haloPSWeightFunction == "oneoverr" || haloPSWeightFunction == "exp") {
       weightFunction = haloPSWeightFunction;
--- src/BDSBunchHalo.cc
+++ src/BDSBunchHalo.cc
@@ -1,13 +1,20 @@
 #include "BDSBunchHalo.hh"
 #include "BDSDebug.hh"
 
+// #define BDSDEBUG
+
 BDSBunchHalo::BDSBunchHalo():
   betaX(0.0), betaY(0.0),
   alphaX(0.0), alphaY(0.0),
   emitX(0.0), emitY(0.0),
   gammaX(0.0), gammaY(0.0),
   envelopeX(0.0), envelopeY(0.0),
-  envelopeXp(0.0), envelopeYp(0.0)
+  envelopeXp(0.0), envelopeYp(0.0),
+  envelopeEmitX(0.0), envelopeEmitY(0.0),
+  envelopeCollMinX(0.0), envelopeCollMaxX(0.0),
+  envelopeCollMinXp(0.0), envelopeCollMaxXp(0.0),
+  envelopeCollMinY(0.0), envelopeCollMaxY(0.0),
+  envelopeCollMinYp(0.0), envelopeCollMaxYp(0.0)
 {
   FlatGen  = new CLHEP::RandFlat(*CLHEP::HepRandom::getTheEngine());  
   weightParameter=1.0;
@@ -32,7 +39,18 @@ void  BDSBunchHalo::SetOptions(const GMAD::Options& opt)
   SetEnvelopeX(opt.envelopeX); 
   SetEnvelopeY(opt.envelopeY);
   SetEnvelopeXp(opt.envelopeXp);
-  SetEnvelopeYp(opt.envelopeYp); 
+  SetEnvelopeYp(opt.envelopeYp);
+  SetEnvelopeEmitX(opt.haloEnvelopeEmitX);
+  SetEnvelopeEmitY(opt.haloEnvelopeEmitY);
+  SetEnvelopeCollMinX(opt.haloEnvelopeCollMinX);
+  SetEnvelopeCollMaxX(opt.haloEnvelopeCollMaxX);
+  SetEnvelopeCollMinXp(opt.haloEnvelopeCollMinXp);
+  SetEnvelopeCollMaxXp(opt.haloEnvelopeCollMaxXp);
+  SetEnvelopeCollMinY(opt.haloEnvelopeCollMinY);
+  SetEnvelopeCollMaxY(opt.haloEnvelopeCollMaxY);
+  SetEnvelopeCollMinYp(opt.haloEnvelopeCollMinYp);
+  SetEnvelopeCollMaxYp(opt.haloEnvelopeCollMaxYp);
+
   SetWeightParameter(opt.haloPSWeightParameter);
   SetWeightFunction(opt.haloPSWeightFunction);
 }
@@ -52,67 +70,81 @@ void BDSBunchHalo::GetNextParticle(G4double& x0, G4double& y0, G4double& z0,
   //  z0 += (T0 - envelopeT * (1.-2.*FlatGen->shoot())) * CLHEP::c_light * CLHEP::s;
   z0 += 0;
 
-  while(true) {
+  while(true)
+  {
     // Flat 2x2d phase space
-    G4double dx  = envelopeX  * (1-2*FlatGen->shoot()) * CLHEP::m;
-    G4double dy  = envelopeY  * (1-2*FlatGen->shoot()) * CLHEP::m;
-    G4double dxp = envelopeXp * (1-2*FlatGen->shoot()) * CLHEP::rad;
-    G4double dyp = envelopeYp * (1-2*FlatGen->shoot()) * CLHEP::rad;
+    G4double dx  = envelopeX  * (1 - 2 * FlatGen->shoot());
+    G4double dy  = envelopeY  * (1 - 2 * FlatGen->shoot());
+    G4double dxp = envelopeXp * (1 - 2 * FlatGen->shoot());
+    G4double dyp = envelopeYp * (1 - 2 * FlatGen->shoot());
 
     // compute single particle emittance 
-    double emitXSp = gammaX*pow(dx,2) + 2.*alphaX*dx*dxp + betaX*pow(dxp,2);
-    double emitYSp = gammaY*pow(dy,2) + 2.*alphaY*dy*dyp + betaY*pow(dyp,2);
-    
-// #ifdef BDSDEBUG
-//     G4cout << __METHOD_NAME__ << "phase space> " << dx << " " << dy << " " << dxp << " " << dyp << G4endl;
-//     G4cout << __METHOD_NAME__ << "emittance> " << emitXSp << " " << emitX << " " << emitYSp << " " << emitY << G4endl;
-// #endif
+    double emitXSp = gammaX * pow(dx, 2) + 2. * alphaX * dx * dxp + betaX * pow(dxp, 2);
+    double emitYSp = gammaY * pow(dy, 2) + 2. * alphaY * dy * dyp + betaY * pow(dyp, 2);
+
+ #ifdef BDSDEBUG
+    G4cout << __METHOD_NAME__ << "phase space> " << dx << " " << dy << " " << dxp << " " << dyp << G4endl;
+    G4cout << __METHOD_NAME__ << "Xcollimators> "  << envelopeCollMinX << " " << envelopeCollMaxX << " " << envelopeCollMinXp << " " << envelopeCollMaxXp << G4endl;
+    G4cout << __METHOD_NAME__ << "Ycollimators> "  << envelopeCollMinY << " " << envelopeCollMaxY << " " << envelopeCollMinYp << " " << envelopeCollMaxYp << G4endl;
+    G4cout << __METHOD_NAME__ << "emittance> " << emitX << " " << emitXSp << " " << envelopeEmitX << " "
+                                               << emitY << " " << emitYSp << " " << envelopeEmitY << G4endl;
+ #endif
 
     // check if particle is within normal beam core, if so continue generation
-    if (emitXSp < emitX || emitYSp <emitY) { 
-// #ifdef BDSDEBUG
-//       G4cout << __METHOD_NAME__ << "continue> " << G4endl;
-// #endif
+    if ((emitXSp < emitX || emitYSp < emitY) || (emitXSp > envelopeEmitX || emitYSp > envelopeEmitY)) //  ||
+        //(dx  > envelopeCollMinX  || dx < envelopeCollMaxX) ||
+        //(dxp > envelopeCollMinXp || dxp < envelopeCollMaxXp) ||
+        //(dy  > envelopeCollMinY  || dx < envelopeCollMaxY) ||
+        //(dyp > envelopeCollMinYp || dxp < envelopeCollMaxYp)
+        //)
+    {
+ #ifdef BDSDEBUG
+      G4cout << __METHOD_NAME__ << "inside> " << G4endl;
+ #endif
       continue;
-    } 
-    else {
+    }
+    else
+    {
       // determine weight, initialise 1 so always passes
       double wx = 1.0;
       double wy = 1.0;
-      if(weightFunction == "flat" || weightFunction == "") { 
-	wx = 1.0;
-	wy = 1.0;
+      if (weightFunction == "flat" || weightFunction == "")
+      {
+        wx = 1.0;
+        wy = 1.0;
       }
-      else if (weightFunction == "oneoverr") { 
-	wx = pow(emitX/fabs(emitXSp),weightParameter);
-	wy = pow(emitY/fabs(emitYSp),weightParameter);	
+      else if (weightFunction == "oneoverr")
+      {
+        wx = pow(emitX / fabs(emitXSp), weightParameter);
+        wy = pow(emitY / fabs(emitYSp), weightParameter);
       }
-      else if (weightFunction == "exp") {
-	wx = exp(-(emitXSp-emitX)/(emitX*weightParameter));
-	wy = exp(-(emitYSp-emitY)/(emitY*weightParameter));
+      else if (weightFunction == "exp")
+      {
+        wx = exp(-(emitXSp - emitX) / (emitX * weightParameter));
+        wy = exp(-(emitYSp - emitY) / (emitY * weightParameter));
       }
-      
-// #ifdef BDSDEBUG
-//       G4cout << __METHOD_NAME__ << emitXSp/emitX << " " << emitYSp/emitY << " " << wx << " " << wy << G4endl;
-// #endif
+
+ #ifdef BDSDEBUG
+       G4cout << __METHOD_NAME__ << emitXSp/emitX << " " << emitYSp/emitY << " " << wx << " " << wy << G4endl;
+ #endif
       // reject
-      if(FlatGen->shoot() > wx && FlatGen->shoot() > wy) 
-	continue;
+      //if(FlatGen->shoot() > wx && FlatGen->shoot() > wy)
+      //continue;
 
       // add to reference orbit 
       x0 += dx;
       y0 += dy;
       xp += dxp;
       yp += dyp;
-      
-      zp = CalculateZp(xp,yp,Zp0);
+
+      zp = CalculateZp(xp, yp, Zp0);
       t = 0 * CLHEP::s;
       E = BDSGlobalConstants::Instance()->ParticleTotalEnergy();
 
 #ifdef BDSDEBUG
       G4cout << __METHOD_NAME__ << "selected> " << dx << " " << dy << " " << dxp << " " << dyp << G4endl;
 #endif
-     
+
       weight = 1.0;
       return;
     }
