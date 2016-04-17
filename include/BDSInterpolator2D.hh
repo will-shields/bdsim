bf823bd84ac3b78a6d5f8f0a68a5d89356fe39c5
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-04-17 22:45:31 +0100

    update interpolator 2d base class.  Define specialisations for conversion to G4ThreeVector as interface requries
    Former-commit-id: 73feda785287b7bd8a81bb2dc12d01058d3e5066

--- include/BDSInterpolator2D.hh
+++ include/BDSInterpolator2D.hh
@@ -1,6 +1,9 @@
 #ifndef BDSINTERPOLATOR2D_H
 #define BDSINTERPOLATOR2D_H
 
+#include "BDSThreeVector.hh"
+#include "BDSTwoVector.hh"
+
 #include "globals.hh" // geant4 types / globals
 #include "G4ThreeVector.hh"
 
@@ -13,7 +16,39 @@ public:
 
   G4ThreeVector GetInterpolatedValueV(G4double x, G4double y) const;
 
+protected:
   virtual T GetInterpolatedValue(G4double x, G4double y) const = 0;
 };
+
+template <>
+G4ThreeVector BDSInterpolator2D<G4double>::GetInterpolatedValueV(G4double x, G4double y) const
+{return G4ThreeVector(GetInterpolatedValue(x,y),0,0);}
+
+template <>
+G4ThreeVector BDSInterpolator2D<G4float>::GetInterpolatedValueV(G4double x, G4double y) const
+{return G4ThreeVector(GetInterpolatedValue(x,y),0,0);}
+
+template <>
+G4ThreeVector BDSInterpolator2D<G4int>::GetInterpolatedValueV(G4double x, G4double y) const
+{return G4ThreeVector(GetInterpolatedValue(x,y),0,0);}
+
+template <>
+G4ThreeVector BDSInterpolator2D<G4long>::GetInterpolatedValueV(G4double x, G4double y) const
+{return G4ThreeVector(GetInterpolatedValue(x,y),0,0);}
+
+template <>
+G4ThreeVector BDSInterpolator2D<BDSTwoVector<G4double> >::GetInterpolatedValueV(G4double x, G4double y) const
+{
+  BDSTwoVector<G4double> result = GetInterpolatedValue(x,y);
+  return G4ThreeVector(result[0],result[1],0);
+}
+
+template <>
+G4ThreeVector BDSInterpolator2D<BDSThreeVector<G4double> >::GetInterpolatedValueV(G4double x, G4double y) const
+{
+  BDSThreeVector<G4double> result = GetInterpolatedValue(x,y);
+  return G4ThreeVector(result[0], result[1], result[2]);
+}
+
 
 #endif
