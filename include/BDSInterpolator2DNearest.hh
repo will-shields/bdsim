96f007b2132216c0c2e1c30f1fff3c1b7ad2d5bb
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-04-17 20:00:14 +0100

    implement nearest neighbour interpolator
    Former-commit-id: f34c339d6cdf1eff27f32153ca5c15c53a84f255

--- include/BDSInterpolator2DNearest.hh
+++ include/BDSInterpolator2DNearest.hh
@@ -2,21 +2,43 @@
 #define BDSINTERPOLATOR2DNEAREST_H
 
 #include "BDSArray2DCoords.hh"
+#include "BDSInterpolator2D.hh"
+
+#include "globals.hh" // geant4 types / globals
+
+/**
+ * @brief Interpolated field array that gives the nearest neighbour value.
+ *
+ * @author Laurie Nevay
+ */
 
 template<typename T>
-class BDSInterpolator2DNearest
+class BDSInterpolator2DNearest: public BDSInterpolator2D<T>
 {
 public:
-  BDSInterpolator2DNearest(BDSArray2DCoords<T> arrayIn);
+  BDSInterpolator2DNearest(BDSArray2DCoords<T>* arrayIn):
+    array(*arrayIn)
+  {;}
   ~BDSInterpolator2DNearest();
 
+  /// Copy constructor
+  BDSInterpolator2DNearest(const BDSInterpolator2DNearest& other)
+  {array(other.array);}
+
+  /// Main accessor this class has to provide. This is relatively simple
+  /// so included here.
+  virtual T GetInterpolatedValue(G4double x, G4double y) const
+  {return array(array.NearestXY(x,y));}
+  
 private:
   /// Private default constructor to force use of provided one.
   BDSInterpolator2DNearest();
 
+  /// Unused assignment operator.
+  BDSInterpolator2DNearest& operator=(const BDSInterpolator2DNearest& rhs);
+
   /// The field data.
   BDSArray2DCoords<T> array;
-
 };
 
 #endif
--- src/BDSInterpolator2DNearest.cc
+++
@@ -1,7 +0,0 @@
-#include "BDSArray2DCoords.hh"
-#include "BDSInterpolator2DNearest.hh"
-
-template<typename T>
-BDSInterpolator2DNearest<T>::BDSInterpolator2DNearest(BDSArray2DCoords<T> arrayIn):
-  array(arrayIn)
-{;}
