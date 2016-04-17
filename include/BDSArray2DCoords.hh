123f12904e8c45b330922914d47ce2a88ad90ffb
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-04-17 19:59:48 +0100

    extend array with coordinates wrapper class
    Former-commit-id: 1d86ee26f715923cedab44623349abe453fb979c

--- include/BDSArray2DCoords.hh
+++ include/BDSArray2DCoords.hh
@@ -2,8 +2,9 @@
 #define BDSARRAY2DCOORDS_H
 
 #include "BDSArray2D.hh"
+#include "BDSTwoVector.hh"
 
-#include "globals.hh"
+#include "globals.hh" // geant4 types / globals
 
 template <typename T>
 class BDSArray2DCoords
@@ -21,28 +22,59 @@ public:
     yStep = (yMax - yMin ) / (G4double)nY;
   }
 
-  G4int NearestX(const G4double x) const
+  inline G4double ArrayCoordsFromX(const G4double x) const
+  {return (x - xMin) / xStep;}
+
+  inline G4double ArrayCoordsFromY(const G4double y) const
+  {return (y - yMin) / yStep;}
+  
+  inline BDSTwoVector<G4double> ArrayCoordsFromXY(const G4double x, const G4double y) const
+  {return BDSTwoVector<G4double>(ArrayCoordsFromX(x), ArrayCoordsFromY(y));}
+
+  inline G4double XFromArrayCoords(const G4double xCoord) const
+  {return xMin + xCoord*xStep;}
+
+  inline G4double YFromArrayCoords(const G4double yCoord) const
+  {return yMin + yCoord*yStep;}
+
+  inline BDSTwoVector<G4double> XYFromArrayCoords(const G4double x, const G4double y) const
+  {return BDSTwoVector<G4double>(XFromArrayCoords(x), YFromArrayCoords(y));}
+  
+  inline G4int NearestX(const G4double x) const
   {return (G4int)round((x - xMin) / xStep);}
 
-  G4int NearestY(const G4double y) const
-  {return (G4int)round((y - yMin) / yStep);}   
+  inline G4int NearestY(const G4double y) const
+  {return (G4int)round((y - yMin) / yStep);}
 
+  inline BDSTwoVector<G4int> NearestXY(const G4double x, const G4double y) const
+  {return BDSTwoVector<G4int>(NearestX(x), NearestY(y));}
+
+  /// Access / set a single element. Note the [] operator cannot be used as it
+  /// only allows one argument.
   T& operator()(const G4int x, const G4int y)
   {
     if ( (x < xMin) || (x > xMax) || (y < xMin) || (y > yMax) )
       {G4cout << "(" << x << "," << y << ") is outside array" << G4endl; exit(1);}
     return data(x,y);
   }
-  
+
+  /// Convenience operator for two vector.
+  T& operator()(const BDSTwoVector<G4int> xy)
+  {return (*this)(xy[0], xy[1]);}
+
+  /// Access a single element.  Note the [] operator cannot be used as it only
+  /// allows one arument.
   const T& operator()(const G4double x, G4double y) const
   {
     if ( (x < xMin) || (x > xMax) || (y < xMin) || (y > yMax) )
       {return defaultValue;}
     return data(x,y);
   }
+
+  const T& operator()(const BDSTwoVector<G4int> xy) const
+  {return (*this)(xy[0], xy[1]);}
   
 private:
-
   BDSArray2D<T> data;
   G4double xMin;
   G4double xMax;
