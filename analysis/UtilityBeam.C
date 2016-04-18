9b78a5349af115e430a6b0113fdfdd3d79e02805
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2016-04-18 18:06:27 +0100

    Complete halo phase space plot
    Former-commit-id: c0eb8c0b8d3f43126916a48663c0c5fa0a9a76b6

--- analysis/UtilityBeam.C
+++ analysis/UtilityBeam.C
@@ -20,19 +20,28 @@ void DrawHalo(TString fileName)
   BDSOutputROOTEventOptions *o = GetOptions(fileName);
   GMAD::OptionsBase *ob = (GMAD::OptionsBase*)o;
 
-  TChain *c = new TChain("Event");
-  Event  *e = new Event();
-
-  GetEvent(fileName,c,e);
-
-  TCanvas *can = new TCanvas("c");
-  c->Draw("Primary.xp:Primary.x");
+#if 0
   std::cout << ob->envelopeX << " " << ob->envelopeXp << std::endl;
   std::cout << ob->envelopeY << " " << ob->envelopeYp << std::endl;
   std::cout << ob->haloEnvelopeCollMinX << " " << ob->haloEnvelopeCollMaxX << std::endl;
   std::cout << ob->haloEnvelopeCollMinY << " " << ob->haloEnvelopeCollMaxY << std::endl;
   std::cout << ob->haloEnvelopeCollMinXp << " " << ob->haloEnvelopeCollMaxXp << std::endl;
   std::cout << ob->haloEnvelopeCollMinYp << " " << ob->haloEnvelopeCollMaxYp << std::endl;
+#endif
+
+  TChain *c = new TChain("Event");
+  Event  *e = new Event();
+
+  GetEvent(fileName,c,e);
+
+  TCanvas *can = new TCanvas("c");
+  can->Divide(2,2);
+  can->cd(1);
+
+  ///////////////////////////////////////////////////////////////////////
+  // X
+  ///////////////////////////////////////////////////////////////////////
+  c->Draw("Primary.xp:Primary.x","","");
 
   TLine *xPos = new TLine(ob->haloEnvelopeCollMaxX,-ob->envelopeXp,
                           ob->haloEnvelopeCollMaxX, ob->envelopeXp);
@@ -43,11 +52,48 @@ void DrawHalo(TString fileName)
   xNeg->Draw();
 
 
-  TLine *yPos = new TLine(-ob->envelopeX,ob->haloEnvelopeCollMaxXp,
-                           ob->envelopeX,ob->haloEnvelopeCollMaxXp);
+  TLine *xpPos = new TLine(-ob->envelopeX,ob->haloEnvelopeCollMaxXp,
+                            ob->envelopeX,ob->haloEnvelopeCollMaxXp);
+  xpPos->Draw();
+
+  TLine *xpNeg = new TLine(-ob->envelopeX,ob->haloEnvelopeCollMinXp,
+                           ob->envelopeX,ob->haloEnvelopeCollMinXp);
+  xpNeg->Draw();
+
+  ///////////////////////////////////////////////////////////////////////
+  // Y
+  ///////////////////////////////////////////////////////////////////////
+  can->cd(2);
+  c->Draw("Primary.yp:Primary.y","","");
+
+
+  TLine *yPos = new TLine(ob->haloEnvelopeCollMaxY,-ob->envelopeYp,
+                          ob->haloEnvelopeCollMaxY, ob->envelopeYp);
   yPos->Draw();
 
-  TLine *yNeg = new TLine(-ob->envelopeX,ob->haloEnvelopeCollMinXp,
-                           ob->envelopeX,ob->haloEnvelopeCollMinXp);
+  TLine *yNeg = new TLine(ob->haloEnvelopeCollMinY,-ob->envelopeYp,
+                          ob->haloEnvelopeCollMinY, ob->envelopeYp);
   yNeg->Draw();
+
+
+  TLine *ypPos = new TLine(-ob->envelopeY,ob->haloEnvelopeCollMaxYp,
+                            ob->envelopeY,ob->haloEnvelopeCollMaxYp);
+  ypPos->Draw();
+
+  TLine *ypNeg = new TLine(-ob->envelopeY,ob->haloEnvelopeCollMinYp,
+                            ob->envelopeY,ob->haloEnvelopeCollMinYp);
+  ypNeg->Draw();
+
+  ///////////////////////////////////////////////////////////////////////
+  // Y vs X
+  ///////////////////////////////////////////////////////////////////////
+  can->cd(3);
+  c->Draw("Primary.y:Primary.x","","");
+
+  ///////////////////////////////////////////////////////////////////////
+  // Yp vs Xp
+  ///////////////////////////////////////////////////////////////////////
+  can->cd(4);
+  c->Draw("Primary.yp:Primary.xp","","");
+
 }
