e2667ae47f16c7d301f5519aeec18925b25013c5
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2016-04-22 14:36:48 +0100

    Use Config to get root files
    Former-commit-id: c00b7e74e79f20fbb23d0badc3030102b5296302

--- analysis/edbdsim.cc
+++ analysis/edbdsim.cc
@@ -1,10 +1,20 @@
 #include "TRint.h"
 #include "TRootApplication.h"
+
+#include "Config.hh"
 #include "EventDisplay.hh"
+
+extern EventDisplay *gEventDisplay = nullptr; 
 
 int main(int argc, char *argv[])
 {
-  TRint  *a = new TRint("App", &argc, argv);
-  EventDisplay *ed = new EventDisplay("io.gdml","output_event-2.root");
+  int rintArgc;
+  char** rintArgv;
+  TRint  *a = new TRint("App", &rintArgc, rintArgv);
+
+  Config *c = Config::Instance(argv[1]);
+  EventDisplay *ed = new EventDisplay(argv[2]);
+  gEventDisplay = ed;
   a->Run(kTRUE);
+  
 }
