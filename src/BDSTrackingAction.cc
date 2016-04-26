508aa99dd4e59af515c56cbdf566da5b0dc4d596
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2016-04-26 13:49:24 +0100

    Formatting without tabs
    Former-commit-id: f94afff95ac5546819e506e0213977194378affd

--- src/BDSTrackingAction.cc
+++ src/BDSTrackingAction.cc
@@ -19,19 +19,19 @@ BDSTrackingAction::BDSTrackingAction(G4bool batchMode):
 void BDSTrackingAction::PreUserTrackingAction(const G4Track* track)
 {
   if (interactive || (track->GetParentID() == 0))
-    {
-      fpTrackingManager->SetStoreTrajectory(1);
-      fpTrackingManager->SetTrajectory(new BDSTrajectory(track));
-    }
+  {
+    fpTrackingManager->SetStoreTrajectory(1);
+    fpTrackingManager->SetTrajectory(new BDSTrajectory(track));
+  }
   else
-    {fpTrackingManager->SetStoreTrajectory(0);}
+  {fpTrackingManager->SetStoreTrajectory(0);}
   
   /*
-    if(track->GetDefinition()->ParticleName()=="neutron")
-    {
+  if(track->GetDefinition()->ParticleName()=="neutron")
+  {
     BDSNeutronTrackInfo* Info= new BDSNeutronTrackInfo();
     Info->SetIsLogged(false);
     fpTrackingManager->SetUserTrackInformation(Info);
-    }
+  }
   */
 }
