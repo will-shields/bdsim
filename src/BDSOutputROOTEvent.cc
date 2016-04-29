0f86a69381837229b9c322b381211a2ab2747fa9
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2016-04-29 18:35:53 +0100

    Put global eloss locations back in for now, until making some options for visualiser
    Former-commit-id: caff19a34749a2166cca31fade9ba697d395b07d

--- src/BDSOutputROOTEvent.cc
+++ src/BDSOutputROOTEvent.cc
@@ -25,7 +25,7 @@ BDSOutputROOTEvent::BDSOutputROOTEvent()
 #else
   primary = new BDSOutputROOTEventSampler<double>("Primary");
 #endif
-  eLoss     = new BDSOutputROOTEventLoss(false,false);
+  eLoss     = new BDSOutputROOTEventLoss(false,true);
   pFirstHit = new BDSOutputROOTEventLoss(true,false);
   pLastHit  = new BDSOutputROOTEventLoss(true,false);
   tHit      = new BDSOutputROOTEventLoss(false,true);
