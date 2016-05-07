244a5b302f8289cae2178f4fd92ff51cc18d5f8a
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2016-05-07 15:29:44 +0100

    General macros needed, as might not be included via bdsim
    Former-commit-id: 0f173083a77833d08ac6a185c8d780a81c41ebdd

---
+++ analysis/rebdsim.hh
@@ -0,0 +1,12 @@
+#include <string>
+
+inline std::string methodName(const std::string& prettyFunction)
+{
+  size_t end    = prettyFunction.rfind("(");
+  size_t begin  = prettyFunction.rfind(" ",end) + 1;
+  // if function has pointer type then begin now is *, if so advance one more
+  if (prettyFunction[begin] == '*') {begin++;}
+  size_t len = end - begin;
+  return prettyFunction.substr(begin,len) +"> ";
+}
+#define __METHOD_NAME__ methodName(__PRETTY_FUNCTION__)
