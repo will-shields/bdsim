1df36704a8f190e0c996ced564aa334eda4ee643
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2016-05-07 17:19:23 +0100

    Fill and write generate TTree::Draw histograms
    Former-commit-id: b844c308c1ef3434dd292105142408635525d708

--- analysis/EventAnalysis.cc
+++ analysis/EventAnalysis.cc
@@ -1,6 +1,9 @@
 #include "EventAnalysis.hh"
 #include "BDSOutputROOTEventHistograms.hh"
 #include "Config.hh"
+#include "TROOT.h"
+#include "TDirectory.h"
+#include "TFile.h"
 
 ClassImp(EventAnalysis)
 
@@ -26,8 +29,8 @@ EventAnalysis::EventAnalysis(Event *eventIn, TChain *chainIn)
   }
 
   histoSum = new BDSOutputROOTEventHistograms(*(event->histos));
-  std::cout << this->event->histos->Get1DHistogram(0) << std::endl;
-  std::cout << histoSum->Get1DHistogram(0) << std::endl;
+  std::cout << __METHOD_NAME__ << " " << this->event->histos->Get1DHistogram(0) << std::endl;
+  std::cout << __METHOD_NAME__ << histoSum->Get1DHistogram(0) << std::endl;
 }
 
 EventAnalysis::~EventAnalysis()
@@ -94,11 +97,124 @@ void EventAnalysis::Terminate()
   }
 }
 
-
 void EventAnalysis::SimpleHistograms()
 {
   if(Config::Instance()->Debug())
   {
-    std::cout << "EventAnalysis::SimpleHistograms>" << std::endl;
+    std::cout << __METHOD_NAME__ << std::endl;
   }
+
+  // loop over histogram specifications and fill
+  auto hd = Config::Instance()->GetHistoDefs();  // histogram definitions
+  for(auto i : hd)
+  {
+    this->FillHistogram(i["treeName"].data(), i["histName"], i["nbins"], i["binning"], i["plot"], i["select"]);
+  }
+}
+
+
+void EventAnalysis::FillHistogram(std::string treeName, std::string histoName,
+                                  std::string nbins,    std::string binning,
+                                  std::string plot,     std::string selection)
+{
+  if(Config::Instance()->Debug())
+  {
+    std::cout << __METHOD_NAME__ << std::endl;
+  }
+  double xlow=0.0, xhigh=0.0;
+  double ylow=0.0, yhigh=0.0;
+  int ndim = Config::Dimension(nbins);
+  int nxbin=0, nybin=0;
+
+  if(ndim == 1)
+  {
+    nxbin = Config::NBins(nbins,1);
+    Config::Binning(binning,1,xlow,xhigh);
+  }
+  else if(ndim == 2)
+  {
+    nxbin = Config::NBins(nbins,1);
+    nybin = Config::NBins(nbins,2);
+    Config::Binning(binning,1,xlow,xhigh);
+    Config::Binning(binning,2,ylow,yhigh);
+  }
+
+  if (!chain)
+  {std::cout << __METHOD_NAME__ << "Error no tree found by name: " << treeName << std::endl; exit(1);}
+
+  if(Config::Instance()->Debug())
+  {
+    std::cout << __METHOD_NAME__ << treeName << " " << histoName << " " << plot << std::endl;
+  }
+
+  auto pltSav = histoName;
+  auto pltCmd = plot+" >> "+pltSav;
+
+  // create histograms
+  if(ndim == 1)
+  {
+    auto h = new TH1D(pltSav.c_str(),pltSav.c_str(), nxbin, xlow, xhigh);
+    h->AddDirectory(kTRUE);
+  }
+  else if(ndim == 2)
+  {
+    auto h = new TH2D(pltSav.c_str(),pltSav.c_str(), nxbin, xlow, xhigh, nybin, ylow, yhigh);
+    h->AddDirectory(kTRUE);
+  }
+
+  auto n = chain->Draw(pltCmd.c_str(),selection.c_str(),"goff");
+
+  std::cout << n << std::endl;
+  if(ndim == 1)
+  {
+    auto h = (TH1*)gDirectory->Get(pltSav.c_str());
+    this->histogramNames.push_back(pltSav);
+    this->histograms1D[pltSav] = h;
+    std::cout << h << std::endl;
+  }
+  else if(ndim == 2)
+  {
+    auto h = (TH2*)gDirectory->Get(pltSav.c_str());
+    this->histogramNames.push_back(pltSav);
+    this->histograms2D[pltSav] = h;
+    std::cout << h << std::endl;
+  }
+
+  gDirectory->ls();
+
+  if(Config::Instance()->Debug())
+  {
+    std::cout << __METHOD_NAME__ << "`" << pltSav << "'  `" << pltCmd << "' " << gDirectory->Get(pltSav.c_str()) << std::endl;
+  }
+}
+
+void EventAnalysis::Write(std::string outputFileName)
+{
+  if(Config::Instance()->Debug())
+  {
+    std::cout << __METHOD_NAME__ << std::endl;
+  }
+
+  TFile *f = new TFile(outputFileName.c_str(),"RECREATE");
+
+  // write rebdsim histograms
+  TDirectory *rebdsimDir = f->mkdir("rebdsimHistorgrams");
+  rebdsimDir->cd();
+  for(auto h : histograms1D)
+  {
+    std::cout << h.first << " " << h.second << std::endl;
+    h.second->Write();
+  }
+  for(auto h : histograms2D)
+  {
+    std::cout << h.first << " " << h.second << std::endl;
+    h.second->Write();
+  }
+
+  // write run merged run histograms
+  TDirectory *bdsimDir = f->mkdir("bdsimHistograms");
+  bdsimDir->cd();
+
+
+  f->Close();
 }
--- analysis/EventAnalysis.hh
+++ analysis/EventAnalysis.hh
@@ -24,12 +24,17 @@ public:
   void Terminate();
 
   void SimpleHistograms();
+  void FillHistogram(std::string treeName, std::string histoName,
+                     std::string nbins,    std::string binning,
+                     std::string plot,     std::string selection);
 
+  void Write(std::string outputFileName);
 
 protected:
   Event  *event;
   TChain *chain;
   std::vector<SamplerAnalysis*> samplerAnalyses;
+  std::vector<std::string>      histogramNames;
   std::map<std::string, TH1*>   histograms1D;
   std::map<std::string, TH2*>   histograms2D;
   BDSOutputROOTEventHistograms  *histoSum;
