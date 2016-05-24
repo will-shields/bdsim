991f3e93d7b3b61a5ce9a94031df01de0c97e1ec
Jochem Snuverink <jochem.snuverink@rhul.ac.uk>
2016-05-24 15:52:16 +0100

    fix example copying with a hack
    Former-commit-id: 625ac29ff3eaba8d131a2c095410fd15c2cabc56

--- cmake/Modules/bdsim_macros.cmake
+++ cmake/Modules/bdsim_macros.cmake
@@ -28,19 +28,19 @@ ENDMACRO(COPY_FILE_IF_CHANGED)
 # Copy all files and directories in in_dir to out_dir. 
 # Subtrees remain intact.
 # Only copy files that are in git
+# hack for now, only copies files from source to build dir
 MACRO(COPY_DIRECTORY_IF_CHANGED in_dir out_dir target)
   message(STATUS "Copying directory ${in_dir}")
   execute_process(
     COMMAND git ls-files --full-name ${in_dir}
     OUTPUT_VARIABLE git_file_list)
   separate_arguments(in_file_list UNIX_COMMAND "${git_file_list}")
-  #message(STATUS "list = ${in_file_list}")
   #message(STATUS "file list: ${in_file_list}")
-  FOREACH(in_file ${in_file_list})
-    set(in_file "${in_dir}/${in_file}")
-    set(out_file "${out_dir}/${in_file}")
+  FOREACH(file ${in_file_list})
+    set(in_file "${CMAKE_SOURCE_DIR}/${file}")
+    set(out_file "${CMAKE_BINARY_DIR}/${file}")
     #message(STATUS "in_file: ${in_file}")
     #message(STATUS "out_file: ${out_file}")
     COPY_FILE_IF_CHANGED(${in_file} ${out_file} ${target})
-  ENDFOREACH(in_file)
+  ENDFOREACH(file)
 ENDMACRO(COPY_DIRECTORY_IF_CHANGED)
