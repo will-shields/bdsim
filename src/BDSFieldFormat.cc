9994db3bc71b41d97770694376b4c53b74881abf
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-05-25 15:55:47 +0100

    field map file format enum
    Former-commit-id: a73d54f405c9d75719213faa0aa07e5a7a31baca

---
+++ src/BDSFieldFormat.cc
@@ -0,0 +1,40 @@
+#include "BDSFieldFormat.hh"
+#include "BDSDebug.hh"
+#include "globals.hh"
+
+#include <map>
+#include <string>
+
+// dictionary for BDSFieldFormat for reflexivity
+template<>
+std::map<BDSFieldFormat, std::string>* BDSFieldFormat::dictionary =
+  new std::map<BDSFieldFormat, std::string> ({
+      {BDSFieldFormat::bdsim2d,   "bdsim2d"},
+      {BDSFieldFormat::bdsim3d,   "bdsim3d"}
+});	
+
+BDSFieldFormat BDS::DetermineFieldFormat(G4String bFormat)
+{
+  std::map<G4String, BDSFieldFormat> formats;
+  formats["bdsim3d"] = BDSFieldFormat::bdsim2d;
+  formats["bdsim3d"] = BDSFieldFormat::bdsim3d;
+  
+  bFormat.toLower();
+
+  auto result = formats.find(bFormat);
+  if (result == formats.end())
+    {
+      // it's not a valid key
+      G4cerr << __METHOD_NAME__ << bFormat << " is not a valid field format" << G4endl;
+
+      G4cout << "Available field formats are:" << G4endl;
+      for (auto it : formats)
+	{G4cout << "\"" << it.first << "\"" << G4endl;}
+      exit(1);
+    }
+
+#ifdef BDSDEBUG
+  G4cout << __METHOD_NAME__ << "determined field format to be " << result->second << G4endl;
+#endif
+  return result->second;
+}
