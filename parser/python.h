bcc3091a22024fb05d615245e9b44496e4e000c1
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-06-01 10:28:54 +0100

    upgrade comments to inline doxygen comments
    Former-commit-id: 706ea476c85572bbad5d5e0e4a73561068da805d

--- parser/python.h
+++ parser/python.h
@@ -13,18 +13,18 @@ namespace GMAD
     int    GmadParser_c(char *name);
 
     /* Interface to extern beamline_list */
-    int          GetNElements();            // Length of list
-    int          GetType(int);              // Type of element
-    const char*  GetName(int);              // Name of element
-    double       GetLength(int);            // Length of element
-    double       GetAngle(int);             // Angle of element
-    double*      GetKs(int);                // All magnetic ks
-    double       GetAper1(int);             // Aperture 1
-    double       GetAper2(int);             // Aperture 2
-    double       GetAper3(int);             // Aperture 3
-    double       GetAper4(int);             // Aperture 4
-    const char*  GetApertureType(int);       // Aperture Type
-    double       GetBeampipeThickness(int); // Beam Pipe Thickness
+    int          GetNElements();            ///< Length of list
+    int          GetType(int);              ///< Type of element
+    const char*  GetName(int);              ///< Name of element
+    double       GetLength(int);            ///< Length of element
+    double       GetAngle(int);             ///< Angle of element
+    double*      GetKs(int);                ///< All magnetic ks
+    double       GetAper1(int);             ///< Aperture 1
+    double       GetAper2(int);             ///< Aperture 2
+    double       GetAper3(int);             ///< Aperture 3
+    double       GetAper4(int);             ///< Aperture 4
+    const char*  GetApertureType(int);      ///< Aperture Type
+    double       GetBeampipeThickness(int); ///< Beam Pipe Thickness
 
   }
 }
