a6e74db06003cd9e7547ed06215e932a82db0086
stewartboogert <stewart.boogert@rhul.ac.uk>
2016-06-02 02:38:37 +0100

    Add output
    Former-commit-id: 7ad300a0d2b5d0b9416234df8bb4b6aaa8746878

--- depend/gcc.sh
+++ depend/gcc.sh
@@ -1,18 +1,38 @@
 source ./setup.sh
 
+DLDURL=http://nl.mirror.babylon.network/gcc/releases/gcc-4.9.3
+DLDFILE=gcc-4.9.3.tar.gz
+PKGNAME=gcc
+LOG=$BASEDIR/$PKGNAME.log
+echo $LOG
+
 ##################################################
 # GCC
 ##################################################
 cd $BASEDIR
-wget http://nl.mirror.babylon.network/gcc/releases/gcc-4.9.3/gcc-4.9.3.tar.gz
-mv gcc-4.9.3.tar.gz ./src/
-mkdir $BASEDIR/build/gcc
-tar zfx $BASEDIR/src/gcc-4.9.3.tar.gz -C ./build/gcc --strip-components=1
-cd $BASEDIR/build/gcc
-./contrib/download_prerequisites
+echo "Downloading $PKGNAME" | tee $LOG
+wget --quiet $DLDURL/$DLDFILE
+rm -rf $BASEDIR/src/$DLDFILE
+mv $DLDFILE ./src/
 
-mkdir $BASEDIR/build/gcc-build
-cd $BASEDIR/build/gcc-build/
-../gcc/configure --prefix=$INSTALLDIR --enable-languages=c,c++,fortran,go
+echo "Unpacking $PKGNAME" | tee -a $LOG
+rm -rf $BASEDIR/build/$PKGNAME
+mkdir $BASEDIR/build/$PKGNAME
+tar zfx $BASEDIR/src/$DLDFILE -C ./build/$PKGNAME --strip-components=1
+cd $BASEDIR/build/$PKGNAME
+echo "Downloading prerequisites $PKGNAME" | tee -a $LOG
+./contrib/download_prerequisites >> $LOG
+
+rm -rf $BASEDIR/build/$PKGNAME-build
+mkdir $BASEDIR/build/$PKGNAME-build
+cd $BASEDIR/build/$PKGNAME-build/
+
+echo "Configuring $PKGNAME" | tee -a $LOG
+../gcc/configure --prefix=$INSTALLDIR --enable-languages=c,c++,fortran,go 
+
+echo "Building $PKGNAME" | tee -a $LOG
+make -j$NCPU
+
+echo "Installing $PKGNAME" | tee -a $LOG
 make -j$NCPU install
 
