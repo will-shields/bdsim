a02878086bee50cf84ab612311b5b78a3473267d
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-06-03 17:31:29 +0100

    tidying while debugging
    Former-commit-id: b0af39c4c134649c0019720523e9caefb2240666

--- include/BDSTrajectoryPoint.hh
+++ include/BDSTrajectoryPoint.hh
@@ -1,6 +1,8 @@
 #ifndef BDSTRAJECTORYPOINT_H
 #define BDSTRAJECTORYPOINT_H
 
+#include "BDSAuxiliaryNavigator.hh"
+
 #include "globals.hh" // geant4 types / globals
 #include "G4Allocator.hh"
 #include "G4Step.hh"
@@ -13,7 +15,6 @@
  * @brief A Point in a trajectory with extra information.
  *
  * @author S. Boogert
- *
  */
 
 class BDSTrajectoryPoint: public G4TrajectoryPoint
@@ -21,7 +22,7 @@ class BDSTrajectoryPoint: public G4TrajectoryPoint
 public:
   BDSTrajectoryPoint();
   BDSTrajectoryPoint(const G4Step* step);
-  virtual ~BDSTrajectoryPoint(){;}
+  virtual ~BDSTrajectoryPoint();
 
   inline void *operator new(size_t);
   inline void operator delete(void *aTrajectoryPoint);
@@ -29,35 +30,35 @@ public:
   {return (this==&right);};
 
   /// @{ Accessor
-  inline G4int         GetPreProcessType()       const {return preProcessType;}
-  inline G4int         GetPreProcessSubType()    const {return preProcessSubType;}
-  inline G4int         GetPostProcessType()      const {return postProcessType;}
-  inline G4int         GetPostProcessSubType()   const {return postProcessSubType;}
-  inline G4double      GetPreWeight()            const {return preWeight;}
-  inline G4double      GetPostWeight()           const {return postWeight;}
-  inline G4double      GetPreEnergy()            const {return preEnergy;}
-  inline G4double      GetPostEnergy()           const {return postEnergy;}
-  inline G4double      GetEnergy()               const {return energy;}
-  inline G4double      GetPreS()                 const {return preS;}
-  inline G4double      GetPostS()                const {return postS;}
-
+  inline G4int    GetPreProcessType()     const {return preProcessType;}
+  inline G4int    GetPreProcessSubType()  const {return preProcessSubType;}
+  inline G4int    GetPostProcessType()    const {return postProcessType;}
+  inline G4int    GetPostProcessSubType() const {return postProcessSubType;}
+  inline G4double GetPreWeight()          const {return preWeight;}
+  inline G4double GetPostWeight()         const {return postWeight;}
+  inline G4double GetPreEnergy()          const {return preEnergy;}
+  inline G4double GetPostEnergy()         const {return postEnergy;}
+  inline G4double GetEnergy()             const {return energy;}
+  inline G4double GetPreS()               const {return preS;}
+  inline G4double GetPostS()              const {return postS;}
   /// @}
   
   /// Output stream
   friend std::ostream& operator<< (std::ostream &out, BDSTrajectoryPoint const &p);
   
 private:
-  G4int              preProcessType;
-  G4int              preProcessSubType;
-  G4int             postProcessType;
-  G4int             postProcessSubType;
+  G4int preProcessType;
+  G4int preProcessSubType;
+  G4int postProcessType;
+  G4int postProcessSubType;
 
-  G4double           preWeight;
-  G4double          postWeight;
+  G4double preWeight;
+  G4double postWeight;
 
-  G4double           preEnergy;
-  G4double          postEnergy;
-  G4double          energy;
+  G4double preEnergy;
+  G4double postEnergy;
+  G4double energy;
+
   G4double  preS;
   G4double postS;
 
@@ -83,7 +84,5 @@ inline void BDSTrajectoryPoint::operator delete(void *aTrajectoryPoint)
 {
   bdsTrajectoryPointAllocator.FreeSingle((BDSTrajectoryPoint *) aTrajectoryPoint);
 }
-
-
 
 #endif
