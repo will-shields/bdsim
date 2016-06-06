5e79bbc53f0989badf148932e5d77122759863ef
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-06-06 12:02:17 +0100

    doxygen for histogram merge
    Former-commit-id: 677cf89ef775a27bf1b5ad7ea1af07801dc4ebc2

--- analysis/HistogramMerge.hh
+++ analysis/HistogramMerge.hh
@@ -9,15 +9,33 @@
 
 #include "BDSOutputROOTEventHistograms.hh"
 
+/**
+ * @brief Merge a set of histgrams.
+ * 
+ * @author Stewart Boogert
+ */
+
 class HistogramMerge
 {
 public:
+  /// Default constructor with no action.
   HistogramMerge();
+
+  /// Construct w.r.t. a set of event histograms. Each histogram
+  /// is cloned in preparation for accumulation.
   HistogramMerge(BDSOutputROOTEventHistograms *h);
+  
   virtual ~HistogramMerge();
 
+  /// Loop over histograms used in non-default constructor and accumulate
+  /// those from the input set of histograms.
   void Add(BDSOutputROOTEventHistograms *h);
+
+  /// Loop over (1D only just now) histograms and calculate mean and std of
+  /// each bin.
   void Terminate();
+
+  /// Write the resultant histograms to an output file - ignores the argument for now.
   void Write(TFile *outputFile);
 
 private:
