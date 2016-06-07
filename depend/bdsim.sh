82370d283dab7ef661a1fb170b517ff36288cd24
stewartboogert <stewart.boogert@rhul.ac.uk>
2016-06-07 18:01:19 +0100

    Fix bdsim build
    Former-commit-id: 3aeb5260f411c81cd7642790f3b83045d23e3d8a

--- depend/bdsim.sh
+++ depend/bdsim.sh
@@ -14,8 +14,14 @@ rm -rf $BASEDIR/build/$PKGNAME-build
 mkdir $BASEDIR/build/$PKGNAME-build
 cd $BASEDIR/build/$PKGNAME-build
 
+echo `pwd` >> $LOG
+
 echo "Cmake $PKGNAME" | tee -a $LOG
 cmake -DCMAKE_CXX_LINK_FLAGS="-L$INSTALLDIR/lib64 -L$INSTALLDIR/lib -Wl,-rpath,$INSTALLDIR/lib64,-rpath,$INSTALLDIR/lib" -DCMAKE_INSTALL_PREFIX=$INSTALLDIR/ ../$PKGNAME/ >> $LOG 2>&1
 
+
 echo "Building $PKGNAME" | tee -a $LOG
-make -j$NCPU install >> $LOG 2>&1
+make -j$NCPU >> $LOG
+
+echo "Install $PKGNAME" | tee -a $LOG
+make -j$NCPU install >> $LOG 
