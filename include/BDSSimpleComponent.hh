d99032457b2a65c3ce45ab3152be2b923c3e5fc7
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-06-21 09:39:32 +0100

    Doxygen for BDSSimpleComponent - make a BDSGeometryComponent a BDSAcceleratorComponent
    Former-commit-id: db8806b127c7a4a8b365ee3dc013fd834f4d7b09

--- include/BDSSimpleComponent.hh
+++ include/BDSSimpleComponent.hh
@@ -7,17 +7,33 @@
 
 class BDSGeometryComponent;
 
+/**
+ * @brief A BDSAcceleratorComponent wrapper for BDSGeometryComponent.
+ * 
+ * This allows a BDSGeometryComponent to be placed in a beam line by
+ * being a necessary BDSAcceleratorComponent, which gives the minimal 
+ * information to BDSBeamline - name, length & angle.
+ * 
+ * This inherits the objects from the BDSGeometryComponent and therefore
+ * effectively owns it without deleting it explicitly as a whole object.
+ *
+ * @author Laurie Nevay
+ */
+
 class BDSSimpleComponent: public BDSAcceleratorComponent
 {
 public:
   BDSSimpleComponent(G4String              name,
 		     BDSGeometryComponent* componentIn,
-					 G4double length,
+		     G4double              length,
 		     G4double              angle = 0);
 
+  /// Default destructor suffices as this calls base class which
+  /// clears everything up.
   virtual ~BDSSimpleComponent(){;}
 
 private:
+  /// Private default constructor to force use of given one.
   BDSSimpleComponent();
 
   /// Required implementation from base class.
