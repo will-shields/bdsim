24c8f3336ed717a51a9ef21a29ce86746fde1a25
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-07-12 13:38:27 +0100

    change loading semantics
    Former-commit-id: 6027b60f8470260cf0e7a04410ee66df587f9198

--- include/BDSBunchRecreate.hh
+++ include/BDSBunchRecreate.hh
@@ -2,13 +2,16 @@
 #define BDSBUNCHRECREATE
 
 #include "BDSBunchInterface.hh"
+#include "BDSOutputROOTEventSampler.hh"
 #include "analysis/Event.hh"
 #include "parser/options.h"
 
 #include "globals.hh" // geant4 types / globals
 
-#include "TChain.h"
+class TFile;
+class TTree;
 
+class TChain;
 
 class BDSBunchRecreate: public BDSBunchInterface
 {
@@ -24,10 +27,7 @@ public:
 
 private:
   /// Open the root file and overlay the data on a local instance of the correct class.
-  void LoadFile();
-  
-  /// File to load coordinates from
-  std::string filename;
+  void LoadFile(G4String filename);
 
   /// Offset in event number from start of file
   G4int eventOffset;
@@ -41,12 +41,18 @@ private:
   /// If true, the seed state will also be loaded and set, if not, merely the
   /// coordinates from the file will be used. This will default to true.
   G4bool strongRecreation;
-  
-  /// Chain of file(s) w.r.t to Event tree for primary coordinate loading.
-  TChain* chain;
 
-  /// A local instance of the event class to copy variables from the ROOT file onto.
-  Event* event;
+    TFile* file;
+    TTree* eventTree;
+    TBranch* primary;
+    TBranch* info;
+#ifndef __ROOTDOUBLE__
+    BDSOutputROOTEventSampler<float>* primaryLocal;
+#else
+    BDSOutputROOTEventSampler<double>* primaryLocal;
+#endif
+    BDSOutputROOTEventInfo* infoLocal;
+
 };
 
 #endif
--- src/BDSBunchRecreate.cc
+++ src/BDSBunchRecreate.cc
@@ -12,65 +12,71 @@
 #include "G4ParticleTable.hh"
 
 #include "TChain.h"
+#include "TFile.h"
+#include "TTree.h"
 
 #include <string>
 #include <vector>
 
 BDSBunchRecreate::BDSBunchRecreate():
-  filename(""),
   eventOffset(0),
   currentEventNumber(0),
   entries(0),
   strongRecreation(true),
-  chain(nullptr),
-  event(nullptr)
+  eventTree(nullptr),
+  primary(nullptr),
+  info(nullptr),
+  primaryLocal(nullptr),
+  infoLocal(nullptr)
 {;}
 
 BDSBunchRecreate::~BDSBunchRecreate()
 {
-  delete chain;
-  delete event;
+  if (file)
+    {file->Close();}
 }
 
 void BDSBunchRecreate::SetOptions(const GMAD::Options& options)
 {
-  filename           = options.distribFile;
   eventOffset        = options.eventOffset;
   currentEventNumber = eventOffset;
   strongRecreation   = options.recreateSeedState;
 
-  LoadFile();
+  LoadFile(options.distribFile);
 }
 
-void BDSBunchRecreate::LoadFile()
+void BDSBunchRecreate::LoadFile(G4String filename)
 {
-  TChain* chain = new TChain("Event");
-  
-  G4int nFilesAdded = chain->Add(filename.c_str());
-  if (nFilesAdded == 0)
+  file = new TFile(filename.c_str(), "READ");
+  if (!file)
     {
       G4cout << __METHOD_NAME__ << "Input ROOT file not found \"" << filename << "\"" << G4endl;
       exit(1);
     }
 
-  entries = chain->GetEntries();
-  
-  event = new Event();
+  eventTree = (TTree*)file->Get("Event");
+  entries = (G4int) eventTree->GetEntries();
+
   // prepare vector of samplers to load - empty vector means none.
   std::vector<std::string> names;
-  event->SetBranchAddress(chain, names);
 
-  // make access efficient by avoiding copying all sampler data each time
-  chain->SetBranchStatus("*", 0);       // turn off all branches
-  chain->SetBranchStatus("Primary", 1); // turn on primary branch
-  chain->SetBranchStatus("Info",    1); // turn on info branch
+#ifndef __ROOTDOUBLE__
+  primaryLocal = new BDSOutputROOTEventSampler<float>();
+#else
+  primaryLocal = new BDSOutputROOTEventSampler<double>();
+#endif
+  infoLocal = new BDSOutputROOTEventInfo();
+  info      = eventTree->GetBranch("Info.");
+  info->SetAddress(infoLocal);
+  primary = eventTree->GetBranch("Primary.");
+  primary->SetAddress(primaryLocal);
   
   // Here we set the particle name from the PDG ID of first useful event in the file.
   // As the bunch is constructed before BDSModularPhysics list this is ok. BDSModularPhysicsList
   // access global constants for the particle name and constructs the particle definition used
   // for every event.
-  chain->GetEntry(currentEventNumber); // load first useful event
-  G4int particlePDGID = event->GetPrimaries()->partID[0]; // get PDG ID of first entry
+  primary->GetEntry(currentEventNumber); // load first useful event
+  G4int particlePDGID = primaryLocal->partID[0]; // get PDG ID of first entry
   G4String particleName = G4ParticleTable::GetParticleTable()->FindParticle(particlePDGID)->GetParticleName();
   BDSGlobalConstants::Instance()->SetParticleName(particleName);
 }
@@ -84,31 +90,30 @@ void BDSBunchRecreate::GetNextParticle(G4double& x0, G4double& y0, G4double& z0,
       G4cout << __METHOD_NAME__ << "reached end of primary entries - repeating" << G4endl;
       currentEventNumber = 0;
     }
-  
-  // load the entry
-  chain->GetEntry(currentEventNumber);
 
   // load the seed state
   if (strongRecreation)
-    {BDSRandom::SetSeedState(event->info->seedStateAtStart);}
+    {
+      info->GetEntry(0);
+      BDSRandom::SetSeedState(infoLocal->seedStateAtStart);
+    }
 
-  // set the variables - the type is templated so the auto helps here
-  const auto& p = *(event->GetPrimaries());
-  // primaries is an instance of BDSOutputROOTEventSampler with only one entry
+  // load the entry
+  primary->GetEntry(currentEventNumber);
+  // Primary is an instance of BDSOutputROOTEventSampler with only one entry
   // all members are vectors - take just first entry - ie index 0
-
   // Note, there's no need to use the curvilinear transform as that would've already
   // been used when the coordinates were generated and before they were recorded, so
   // we simply load the basic global coordinates directly.
-  x0 = p.x[0];
-  y0 = p.y[0];
-  z0 = p.z;
-  xp = p.xp[0];
-  yp = p.yp[0];
-  zp = p.zp[0];
-  t  = p.t[0];
-  weight = p.weight[0];
-  E      = p.energy[0]; // total energy in both cases
+  x0 = primaryLocal->x[0];
+  y0 = primaryLocal->y[0];
+  z0 = primaryLocal->z;
+  xp = primaryLocal->xp[0];
+  yp = primaryLocal->yp[0];
+  zp = primaryLocal->zp[0];
+  t  = primaryLocal->t[0];
+  weight = primaryLocal->weight[0];
+  E      = primaryLocal->energy[0]; // total energy in both cases
   
   // increment
   currentEventNumber++;
