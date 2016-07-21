e2a5258a494e608d7b7ad41a623cf5ef8517118d
William Shields <williamshields@dhcp232.pp.rhul.ac.uk>
2016-07-21 13:12:42 +0100

    Fix for incorrect resolution of earlier merge conflict.
    Former-commit-id: 762f3264b0ee57c56376f069c97c4078956bb69f

--- include/BDSIntegratorType.hh
+++ include/BDSIntegratorType.hh
@@ -13,11 +13,9 @@
 
 struct integratortypes_def
 {
-<<<<<<< HEAD
-  enum type {solenoid, dipole, quadrupole, sextupole, multipole, octupole, decapole, fringe,
-=======
-  enum type {none, solenoid, dipole, quadrupole, sextupole, octupole, decapole,
->>>>>>> origin/fieldmaps
+
+  enum type {none, solenoid, dipole, quadrupole, sextupole,
+	     multipole, octupole, decapole, fringe,
 	     g4cashkarprkf45, g4classicalrk4, g4constrk4, g4expliciteuler,
 	     g4impliciteuler, g4simpleheum, g4simplerunge,
 	     g4exacthelixstepper, g4helixexpliciteuler, g4helixheum,
