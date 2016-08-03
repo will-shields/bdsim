f7f953bd08943ac92295e79a75a030f27f585413
William Shields <williamshields@dhcp232.pp.rhul.ac.uk>
2016-08-03 21:46:05 +0100

    Rewrite of fringe field integrator. Include unused backup runge kutta integrator to remove compiler warnings (similar to dipole integrator).
    Former-commit-id: a0f79a61b48f1b943ed252a45f75b4a36bf520da

--- src/BDSIntegratorFringefield.cc
+++ src/BDSIntegratorFringefield.cc
@@ -8,98 +8,133 @@
 #include "G4MagIntegratorStepper.hh"
 #include "G4ThreeVector.hh"
 
-#include <cmath>
-#include <utility>
 
 BDSIntegratorFringefield::BDSIntegratorFringefield(BDSMagnetStrength const* strength,
-						 G4double                 brho,
-						 G4Mag_EqRhs*             eqOfMIn):
-  BDSIntegratorDipole(strength, brho, eqOfMIn),
-  yInitial(0), yMidPoint(0), yFinal(0)
+                                                   G4double                 brho,
+                                                   G4Mag_EqRhs*             eqOfMIn):
+        BDSIntegratorDipole(strength, brho, eqOfMIn),
+        angle((*strength)["angle"]),
+        bField((*strength)["field"])
 {
   polefaceAngle = (*strength)["polefaceangle"];
-  angle = (*strength)["angle"];
-  bField = (*strength)["field"];
-#ifdef BDSDEBUG
-  G4cout << __METHOD_NAME__ << "B' = " << bPrime << G4endl;
-#endif
 }
 
-void BDSIntegratorFringefield::Stepper(const G4double yInput[],
-				      const G4double dydx[],
-				      const G4double h,
-				      G4double       yOut[],
-				      G4double       yErr[])
+void BDSIntegratorFringefield::AdvanceHelix(const G4double yIn[],
+                                            const G4double dydx[],
+                                            const G4double h,
+                                            G4double       yOut[],
+                                            G4double       yErr[])
 {
-  const G4double *pIn   = yInput+3;
+#ifdef BDSDEBUG
+  G4cout << "BDSIntegratorDipole: step= " << h/CLHEP::m << " m" << G4endl
+         << " x  = " << yIn[0]/CLHEP::m     << " m" << G4endl
+         << " y  = " << yIn[1]/CLHEP::m     << " m" << G4endl
+         << " z  = " << yIn[2]/CLHEP::m     << " m" << G4endl
+         << " px = " << yIn[3]/CLHEP::GeV   << " GeV/c" << G4endl
+         << " py = " << yIn[4]/CLHEP::GeV   << " GeV/c" << G4endl
+         << " pz = " << yIn[5]/CLHEP::GeV   << " GeV/c" << G4endl
+         << " q  = " << charge/CLHEP::eplus << " e" << G4endl
+	 << " B  = " << bField/(CLHEP::tesla) << " T" << G4endl
+    //         << " k= " << kappa/(1./CLHEP::m2) << "m^-2" << G4endl
+         << G4endl;
+#endif
+
+  const G4double *pIn   = yIn+3;
   G4ThreeVector GlobalP = G4ThreeVector(pIn[0], pIn[1], pIn[2]);
-  G4ThreeVector GlobalR = G4ThreeVector(yInput[0], yInput[1], yInput[2]);
-  InitialiseTransform(GlobalP, GlobalR, h);
-  G4ThreeVector LocalR  = ConvertToLocal(GlobalR);
-  G4ThreeVector LocalRp = G4ThreeVector(dydx[0], dydx[1], dydx[2]);
-  
-  G4double      InitMag = GlobalP.mag();
-  G4ThreeVector InitMomDir = GlobalP.unit();
- 
-  G4double      charge  = (eqOfM->FCof())/CLHEP::c_light;
-  G4double      rho = InitMag/CLHEP::GeV/(0.299792458 * bField * charge) * CLHEP::m;
-    
+  G4ThreeVector GlobalR = G4ThreeVector(yIn[0], yIn[1], yIn[2]);
+
+  G4double      InitMag        = GlobalP.mag();
+  G4ThreeVector InitMomDir     = GlobalP.unit();
+
   if(bField==0 || eqOfM->FCof()==0)
     {
       G4ThreeVector positionMove = h * InitMomDir;
-      
-      yOut[0] = yInput[0] + positionMove.x();
-      yOut[1] = yInput[1] + positionMove.y();
-      yOut[2] = yInput[2] + positionMove.z();
-      
+
+      yOut[0] = yIn[0] + positionMove.x();
+      yOut[1] = yIn[1] + positionMove.y();
+      yOut[2] = yIn[2] + positionMove.z();
+
       yOut[3] = GlobalP.x();
       yOut[4] = GlobalP.y();
       yOut[5] = GlobalP.z();
-      
+
       distChord = 0;
       return;
     }
-    
-  std::pair<G4ThreeVector,G4ThreeVector> RandRp = updatePandR(rho,h,LocalR,LocalRp);
 
+  G4ThreeVector globalPositionIn(yIn[0], yIn[1], yIn[2]);
+  G4ThreeVector LocalR  = ConvertToLocal(globalPositionIn);
+  G4ThreeVector LocalP = ConvertAxisToLocal(globalPositionIn, InitMomDir);
+
+  G4double      charge  = (eqOfM->FCof())/CLHEP::c_light;
+  G4double      rho = InitMag/CLHEP::GeV/(0.299792458 * bField/CLHEP::tesla * charge) * CLHEP::m;
+
+  std::pair<G4ThreeVector,G4ThreeVector> RandRp = updatePandR(rho,h,LocalR,LocalP);
   LocalR = RandRp.first;
-  LocalRp = RandRp.second;
-    
+  LocalP = RandRp.second;
+  G4double CosT_ov_2 = cos(h/rho/2.0);
+  distChord = fabs(rho)*(1.-CosT_ov_2);
+
+
   G4double x0  = LocalR.x();
   G4double y0  = LocalR.y();
   G4double z0  = LocalR.z();
-  G4double xp  = LocalRp.x();
-  G4double yp  = LocalRp.y();
-  G4double zp  = LocalRp.z();
-    
+  G4double xp  = LocalP.x();
+  G4double yp  = LocalP.y();
+  G4double zp  = LocalP.z();
+
   // initialise output varibles with input position as default
   G4double x1  = x0;
   G4double y1  = y0;
-  G4double z1  = z0 + h; // new z position will be along z by step length h
+  G4double z1  = z0;  // new z position will be along z by step length h
   G4double xp1 = xp;
   G4double kick =(y0 * tan(polefaceAngle)/rho);
   G4double yp1 = yp - kick;
   G4double zp1 = zp;
-    
+
   LocalR.setX(x1);
   LocalR.setY(y1);
   LocalR.setZ(z1);
-  
-  LocalRp.setX(xp1);
-  LocalRp.setY(yp1);
-  LocalRp.setZ(zp1);
 
-  GlobalR = ConvertToGlobal(LocalR);
-  GlobalP = ConvertAxisToGlobal(LocalRp);
-  
-  yOut[0] = GlobalR.x();
-  yOut[1] = GlobalR.y();
-  yOut[2] = GlobalR.z();
+  LocalP.setX(xp1);
+  LocalP.setY(yp1);
+  LocalP.setZ(zp1);
 
-  yOut[3] = GlobalP.x();
-  yOut[4] = GlobalP.y();
-  yOut[5] = GlobalP.z();
+  if(true){
+    GlobalR = ConvertToGlobal(LocalR);
+    GlobalP = ConvertAxisToGlobal(GlobalP, LocalP);
+    GlobalP *= InitMag;
 
-  for(G4int i = 0; i < nVariables; i++)
-    {yErr[i] = 0;}
+    yOut[0] = GlobalR.x();
+    yOut[1] = GlobalR.y();
+    yOut[2] = GlobalR.z();
+
+    yOut[3] = GlobalP.x();
+    yOut[4] = GlobalP.y();
+    yOut[5] = GlobalP.z();
+    return;
+  }
+  else
+  {
+#ifdef BDSDEBUG
+    G4cout << __METHOD_NAME__ << " local helical steps - using G4ClassicalRK4" << G4endl;
+#endif
+    G4cout << "Using Runge Kutta "<< G4endl;
+
+    // use a classical Runge Kutta stepper here
+    backupStepper->Stepper(yIn, dydx, h, yOut, yErr);
+  }
+}
+
+void BDSIntegratorFringefield::Stepper(const G4double yInput[],
+                                       const G4double dydx[],
+                                       const G4double hstep,
+                                       G4double yOut[],
+                                       G4double yErr[])
+{
+  G4double err = 1e-10 * hstep; // very small linear increase with distance
+  for(G4int i=0; i<nVariables; i++)
+  {yErr[i] = 0;}
+
+  AdvanceHelix(yInput,dydx,hstep,yOut,yErr);
 }
