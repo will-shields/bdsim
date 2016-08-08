c22c6e2ab5aedca600f27119e704b7f4e2661231
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-08-08 18:37:56 +0100

    Fix octupole and sextupole magnetic fields - should be in global coordinates when returned.
    Former-commit-id: 909a6a5bea88431bf4ff8a64dbc72185731474e2

--- src/BDSOctMagField.cc
+++ src/BDSOctMagField.cc
@@ -17,13 +17,20 @@ void BDSOctMagField::GetFieldValue(const G4double Point[4],
   G4ThreeVector GlobalR(Point[0], Point[1], Point[2]);
   
   auxNavigator->LocateGlobalPointAndSetup(GlobalR);
-  G4AffineTransform GlobalAffine = auxNavigator->GetGlobalToLocalTransform();
-  G4ThreeVector     LocalR       = GlobalAffine.TransformPoint(GlobalR); 
+  G4AffineTransform globalToLocal = auxNavigator->GetGlobalToLocalTransform();
+  G4AffineTransform localToGlobal = auxNavigator->GetLocalToGlobalTransform();
+  G4ThreeVector     LocalR        = globalToLocal.TransformPoint(GlobalR); 
   
-  Bfield[0] = (3*LocalR.x()*LocalR.x()*LocalR.y()-pow(LocalR.y(),3))*itsBTrpPrime/6.;
-  Bfield[1] = (pow(LocalR.x(),3)-LocalR.x()*LocalR.y()*LocalR.y())*itsBTrpPrime/6.;
-  Bfield[2] = 0;
+  G4double bx = (3*LocalR.x()*LocalR.x()*LocalR.y()-pow(LocalR.y(),3))*itsBTrpPrime/6.;
+  G4double by = (pow(LocalR.x(),3)-LocalR.x()*LocalR.y()*LocalR.y())*itsBTrpPrime/6.;
   // factor of 6 is actually 3-factorial.
+
+  G4ThreeVector localB(bx, by, 0);
+
+  G4ThreeVector globalB = localToGlobal.TransformAxis(localB);
+  Bfield[0] = globalB[0];
+  Bfield[1] = globalB[1];
+  Bfield[2] = globalB[2];
 }
 
 
