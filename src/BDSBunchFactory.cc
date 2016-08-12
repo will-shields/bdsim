01084b0a525ec10cdfd7e739fbecdec706119b54
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-08-12 11:30:44 +0100

    Fix indentation / braces.
    Former-commit-id: 13d5db0cb97e7a55d105dea2a5b3970310287060

--- src/BDSBunchFactory.cc
+++ src/BDSBunchFactory.cc
@@ -42,8 +42,10 @@ BDSBunchInterface* BDSBunchFactory::createBunch(G4String distribType, G4String d
   else if(distribType == "halo") 
     bdsBunch = new BDSBunchHalo();
   else if(distribType == "userfile")
-    if(distribFile.rfind("gz") != std::string::npos)  bdsBunch = new BDSBunchUserFile<igzstream>();
-    else                                              bdsBunch = new BDSBunchUserFile<std::ifstream>();
+    if(distribFile.rfind("gz") != std::string::npos)
+      {bdsBunch = new BDSBunchUserFile<igzstream>();}
+    else
+      {bdsBunch = new BDSBunchUserFile<std::ifstream>();}
   else if(distribType == "composite")
     bdsBunch = new BDSBunchComposite();
   else if(distribType == "ptc") 
