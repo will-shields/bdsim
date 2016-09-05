cb2492ef8152f53533fdd82f193674c42296ae29
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-09-05 14:00:18 +0100

    Allow the step to record the volume that was used for a transform when it was prepared.
    Former-commit-id: 42e57530f9b87bd8f8ce77097a94557f19431287

--- include/BDSStep.hh
+++ include/BDSStep.hh
@@ -3,6 +3,8 @@
 
 #include "G4ThreeVector.hh"
 
+class G4VPhysicalVolume;
+
 /**
  * @brief A simple class to represent the positions of a step.
  *
@@ -13,22 +15,27 @@ class BDSStep
 {
 public:
   BDSStep();
-  BDSStep(G4ThreeVector preStepPointIn, G4ThreeVector postStepPointIn);
+  BDSStep(G4ThreeVector preStepPointIn,
+	  G4ThreeVector postStepPointIn,
+	  G4VPhysicalVolume* volumeForTransformIn = nullptr);
   ~BDSStep();
 
   /// @{ Accessor.
   inline G4ThreeVector PreStepPoint()  const {return preStepPoint;}
   inline G4ThreeVector PostStepPoint() const {return postStepPoint;}
+  inline G4VPhysicalVolume* VolumeForTransform() const {return volumeForTransform;}
   /// @}
 
   /// @{ Setter.
   inline void SetPreStepPoint(G4ThreeVector preIn)   {preStepPoint = preIn;}
   inline void SetPostStepPoint(G4ThreeVector postIn) {postStepPoint = postIn;}
+  inline void SetVolumeForTransform(G4VPhysicalVolume* volIn) {volumeForTransform = volIn;}
   /// @}
   
 private:
   G4ThreeVector preStepPoint;
   G4ThreeVector postStepPoint;
+  G4VPhysicalVolume* volumeForTransform; ///< The volume that was used for the transform.
 };
 
 #endif
--- src/BDSStep.cc
+++ src/BDSStep.cc
@@ -3,12 +3,16 @@
 
 BDSStep::BDSStep():
   preStepPoint(G4ThreeVector()),
-  postStepPoint(G4ThreeVector())
+  postStepPoint(G4ThreeVector()),
+  volumeForTransform(nullptr)
 {;}
 
-BDSStep::BDSStep(G4ThreeVector preStepPointIn, G4ThreeVector postStepPointIn):
+BDSStep::BDSStep(G4ThreeVector preStepPointIn,
+		 G4ThreeVector postStepPointIn,
+		 G4VPhysicalVolume* volumeForTransformIn):
   preStepPoint(preStepPointIn),
-  postStepPoint(postStepPointIn)
+  postStepPoint(postStepPointIn),
+  volumeForTransform(volumeForTransformIn)
 {;}
 
 BDSStep::~BDSStep()
