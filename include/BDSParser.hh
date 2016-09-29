78920aa996287dc6c96f9703dbdda7db52384d5e
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-09-29 17:31:56 +0900

    Provide interface to parser objects.  Alphebetise accessors as easier to read.
    Former-commit-id: 24766bc19a5dd2c6d111728928d303bc2ede448c

--- include/BDSParser.hh
+++ include/BDSParser.hh
@@ -34,24 +34,30 @@ public:
   void AmalgamateOptions(const GMAD::Options& optionsIn);
   /// Check options for consistency
   void CheckOptions();
+
+  /// Return the vector of region objects.
+  inline const std::vector<GMAD::Atom>& GetAtoms() const {return atom_list;}
   
   /// Return beamline
   inline const GMAD::FastList<GMAD::Element>& GetBeamline() const {return beamline_list;}
   
   /// Return biasing list
   inline const GMAD::FastList<GMAD::PhysicsBiasing>& GetBiasing() const {return xsecbias_list;}
+
+  /// Return cavity model list
+  inline const std::vector<GMAD::CavityModel>& GetCavityModels() const {return cavitymodel_list;}
   
+  /// Return the vector of field objects.
+  inline const std::vector<GMAD::Field>& GetFields() const {return field_list;}
+
   /// Return material list
   inline const std::vector<GMAD::Material>& GetMaterials() const {return material_list;}
-  
-  /// Return atom list
-  inline const std::vector<GMAD::Atom>& GetAtoms() const {return atom_list;}
   
   /// Return region list
   inline const std::vector<GMAD::Region>& GetRegions() const {return region_list;}
   
-  /// Return cavity model list
-  inline const std::vector<GMAD::CavityModel>& GetCavityModels() const {return cavitymodel_list;}
+  /// Return the vector of placement objects.
+  inline const std::vector<GMAD::Placement>& GetPlacements() const {return placement_list;}
 
 protected:
   /// Constructor from filename
