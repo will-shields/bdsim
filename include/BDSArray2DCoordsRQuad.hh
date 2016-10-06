803437fc8b80e290240f9285508b68cc12a6c2e2
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-10-06 14:15:59 +0900

    2D array in coords that reflects quadrant appropriately for quadrupole field map.
    Former-commit-id: 595e1738fd03047e4b139ad232b3dc17eb4dfc58

---
+++ include/BDSArray2DCoordsRQuad.hh
@@ -0,0 +1,82 @@
+#ifndef BDSARRAY2DCOORDSRQUAD_H
+#define BDSARRAY2DCOORDSRQUAD_H
+
+#include "BDSArray2DCoords.hh"
+#include "BDSFieldValue.hh"
+
+#include "globals.hh"
+
+#include <ostream>
+
+/**
+ * @brief A wrapper to achieve 2D reflection of a minimal quadrupole field solve.
+ * 
+ * This allows a small 2pi/8 segment between the x axis and y=x in the positive
+ * quadrant to be reflected to represent a quadrupole.  Ie relfected about y=z
+ * in the positive quadrant (A); reflected and inverted in the negative quadrant (C);
+ * reflected and y field component inverted in quadrant (B); reflected and x component
+ * inverted in quadrant (D)
+ *
+ * B | A
+ * -----
+ * C | D
+ *
+ * This owns the array it wraps.
+ *
+ * This makes an array look 4 times bigger than it is - ie 2x as big in x and y. The
+ * array coordinates are correspondingly shifted. What would've been (0,0) in a 100x100
+ * array will now be at index (101,101).
+ *
+ * This is reflected about 0,0 and can only be so given the implementation.
+ *
+ * Some interfaces are overloaded and some aren't as are required to be (sizeX for example).
+ * The ostream << writes both the raw array and the reflected version too.
+ *
+ * Note, by return a reference to a non-const member returnValue that may be updated by another
+ * query, thread safety could be an issue.
+ * 
+ * @author Laurie Nevay
+ */
+
+class BDSArray2DCoordsRQuad: public BDSArray2DCoords
+{
+public:
+  BDSArray2DCoordsRQuad(BDSArray2DCoords* arrayIn);
+  virtual ~BDSArray2DCoordsRQuad(){;}
+
+  /// @{ Overridden from BDSArray4DCoords.
+  virtual G4bool OutsideCoords(const G4double x,
+			       const G4double y,
+			       const G4double z,
+			       const G4double t) const;
+
+  virtual G4double ArrayCoordsFromX(const G4double x) const;
+  virtual G4double ArrayCoordsFromY(const G4double y) const;
+
+  virtual G4int NearestX(const G4double x) const;
+  virtual G4int NearestY(const G4double y) const;
+  /// @}
+
+  /// @{ Overridden from BDSArray4D.
+  virtual const BDSFieldValue& GetConst(const G4int x,
+					const G4int y,
+					const G4int z,
+					const G4int t) const;
+
+  virtual G4bool Outside(const G4int x,
+			 const G4int y,
+			 const G4int z,
+			 const G4int t) const;
+  /// @}
+  
+  friend std::ostream& operator<< (std::ostream& out, BDSArray2DCoordsRQuad const &a);
+
+private:
+  BDSArray2DCoordsRQuad() = delete;
+
+  /// Must return by reference and the true data value may have to be reflected
+  /// in one or more dimensions.  Copy to this and return reference to it.
+  mutable BDSFieldValue returnValue;
+};
+
+#endif
---
+++ src/BDSArray2DCoordsRQuad.cc
@@ -0,0 +1,151 @@
+#include "BDSArray2DCoordsRQuad.hh"
+#include "BDSFieldValue.hh"
+
+#include "globals.hh"
+
+#include <algorithm>
+#include <cmath>
+#include <ostream>
+
+
+BDSArray2DCoordsRQuad::BDSArray2DCoordsRQuad(BDSArray2DCoords* arrayIn):
+  BDSArray2DCoords(*arrayIn),
+  returnValue(BDSFieldValue())
+{;}
+
+G4bool BDSArray2DCoordsRQuad::OutsideCoords(const G4double x,
+					    const G4double y,
+					    const G4double z,
+					    const G4double t) const
+{
+  G4bool rx = x < -xMax || x > xMax;
+  G4bool ry = y < -yMax || y > yMax;
+  G4bool rz = z <  zMin || z > zMax;
+  G4bool rt = t <  tMin || t > tMax;
+  return rx || ry || rz || rt;
+}
+
+G4double BDSArray2DCoordsRQuad::ArrayCoordsFromX(const G4double x) const
+{
+  // xmin becomes -xmax
+  return (x + xMax) / xStep;
+}
+
+G4double BDSArray2DCoordsRQuad::ArrayCoordsFromY(const G4double y) const
+{
+  return (y + yMax) / xStep;
+}
+
+G4int BDSArray2DCoordsRQuad::NearestX(const G4double x) const
+{
+  return (G4int)round((x+xMax)/xStep);
+}
+
+G4int BDSArray2DCoordsRQuad::NearestY(const G4double y) const
+{
+  return (G4int)round((y+yMax)/yStep);
+}
+
+const BDSFieldValue& BDSArray2DCoordsRQuad::GetConst(const G4int x,
+						     const G4int y,
+						     const G4int z,
+						     const G4int t) const
+{
+  if (Outside(x,y,z,t))
+    {return defaultValue;}
+
+  // Data is really
+  // A
+
+  // This interface makes it look like (0,0) bottom left, top right (2*xsize, 2*ysize)
+  // in array coords - ie int number of elements.
+  // B | A
+  // -----
+  // C | D
+
+  // factors to multiply result by - saves duplicate testing of which quadrant etc
+  G4double xr = 1;
+  G4double yr = 1;
+  G4int    xi = x; // copy of x index that can be modified.
+  G4int    yi = y; 
+  
+  if (x < nX)
+    {
+      if (y < nY)
+	{// C quadrant - coordinate flip is a subtraction in array coords
+	  xi = nX - x; xr = -1; // flip
+	  yi = nY - y; yr = -1; // flip
+	}
+      else
+	{// B quadrant
+	  xi = nX - x; // flip
+	  yi = y - nY; yr = -1; // just offset index, but flip result
+	}
+    }
+  else
+    {
+      if (y < nY)
+	{// D quadrant
+	  xi = x - nX; // just offset index
+	  yi = nY - y;
+	}
+      else
+	{// A quadrant
+	  xi = x - nX;
+	  yi = y - nY;
+	}
+    }
+
+  G4bool swapResult = false;
+  if (yi > xi) //lies above y = x line (even in array coords)
+    {
+      std::swap(xi, yi);
+      swapResult = true;
+    }
+
+  returnValue = BDSArray2DCoords::GetConst(x,y,z,t);
+
+  if (swapResult)
+    {
+      G4double xv = returnValue.x();
+      G4double yv = returnValue.y();
+      returnValue = BDSFieldValue(yv,xv,returnValue.z());
+    }
+  
+  returnValue[0] = returnValue.x() * xr;
+  returnValue[1] = returnValue.y() * yr;
+
+  return returnValue;
+}
+
+G4bool BDSArray2DCoordsRQuad::Outside(const G4int x,
+				      const G4int y,
+				      const G4int z,
+				      const G4int t) const
+{
+  G4bool rx = x < 0 || x > 2*(nX-1);
+  G4bool ry = y < 0 || y > 2*(nY-1);
+  G4bool rz = z < 0 || z > 2*(nZ-1);
+  G4bool rt = t < 0 || t > 2*(nT-1);
+  return rx || ry || rz || rt;
+}
+
+std::ostream& operator<< (std::ostream& out, BDSArray2DCoordsRQuad const &a)
+{
+  out << *(static_cast<const BDSArray2DCoords*>(&a));
+
+  out << G4endl << "REFLECTED VERSION" << G4endl;
+  
+  for (G4int t = 0; t < a.nT; t++)
+    {
+      for (G4int z = 0; z < a.nZ; z++)
+	{
+	  for (G4int y = 0; y < 2*a.nY; y++)
+	    {
+	      for (G4int x = 0; x < 2*a.nX; x++)
+		{out << a.GetConst(x,y,z,t) << G4endl;}
+	    }
+	}
+    }
+  return out;
+}
