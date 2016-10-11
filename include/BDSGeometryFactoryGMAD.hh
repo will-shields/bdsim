0141ff3cee0a5a0eb4d26cd3f5205ddec5482c2a
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-10-11 13:47:35 +0100

    Refactor GMAD geometry builder into factory.
    Former-commit-id: ff5218b9443668768027b6f28267572756e469bb

---
+++ include/BDSGeometryFactoryGMAD.hh
@@ -0,0 +1,90 @@
+#ifndef BDSGEOMETRYFACTORYGMAD_H
+#define BDSGEOMETRYFACTORYGMAD_H
+
+#include "BDSGeometryFactoryBase.hh"
+
+#include "globals.hh"
+#include "G4RotationMatrix.hh"
+
+#include <fstream>
+#include <map>
+#include <vector>
+
+class BDSGeometryExternal;
+class G4Colour;
+class G4LogicalVolume;
+class G4VPhysicalVolume;
+class G4VSolid;
+
+/**
+ * @brief Factory for GMAD geometry description.
+ *
+ * Originally part of BDSIM. Adapated for recent refactoring.
+ */
+
+class BDSGeometryFactoryGMAD: public BDSGeometryFactoryBase
+{
+public:
+  /// Singleton accessor
+  static BDSGeometryFactoryGMAD* Instance();
+
+  virtual ~BDSGeometryFactoryGMAD();
+
+  /// Main interface overrides base class method to construct a file with
+  /// optional colour mapping.
+  virtual BDSGeometryExternal* Build(G4String fileName,
+				     std::map<G4String, G4Colour*>* colourMapping = nullptr);
+
+protected:
+  /// Clear all transient vectors without deleting the objects the pointers point to.
+  virtual void CleanUp();
+  
+private:
+  /// Private default constructor as singleton.
+  BDSGeometryFactoryGMAD();
+
+  /// Singleton instance
+  static BDSGeometryFactoryGMAD* instance;
+
+  /// Simple there was an error print out.
+  inline void Error() {G4cout << "ggmad: error in file format" << G4endl;}
+
+  /// Read a single word from the ifstream.
+  G4String GetWord(std::ifstream& infputf) const;
+
+  /// Get a numerical parameter from the ifstream.
+  void GetParameter(std::ifstream& inputf,
+		    G4double&      x,
+		    G4String       name,
+		    G4String       lastToken) const;
+
+  /// Get a string parameter from the ifstream.
+  void GetParameter(std::ifstream& inputf,
+		    G4String&      lval,
+		    G4String       name,
+		    G4String lastToken) const;
+
+  /// Construct logical volume, rotation and placement.
+  void Finish(G4String         name,
+	      G4VSolid*        solid,
+	      G4String         materialName,
+	      G4LogicalVolume* mother,
+	      G4double         phi   = 0,
+	      G4double         theta = 0,
+	      G4double         psi   = 0,
+	      G4double         x0 = 0,
+	      G4double         y0 = 0,
+	      G4double         z0 = 0);
+
+  /// Cache of whether to check overlaps or not.
+  const G4bool checkOverlaps;
+
+  /// @{ Transient vector for construction.
+  std::vector<G4RotationMatrix*>  rotations;
+  std::vector<G4VPhysicalVolume*> pvs;
+  std::vector<G4LogicalVolume*>   lvs;
+  std::vector<G4VSolid*>          solids;
+  /// @}
+};
+
+#endif
