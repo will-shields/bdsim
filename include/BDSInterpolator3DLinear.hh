0adf208df7e54b526813259322b909bcec9d373a
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-10-19 18:55:37 +0100

    3D Linear interpolation.
    Former-commit-id: 79ff0f8d51568a66304074fa3f2c2a908327fee9

---
+++ include/BDSInterpolator3DLinear.hh
@@ -0,0 +1,44 @@
+#ifndef BDSINTERPOLATE3DLINEAR_H
+#define BDSINTERPOLATE3DLINEAR_H
+
+#include "BDSFieldValue.hh"
+#include "BDSInterpolator3D.hh"
+
+#include "G4Types.hh"
+
+class BDSArray3DCoords;
+
+/** 
+ * @brief Linear interpolation over 3d array.
+ * 
+ * Does not own array - so multiple interpolators could be used on same data.
+ * 
+ * This uses a trilinear algorithm to interpolate a 3d array and return
+ * the value at any arbitrary point. If the point lies outside the array
+ * the default value for the templated parameter is returned (typically 0).
+ * Therefore, the field drops to 0 outside the specified region.
+ * 
+ * @author Laurie Nevay
+ */
+
+class BDSInterpolator3DLinear: public BDSInterpolator3D
+{
+public:
+  BDSInterpolator3DLinear(BDSArray3DCoords* arrayIn);
+  virtual ~BDSInterpolator3DLinear();
+
+protected:
+  virtual BDSFieldValue GetInterpolatedValueT(G4double x, G4double y, G4double z) const;
+
+private:
+  /// Private default constructor to force use of provided one.
+  BDSInterpolator3DLinear() = delete;
+
+  /// Interpolate between to field values for a given normalised distance between
+  /// 0,1.
+  BDSFieldValue Interpolate1D(const BDSFieldValue& v1,
+			      const BDSFieldValue& v2,
+			      const G4double       dist) const;
+};
+
+#endif
---
+++ src/BDSInterpolator3DLinear.cc
@@ -0,0 +1,69 @@
+#include "BDSArray3DCoords.hh"
+#include "BDSFieldValue.hh"
+#include "BDSInterpolator3DLinear.hh"
+
+#include "globals.hh"
+
+#include <cmath>
+
+BDSInterpolator3DLinear::BDSInterpolator3DLinear(BDSArray3DCoords* arrayIn):
+  BDSInterpolator3D(arrayIn)
+{;}
+
+BDSInterpolator3DLinear::~BDSInterpolator3DLinear()
+{;}
+
+BDSFieldValue BDSInterpolator3DLinear::Interpolate1D(const BDSFieldValue& v1,
+						     const BDSFieldValue& v2,
+						     const G4double       dist) const
+{
+  return v1*(1.-dist) + v2*dist;
+}
+
+BDSFieldValue BDSInterpolator3DLinear::GetInterpolatedValueT(G4double x,
+							     G4double y,
+							     G4double z) const
+{
+  G4double xarr = array->ArrayCoordsFromX(x);
+  G4double yarr = array->ArrayCoordsFromY(y);
+  G4double zarr = array->ArrayCoordsFromY(z);
+  
+  G4double x0 = floor(xarr);
+  G4double x1 = ceil(xarr);
+  G4double y0 = floor(yarr);
+  G4double y1 = ceil(yarr);
+  G4double z0 = floor(zarr);
+  G4double z1 = floor(zarr);
+
+  // The fractional part will give us the normalised distance between x0 and the point.
+  // ie if x were in spatial coords, xDist = (x-x0)/(x1-x0) where x is an arbitrary point
+  // between lattice points x0 and x1.
+  G4double xDist = xarr - x0;
+  G4double yDist = yarr - y0;
+  G4double zDist = zarr - z0;
+
+  // Get the values we requires - indices are x,y,z
+  BDSFieldValue C000 = array->GetConst(x0,y0,z0);
+  BDSFieldValue C001 = array->GetConst(x0,y0,z1);
+  BDSFieldValue C010 = array->GetConst(x0,y1,z0);
+  BDSFieldValue C011 = array->GetConst(x0,y1,z1);
+  BDSFieldValue C100 = array->GetConst(x1,y0,z0);
+  BDSFieldValue C101 = array->GetConst(x1,y0,z1);
+  BDSFieldValue C110 = array->GetConst(x1,y1,z0);
+  BDSFieldValue C111 = array->GetConst(x1,y1,z1);
+
+  // Interpolate along x
+  BDSFieldValue C00  = Interpolate1D(C000, C100, xDist);
+  BDSFieldValue C01  = Interpolate1D(C001, C101, xDist);
+  BDSFieldValue C10  = Interpolate1D(C010, C110, xDist);
+  BDSFieldValue C11  = Interpolate1D(C011, C111, xDist);
+
+  // Interpolate along y
+  BDSFieldValue C0   = Interpolate1D(C00, C10, yDist);
+  BDSFieldValue C1   = Interpolate1D(C01, C11, yDist);
+
+  // Interpolate along z
+  BDSFieldValue C    = Interpolate1D(C0, C1, zDist);
+
+  return C;
+}
