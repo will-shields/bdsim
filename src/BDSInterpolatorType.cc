916c414d76e195bd1ba83985b44c9b62cd492b51
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-10-20 00:09:46 +0100

    Update integrator type reflexivity.
    Former-commit-id: d8ead5a50ee200e5276cad3ee3635631c335418a

--- src/BDSInterpolatorType.cc
+++ src/BDSInterpolatorType.cc
@@ -10,25 +10,35 @@
 template<>
 std::map<BDSInterpolatorType, std::string>* BDSInterpolatorType::dictionary =
   new std::map<BDSInterpolatorType, std::string> ({
+      {BDSInterpolatorType::nearest2d,  "nearest1d"},
       {BDSInterpolatorType::nearest2d,  "nearest2d"},
-      {BDSInterpolatorType::linear2d,   "linear2d"},
-      {BDSInterpolatorType::cubic2d,    "cubic2d"},
       {BDSInterpolatorType::nearest3d,  "nearest3d"},
-      {BDSInterpolatorType::linear3d,   "linear3d"},
       {BDSInterpolatorType::nearest4d,  "nearest4d"},
-      {BDSInterpolatorType::linear4d,   "linear4d"}
+      {BDSInterpolatorType::linear2d,   "linear1d"},
+      {BDSInterpolatorType::linear2d,   "linear2d"},
+      {BDSInterpolatorType::linear3d,   "linear3d"},
+      {BDSInterpolatorType::linear4d,   "linear4d"},
+      {BDSInterpolatorType::cubic1d,    "cubic1d"},
+      {BDSInterpolatorType::cubic2d,    "cubic2d"},
+      {BDSInterpolatorType::cubic3d,    "cubic3d"},
+      {BDSInterpolatorType::cubic4d,    "cubic4d"}
     });
 
 BDSInterpolatorType BDS::DetermineInterpolatorType(G4String interpolatorType)
 {
   std::map<G4String, BDSInterpolatorType> types;
+  types["nearest1d"] = BDSInterpolatorType::nearest1d;
   types["nearest2d"] = BDSInterpolatorType::nearest2d;
-  types["linear2d"]  = BDSInterpolatorType::linear2d;
-  types["cubic2d"]   = BDSInterpolatorType::cubic2d;
   types["nearest3d"] = BDSInterpolatorType::nearest3d;
-  types["linear3d"]  = BDSInterpolatorType::linear3d;
   types["nearest4d"] = BDSInterpolatorType::nearest4d;
+  types["linear1d"]  = BDSInterpolatorType::linear1d;
+  types["linear2d"]  = BDSInterpolatorType::linear2d;
+  types["linear3d"]  = BDSInterpolatorType::linear3d;
   types["linear4d"]  = BDSInterpolatorType::linear4d;
+  types["cubic1d"]   = BDSInterpolatorType::cubic1d;
+  types["cubic2d"]   = BDSInterpolatorType::cubic2d;
+  types["cubic3d"]   = BDSInterpolatorType::cubic3d;
+  types["cubic4d"]   = BDSInterpolatorType::cubic4d;
 
   interpolatorType.toLower();
 
