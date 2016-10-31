2585d2ab024dc2affee660102d45f1e7748fcb6c
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-10-31 16:05:01 +0100

    Implement field querying in inteprolator program for all dimensions plus fixes for units.
    Former-commit-id: 01fe3522c011969c8d8c84f468df79f4229ed52b

--- interpolator/BDSIQuery.cc
+++ interpolator/BDSIQuery.cc
@@ -42,7 +42,41 @@ void BDSI::Query(G4Field* field,
 }
 
 void BDSI::Query1D(G4Field* field, const GMAD::Query& params, const BDSFieldClassType type)
-{;}
+{
+  G4double xmin = G4double(params.xmin*CLHEP::m);
+  G4double xmax = G4double(params.xmax*CLHEP::m);
+  G4int    nX   = G4int(params.nx);
+  G4String outputName = G4String(params.outfile);
+  
+  G4cout << "Querying " << outputName << G4endl;
+  
+  double xStep = (xmax - xmin) / (G4double)nX;
+  
+  std::ofstream ofile;
+  ofile.open(outputName+".dat");
+
+  ofile << "nx> "   << nX << "\n";
+  ofile << "xmin> " << xmin/CLHEP::m << "\n";
+  ofile << "xmax> " << xmax/CLHEP::m << "\n";
+  ofile << "! X     Fx    Fy    Fz\n";
+  
+  G4double totalN = (G4double)nX;
+
+  G4int i = 0;
+  for (G4double x = xmin; x < xmax; x += xStep)
+    {
+      G4double percentage = ((G4double)i / totalN) *100;
+      std::cout << "\r" << floor(percentage) << "%";
+      G4double result[6] = {0,0,0,0,0,0};
+      G4double coords[4] = {x,0,0,0};
+      field->GetFieldValue(coords, result);
+      WriteOut(ofile, 2, coords, result, type);
+      i++;
+    }
+  
+  ofile.close();
+  std::cout << std::endl;
+}
 
 void BDSI::Query2D(G4Field* field, const GMAD::Query& params, const BDSFieldClassType type)
 {
@@ -62,12 +96,12 @@ void BDSI::Query2D(G4Field* field, const GMAD::Query& params, const BDSFieldClas
   std::ofstream ofile;
   ofile.open(outputName+".dat");
 
-  ofile << "nx> "   << nX   << "\n";
-  ofile << "ny> "   << nY   << "\n";
-  ofile << "xmin> " << xmin << "\n";
-  ofile << "xmax> " << xmax << "\n";
-  ofile << "ymin> " << ymin << "\n";
-  ofile << "ymax> " << ymax << "\n";
+  ofile << "nx> "   << nX << "\n";
+  ofile << "ny> "   << nY << "\n";
+  ofile << "xmin> " << xmin/CLHEP::m << "\n";
+  ofile << "xmax> " << xmax/CLHEP::m << "\n";
+  ofile << "ymin> " << ymin/CLHEP::m << "\n";
+  ofile << "ymax> " << ymax/CLHEP::m << "\n";
   ofile << "! X     Y     Fx    Fy    Fz\n";
   
   G4double totalN = (G4double)nX * (G4double)nY;
@@ -90,6 +124,127 @@ void BDSI::Query2D(G4Field* field, const GMAD::Query& params, const BDSFieldClas
   std::cout << std::endl;
 }
 
+void BDSI::Query3D(G4Field* field, const GMAD::Query& params, const BDSFieldClassType type)
+{
+  G4double xmin = G4double(params.xmin*CLHEP::m);
+  G4double xmax = G4double(params.xmax*CLHEP::m);
+  G4double ymin = G4double(params.ymin*CLHEP::m);
+  G4double ymax = G4double(params.ymax*CLHEP::m);
+  G4double zmin = G4double(params.zmin*CLHEP::m);
+  G4double zmax = G4double(params.zmax*CLHEP::m);
+  G4int    nX   = G4int(params.nx);
+  G4int    nY   = G4int(params.ny);
+  G4int    nZ   = G4int(params.nz);
+  G4String outputName = G4String(params.outfile);
+  
+  G4cout << "Querying " << outputName << G4endl;
+  
+  double xStep = (xmax - xmin) / (G4double)nX;
+  double yStep = (ymax - ymin) / (G4double)nY;
+  double zStep = (ymax - ymin) / (G4double)nZ;
+  
+  std::ofstream ofile;
+  ofile.open(outputName+".dat");
+
+  ofile << "nx> "   << nX << "\n";
+  ofile << "ny> "   << nY << "\n";
+  ofile << "nz> "   << nZ << "\n";
+  ofile << "xmin> " << xmin/CLHEP::m << "\n";
+  ofile << "xmax> " << xmax/CLHEP::m << "\n";
+  ofile << "ymin> " << ymin/CLHEP::m << "\n";
+  ofile << "ymax> " << ymax/CLHEP::m << "\n";
+  ofile << "zmin> " << zmin/CLHEP::m << "\n";
+  ofile << "zmax> " << zmax/CLHEP::m << "\n";
+  ofile << "! X     Y     Z     Fx    Fy    Fz\n";
+  
+  G4double totalN = (G4double)nX * (G4double)nY * (G4double)nZ;
+
+  G4int i = 0;
+  for (G4double z = zmin; z < zmax; z += zStep)
+    {
+      G4double percentage = ((G4double)i / totalN) *100;
+      std::cout << "\r" << floor(percentage) << "%";
+      for (G4double y = ymin; y < ymax; y += yStep)
+	{
+	  for (G4double x = xmin; x < xmax; x += xStep)
+	    {
+	      G4double result[6] = {0,0,0,0,0,0};
+	      G4double coords[4] = {x,y,z,0};
+	      field->GetFieldValue(coords, result);
+	      WriteOut(ofile, 2, coords, result, type);
+	      i++;
+	    }
+	}
+    }
+  ofile.close();
+  std::cout << std::endl;
+}
+
+void BDSI::Query4D(G4Field* field, const GMAD::Query& params, const BDSFieldClassType type)
+{
+  G4double xmin = G4double(params.xmin*CLHEP::m);
+  G4double xmax = G4double(params.xmax*CLHEP::m);
+  G4double ymin = G4double(params.ymin*CLHEP::m);
+  G4double ymax = G4double(params.ymax*CLHEP::m);
+  G4double zmin = G4double(params.zmin*CLHEP::m);
+  G4double zmax = G4double(params.zmax*CLHEP::m);
+  G4double tmin = G4double(params.zmin*CLHEP::s);
+  G4double tmax = G4double(params.zmax*CLHEP::s);
+  G4int    nX   = G4int(params.nx);
+  G4int    nY   = G4int(params.ny);
+  G4int    nZ   = G4int(params.nz);
+  G4int    nT   = G4int(params.nz);
+  G4String outputName = G4String(params.outfile);
+  
+  G4cout << "Querying " << outputName << G4endl;
+  
+  double xStep = (xmax - xmin) / (G4double)nX;
+  double yStep = (ymax - ymin) / (G4double)nY;
+  double zStep = (ymax - ymin) / (G4double)nZ;
+  double tStep = (tmax - tmin) / (G4double)nT;
+  
+  std::ofstream ofile;
+  ofile.open(outputName+".dat");
+
+  ofile << "nx> "   << nX << "\n";
+  ofile << "ny> "   << nY << "\n";
+  ofile << "nz> "   << nZ << "\n";
+  ofile << "xmin> " << xmin/CLHEP::m << "\n";
+  ofile << "xmax> " << xmax/CLHEP::m << "\n";
+  ofile << "ymin> " << ymin/CLHEP::m << "\n";
+  ofile << "ymax> " << ymax/CLHEP::m << "\n";
+  ofile << "zmin> " << zmin/CLHEP::m << "\n";
+  ofile << "zmax> " << zmax/CLHEP::m << "\n";
+  ofile << "tmin> " << tmin/CLHEP::s << "\n";
+  ofile << "tmax> " << tmax/CLHEP::s << "\n";
+  ofile << "! X     Y     Z     Fx    Fy    Fz\n";
+  
+  G4double totalN = (G4double)nX * (G4double)nY * (G4double)nZ;
+
+  G4int i = 0;
+  for (G4double t = tmin; t < tmax; t += tStep)
+    {
+      G4double percentage = ((G4double)i / totalN) *100;
+      std::cout << "\r" << floor(percentage) << "%";
+      for (G4double z = zmin; z < zmax; z += zStep)
+	{
+	  for (G4double y = ymin; y < ymax; y += yStep)
+	    {
+	      for (G4double x = xmin; x < xmax; x += xStep)
+		{
+		  G4double result[6] = {0,0,0,0,0,0};
+		  G4double coords[4] = {x,y,z,t};
+		  field->GetFieldValue(coords, result);
+		  WriteOut(ofile, 2, coords, result, type);
+		  i++;
+		}
+	    }
+	}
+    }
+  ofile.close();
+  std::cout << std::endl;
+}
+
 void BDSI::WriteOut(std::ofstream& out,
 		    const G4int    nDim,
 		    const G4double coords[4],
@@ -98,7 +253,7 @@ void BDSI::WriteOut(std::ofstream& out,
 {
   // write coordinates
   for (G4int i = 0; i < nDim; i++)
-	  {out << coords[i] << "\t";}
+    {out << coords[i]/CLHEP::m << "\t";}
 
   // write field components
   switch (type.underlying())
@@ -131,9 +286,3 @@ void BDSI::WriteOut(std::ofstream& out,
       {break;}
     }
 }
-
-void BDSI::Query3D(G4Field* field, const GMAD::Query& params, const BDSFieldClassType type)
-{;}
-
-void BDSI::Query4D(G4Field* field, const GMAD::Query& params, const BDSFieldClassType type)
-{;}
