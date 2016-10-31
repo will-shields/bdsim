c8112821f21bc3161dc2fbe7e604e98886c5c2cf
William Shields <williamshields@dhcp232.pp.rhul.ac.uk>
2016-10-31 16:26:31 +0000

    Fix for multipole tracking bug.
    Former-commit-id: 5692410ae6854e6e96b7f38cdb23eb62ffbe9f6c

--- src/BDSIntegratorMultipole.cc
+++ src/BDSIntegratorMultipole.cc
@@ -170,7 +170,7 @@ void BDSIntegratorMultipoleThin::AdvanceHelix(const G4double yIn[],
   LocalRp.setZ(zp1);
 
   BDSStep globalPosDir = ConvertToGlobalStep(LocalR, LocalRp, false);
-  GlobalP = globalPosDir.PreStepPoint();
+  GlobalR = globalPosDir.PreStepPoint();
   GlobalP = globalPosDir.PostStepPoint();	
   // TBC - this normalisation seemed to be missing originally - is this correct?
   //GlobalP*=InitMag; // multiply the unit direction by magnitude to get momentum
