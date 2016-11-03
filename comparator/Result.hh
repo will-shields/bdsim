c0bed932269e0f19fb4ff6e50c29bffbbe8a5afa
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-11-03 15:49:09 +0100

    Alternate constructor for Result to avoid duplication of member initialisation throughout code.
    Former-commit-id: 53ec116295f963880ef38f545da812672c76d4ec

--- comparator/Result.hh
+++ comparator/Result.hh
@@ -18,6 +18,18 @@
 class Result
 { 
 public:
+  Result(std::string nameIn, std::string objTypeIn):
+    name(nameIn),
+    objtype(objTypeIn),
+    passed(true)
+  {;}
+
+  Result():
+    name(""),
+    objtype(""),
+    passed(true)
+  {;}
+  
   std::string name;    ///< Name of object being compared in files.
   std::string objtype; ///< Name of class of object being compared in files.
   int         passed;  ///< Whether it passed or not.
