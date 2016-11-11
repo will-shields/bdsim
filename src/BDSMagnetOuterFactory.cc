459ab9524691ba4c6a6809d2b4e6a81f11daa3e6
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-11-11 18:10:34 +0100

    Remove excessive debug output; fix indentation; match code style with explicit braces.
    Former-commit-id: 82af2ea340de331ddf93ea5a1d990f61cd5cc6dd

--- src/BDSMagnetOuterFactory.cc
+++ src/BDSMagnetOuterFactory.cc
@@ -57,64 +57,33 @@ BDSMagnetOuterFactory::~BDSMagnetOuterFactory()
 
 BDSMagnetOuterFactoryBase* BDSMagnetOuterFactory::GetAppropriateFactory(BDSMagnetGeometryType magnetTypeIn)
 {
-  switch(magnetTypeIn.underlying()){
-    
-  case BDSMagnetGeometryType::none:
-#ifdef BDSDEBUG
-    G4cout << __METHOD_NAME__ << "'none' magnet factory (no outer geometry)" << G4endl;
-#endif
-    return BDSMagnetOuterFactoryNone::Instance();
-    break;
-  case BDSMagnetGeometryType::cylindrical:
-#ifdef BDSDEBUG
-    G4cout << __METHOD_NAME__ << "cylindrical magnet factory" << G4endl;
-#endif
-    return BDSMagnetOuterFactoryCylindrical::Instance();
-    break;
-  case BDSMagnetGeometryType::polescircular:
-#ifdef BDSDEBUG
-    G4cout << __METHOD_NAME__ << "poles with circular yoke factory" << G4endl;
-#endif
-    return BDSMagnetOuterFactoryPolesCircular::Instance();
-    break;
-  case BDSMagnetGeometryType::polessquare:
-#ifdef BDSDEBUG
-    G4cout << __METHOD_NAME__ << "poles with square yoke factory" << G4endl;
-#endif
-    return BDSMagnetOuterFactoryPolesSquare::Instance();
-    break;
-  case BDSMagnetGeometryType::polesfacet:
-#ifdef BDSDEBUG
-    G4cout << __METHOD_NAME__ << "poles with faceted yoke factory" << G4endl;
-#endif
-    return BDSMagnetOuterFactoryPolesFacet::Instance();
-    break;
-  case BDSMagnetGeometryType::polesfacetcrop:
-#ifdef BDSDEBUG
-    G4cout << __METHOD_NAME__ << "poles with faceted and cropped yoke factory" << G4endl;
-#endif
-    return BDSMagnetOuterFactoryPolesFacetCrop::Instance();
-    break;
-  case BDSMagnetGeometryType::lhcleft:
-#ifdef BDSDEBUG
-    G4cout << __METHOD_NAME__ << "LHC magnet factory - with left offset" << G4endl;
-#endif
-    return BDSMagnetOuterFactoryLHCLeft::Instance();
-    break;
-  case BDSMagnetGeometryType::lhcright:
-#ifdef BDSDEBUG
-    G4cout << __METHOD_NAME__ << "LHC magnet factory - with right offset" << G4endl;
-#endif
-    return BDSMagnetOuterFactoryLHCRight::Instance();
-    break;
-  case BDSMagnetGeometryType::external:
-    return nullptr;
-    break;
-  default:
-    G4cerr << __METHOD_NAME__ << "unknown type \"" << magnetTypeIn << G4endl;
-    exit(1);
-    break;
-  }
+  switch(magnetTypeIn.underlying())
+    {
+    case BDSMagnetGeometryType::none:
+      {return BDSMagnetOuterFactoryNone::Instance(); break;}
+    case BDSMagnetGeometryType::cylindrical:
+      {return BDSMagnetOuterFactoryCylindrical::Instance(); break;}
+    case BDSMagnetGeometryType::polescircular:
+      {return BDSMagnetOuterFactoryPolesCircular::Instance(); break;}
+    case BDSMagnetGeometryType::polessquare:
+      {return BDSMagnetOuterFactoryPolesSquare::Instance(); break;}
+    case BDSMagnetGeometryType::polesfacet:
+      {return BDSMagnetOuterFactoryPolesFacet::Instance(); break;}
+    case BDSMagnetGeometryType::polesfacetcrop:
+      {return BDSMagnetOuterFactoryPolesFacetCrop::Instance(); break;}
+    case BDSMagnetGeometryType::lhcleft:
+      {return BDSMagnetOuterFactoryLHCLeft::Instance(); break;}
+    case BDSMagnetGeometryType::lhcright:
+      {return BDSMagnetOuterFactoryLHCRight::Instance(); break;}
+    case BDSMagnetGeometryType::external:
+      {return nullptr; break;}
+    default:
+      {
+	G4cerr << __METHOD_NAME__ << "unknown type \"" << magnetTypeIn << G4endl;
+	exit(1);
+	break;
+      }
+    }
 }
 
 BDSMagnetOuter* BDSMagnetOuterFactory::CreateMagnetOuter(BDSMagnetType       magnetType,
