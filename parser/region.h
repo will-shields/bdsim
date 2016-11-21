d15ac61899de9b7551d5749837c132a3853a6140
Jochem Snuverink <jochem.snuverink@psi.ch>
2016-11-21 12:30:50 +0100

    improve parser output messages somewhat
    Former-commit-id: c83ea9466fd0df24f1f29e8573958a639a77182a

--- parser/region.h
+++ parser/region.h
@@ -42,14 +42,14 @@ namespace GMAD
     void Region::set_value(std::string property, T value)
     {
 #ifdef BDSDEBUG
-      std::cout << "parser> Setting value " << std::setw(25) << std::left << property << value << std::endl;
+      std::cout << "region> Setting value " << std::setw(25) << std::left << property << value << std::endl;
 #endif
       // member method can throw runtime_error, catch and exit gracefully
       try {
 	set(this,property,value);
       }
       catch(std::runtime_error) {
-	std::cerr << "Error: parser> unknown option \"" << property << "\" with value " << value  << std::endl;
+	std::cerr << "Error: region> unknown option \"" << property << "\" with value " << value  << std::endl;
 	exit(1);
       }
     }
