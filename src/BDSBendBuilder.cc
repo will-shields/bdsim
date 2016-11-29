625b7cfbb95f5d878645f82a6d7eecbc3738b635
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-11-29 17:55:41 +0900

    Fix semi-angle from merge.
    Former-commit-id: 070435b23af6a8028f887ce877336deaa225ecac

--- src/BDSBendBuilder.cc
+++ src/BDSBendBuilder.cc
@@ -444,9 +444,9 @@ BDSMagnet* BDS::BuildSBend(const Element*     element,
   
   //calculate their angles and length
   G4double length     = element->l*CLHEP::m;
-  G4double semiangle  = -(*st)["angle"];
+  G4double semiangle  = -(*st)["angle"] / (G4double) nSBends;
   G4double semilength = length / (G4double) nSBends;
-  G4double rho        = element->l*CLHEP::m/ (semiangle * nSBends);
+  G4double rho        = element->l*CLHEP::m/ (semiangle * nSBends); // semi-rho
   
   // angle increment for sbend elements with poleface rotation(s) specified
   G4double deltastart = -element->e1/(0.5*(nSBends-1));
