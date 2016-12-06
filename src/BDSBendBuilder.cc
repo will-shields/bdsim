78d9ee6b5fb8bb6ddeb8777da92d21ca2f883587
Jochem Snuverink <jochem.snuverink@psi.ch>
2016-12-06 16:01:01 +0100

    bendingradius inversely proportional to charge
    Former-commit-id: df7773627ee1c4ef0859df33499ccaf5a9c19827

--- src/BDSBendBuilder.cc
+++ src/BDSBendBuilder.cc
@@ -261,8 +261,8 @@ BDSLine* BDS::BuildRBendLine(const Element*          element,
   G4double     arcLength = (*st)["length"];
   if (BDS::IsFinite(angle))
     {// avoid bad calculations for zero angle
-      // Here we need bending radius to be in correct global carteasian convention, hence -ve.
-      bendingRadius = -charge * brho / (*st)["field"];
+      // Here we need bending radius to be in correct global cartesian convention, hence -ve.
+      bendingRadius = - brho / (*st)["field"] / charge;
       arcLength = std::abs(bendingRadius * angle); // arc length
     }
   const G4String name = element->name;
