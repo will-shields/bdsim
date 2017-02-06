6b6c1c8506d86be5950ab946a40dc79ea3ed50ac
William Shields <williamshields@dhcp232.pp.rhul.ac.uk>
2017-02-06 17:37:14 +0000

    Shared functions class for tracking test files.
    Former-commit-id: d8fc1dcf07772f3398223bfcf5bb5c72b975a713

--- examples/features/components/dipoles/trackingTests.py
+++ examples/features/components/dipoles/trackingTests.py
@@ -1,5 +1,12 @@
 import numpy as _np
 from scipy import constants as _con
+import os as _os
+import glob as _glob
+from pybdsim import Beam as _Beam
+from pybdsim import Options as _Options
+from pybdsim import Writer as _Writer
+from pybdsim import Builder as _Builder
+
 
 elements = ['drift',
             'rbend',
@@ -136,44 +143,135 @@ class params():
         for key in self.numFiles:
             self.totalFiles += self.numFiles[key]
 
-def calcBField(length,angle,energy,particle):
-    #Calculate the magnetic field for a dipole
-    if angle == 0:
-        return 0
-    else:
-        energies = calcEnergy(energy,particle)
-        rho = length / angle
-        b = energies['brho'] / rho
-        return b
+class _functions(params):
 
-def calcEnergy(total_energy,particle='e-'):
-    #Calculate the energy & momentum of a proton and electron at a given total energy.
-    eMass = _con.electron_mass * _con.c**2 / _con.e / 1e9
-    pMass = _con.proton_mass * _con.c**2 / _con.e / 1e9
+    def calcBField(self,length,angle,energy,particle):
+        #Calculate the magnetic field for a dipole
+        if angle == 0:
+            return 0
+        else:
+            energies = self.calcEnergy(energy,particle)
+            rho = length / angle
+            b = energies['brho'] / rho
+            return b
 
-    if particle == 'proton' or particle == 'p':
-        protonEnergy = total_energy
-        protonKinetic = total_energy - pMass
-        protonMomentum = _np.sqrt(total_energy**2 - pMass**2)
-        brho = 3.335640951981521 * protonMomentum
-        electronMomentum = protonMomentum
-        electronEnergy = _np.sqrt(electronMomentum**2 + eMass**2)
-        electronKinetic = electronEnergy - eMass
-    elif particle == 'e' or particle == 'e-':
-        electronEnergy = total_energy
-        electronKinetic = total_energy - eMass
-        electronMomentum = _np.sqrt(total_energy**2 - eMass**2)
-        brho = 3.335640951981521 * electronMomentum
-        protonMomentum = electronMomentum
-        protonEnergy = _np.sqrt(protonMomentum**2 + pMass**2)
-        protonKinetic = protonEnergy - pMass
+    def calcEnergy(self,total_energy,particle='e-'):
+        #Calculate the energy & momentum of a proton and electron at a given total energy.
+        eMass = _con.electron_mass * _con.c**2 / _con.e / 1e9
+        pMass = _con.proton_mass * _con.c**2 / _con.e / 1e9
 
-    res={'e-'     : {'KE' : electronKinetic,
-                     'TE' : electronEnergy,
-                     'P'  : electronMomentum},
-         'proton' : {'KE' : electronKinetic,
-                     'TE' : electronEnergy,
-                     'P'  : electronMomentum},
-         'brho'   : brho}
-    return res
+        if particle == 'proton' or particle == 'p':
+            protonEnergy = total_energy
+            protonKinetic = total_energy - pMass
+            protonMomentum = _np.sqrt(total_energy**2 - pMass**2)
+            brho = 3.335640951981521 * protonMomentum
+            electronMomentum = protonMomentum
+            electronEnergy = _np.sqrt(electronMomentum**2 + eMass**2)
+            electronKinetic = electronEnergy - eMass
+        elif particle == 'e' or particle == 'e-':
+            electronEnergy = total_energy
+            electronKinetic = total_energy - eMass
+            electronMomentum = _np.sqrt(total_energy**2 - eMass**2)
+            brho = 3.335640951981521 * electronMomentum
+            protonMomentum = electronMomentum
+            protonEnergy = _np.sqrt(protonMomentum**2 + pMass**2)
+            protonKinetic = protonEnergy - pMass
+
+        res={'e-'     : {'KE' : electronKinetic,
+                         'TE' : electronEnergy,
+                         'P'  : electronMomentum},
+             'proton' : {'KE' : electronKinetic,
+                         'TE' : electronEnergy,
+                         'P'  : electronMomentum},
+             'brho'   : brho}
+        return res
+
+    def _getBeam(self,particle,energy):
+        ''' Function to create and return a pybdsim.Beam.Beam instance.
+            The beam is set to PTC.'''
+        beam = _Beam.Beam()
+        beam.SetParticleType(particletype=particle)
+        beam.SetEnergy(energy)
+        beam.SetDistributionType(distrtype='ptc')
+        beam._SetSigmaE(sigmae=0)
+        return beam
+
+    def _writeCMakeLists(self):
+        ''' Function to write a CMakeLists.txt file with simple_testing for
+            a test file and add_subdirectory where appropriate.'''
+        
+        def getCMakeString(filename):
+            fname = filename.split('.gmad')[0]
+            cmstring = 'simple_testing(' + fname + ' "--file=' + filename + '" '
+            cmstring += '${OVERLAP_CHECK})\r\n'
+            return cmstring
+
+        #cmakelist file for main tracking test directory
+        allTestsCMakeList = open('CMakeLists.txt','w')
+        
+        for element in elements:
+            allTestsCMakeList.write('add_subdirectory('+element+')\r\n')
+            #cmakelist file for this element.
+            elementCMakeList = open('CMakeLists.txt','w')
+            
+            #if tests split into multiple sub-dirs, create cmakelist file in those too.
+            testDirs = _glob.glob('*/')
+            if len(testDirs) > 0:
+                for dir in testDirs:
+                    elementCMakeList.write('add_subdirectory('+dir+')\r\n')
+                    _os.chdir(dir)
+                    
+                    testFiles = _glob.glob('*.gmad')
+                    testCMakeList = open('CMakeLists.txt','w')
+                    for file in testFiles:
+                        testCMakeList.write(getCMakeString(file))
+                    testCMakeList.close()
+                    _os.chdir('../')
+                elementCMakeList.close()
+            else:
+                testFiles = _glob.glob('*.gmad')
+                for file in testFiles:
+                    elementCMakeList.write(getCMakeString(file))
+                elementCMakeList.close()
+        allTestsCMakeList.close()
+
+    def _mkdirs(self):
+        ''' Function to make the directories that the tests will be written in.
+            If the number of tests for a given element is > 1000, the files will
+            be split into multiple subdirectories. '''
+        for element in elements:
+            if not _os.path.exists(element):
+                _os.system("mkdir -p " + element)
+            if self.numFiles[element] > 1000:
+                _os.chdir(element)
+                numdirs = self.numFiles[element] - _np.mod(self.numFiles[element],1000)
+                dirs = _np.linspace(0,numdirs,(numdirs/1000)+1)
+                for dir in dirs:
+                    _os.system("mkdir -p " + _np.str(_np.int(dir)))
+                _os.chdir('../')
+
+    def _writeToDisk(self,element,filename,machine):
+        ''' Function that writes the test machine to disk.'''
+        self._testfileNum += 1
+        
+        writer = _Writer.Writer()
+        writer.Components.WriteInMain()
+        writer.Sequence.WriteInMain()
+        writer.Samplers.WriteInMain()
+        writer.Beam.WriteInMain()
+        _os.chdir(element)
+        #set beam and options path depending on file location.
+        if self.numFiles[element] > 1000:
+            dir = _np.str(_np.int(self._testfileNum - _np.mod(self._testfileNum,1000)))
+            _os.chdir(dir)
+            machine.beam._SetDistribFileName('../../trackingTestBeam.madx')
+            writer.Options.CallExternalFile('../../trackingTestOptions.gmad')
+            writer.WriteMachine(machine, filename,verbose = False)
+            _os.chdir('../')
+        else:
+            machine.beam._SetDistribFileName('../trackingTestBeam.madx')
+            writer.Options.CallExternalFile('../trackingTestOptions.gmad')
+            writer.WriteMachine(machine, filename,verbose = False)
+        _os.chdir('../')
+
 
