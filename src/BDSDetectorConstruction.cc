b4b12b0b1b24b3c2758139f4794228736fc47091
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2017-02-13 23:15:31 +0000

    Go back to 1 to 1 curvilinear beam line.
    Former-commit-id: 03c82a7ec5112432b6f01aafba66ba272d423969

--- src/BDSDetectorConstruction.cc
+++ src/BDSDetectorConstruction.cc
@@ -267,7 +267,7 @@ void BDSDetectorConstruction::BuildBeamline()
 
   // Build curvilinear geometry w.r.t. beam line.
   BDSCurvilinearBuilder* clBuilder = new BDSCurvilinearBuilder();
-  BDSBeamline* clBeamline = clBuilder->BuildCurvilinearBeamLine(beamline);
+  BDSBeamline* clBeamline = clBuilder->BuildCurvilinearBeamLine1To1(beamline);
   delete clBuilder;
   
   // register the beamline in the holder class for the full model
