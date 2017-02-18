c0779733af6edde0af8707f2dcc1d72db7975ac8
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2017-02-18 17:46:51 +0000

    Moved process types to include
    Former-commit-id: 6423ba164fad0c2c23432e10fafc74030355d17d

--- src/BDSOutputROOTEventTrajectory.cc
+++ src/BDSOutputROOTEventTrajectory.cc
@@ -8,68 +8,6 @@
 #include "BDSTrajectory.hh"
 #endif
 
-#if 0
-  0  fNotDefined,
-  1  fTransportation,
-    91 TRANSPORTATION,
-    92 COUPLED_TRANSPORTATION
-  2  fElectromagnetic,
-    1  fCoulombScattering, 
-    2  fIonisation, 
-    3  fBremsstrahlung, 
-    4  fPairProdByCharged,
-    5  fAnnihilation, 
-    6  fAnnihilationToMuMu,
-    7  fAnnihilationToHadrons,
-    8  fNuclearStopping,
-    10 fMultipleScattering,   
-    11 fRayleigh,
-    12 fPhotoElectricEffect,
-    13 fComptonScattering,
-    14 fGammaConversion,
-    15 fGammaConversionToMuMu,
-    21 fCerenkov,
-    22 fScintillation,
-    23 fSynchrotronRadiation,
-    24 fTransitionRadiation 
-  3  fOptical,             
-    0 kCerenkov,      
-    1 kScintillation, 
-    2 kAbsorption,    
-    3 kRayleigh,      
-    4 kMieHG,         
-    5 kBoundary,      
-    6 kWLS,           
-    7 kNoProcess      
-  4  fHadronic,
-    111 fHadronElastic,
-    121 fHadronInelastic,
-    131 fCapture,
-    141 fFission,
-    151 fHadronAtRest,
-    152 fLeptonAtRest,
-    161 fChargeExchange,
-    210 fRadioactiveDecay
-  5  fPhotolepton_hadron,
-  6  fDecay,
-    201 DECAY = 201,
-    202 DECAY_WithSpin,
-    203 DECAY_PionMakeSpin,
-    210 DECAY_Radioactive,
-    211 DECAY_Unknown,
-    231 DECAY_External = 231
-  7  fGeneral,
-    401 STEP_LIMITER,
-    402 USER_SPECIAL_CUTS,
-    403 NEUTRON_KILLER
-  8  fParameterisation,
-  9  fUserDefined,
-  10 fParallel,
-    491 SBLN_UNKNOWN! 
-  11 fPhonon,
-  12 fUCN
-#endif
-
 ClassImp(BDSOutputROOTEventTrajectory)
 
 BDSOutputROOTEventTrajectory::BDSOutputROOTEventTrajectory():
