2db83d5acc0b1b8049d28c5dbce9b7beeff93a44
William Shields <williamshields@MacBook-Pro-3.local>
2017-02-20 17:49:47 +0000

    Further spacing fixes.
    Former-commit-id: 6230a180c4113a1be531c8cbf369434de68d413a

--- examples/features/testing/pybdsimTest.py
+++ examples/features/testing/pybdsimTest.py
@@ -13,23 +13,25 @@ from pybdsim import Writer as _pybdsimWriter
 from pybdsim import Options as _options
 
 # data type with multiple entries that can be handled by the functions.
-multiEntryTypes = [tuple,list,_np.ndarray]
+multiEntryTypes = [tuple, list, _np.ndarray]
 
 GlobalData = Globals.Globals()
 
 returnCode = {'SUCCESS'          : 0,
               'FAILED'           : 1,
               'INCORRECT_ARGS'   : 2,
-              'FILE_NOT_FOUND'   : 3, # i.e BDSIM did not generate anything
+              'FILE_NOT_FOUND'   : 3,  # i.e BDSIM did not generate anything
               'OVERLAPS'         : 4,
               'STUCK_PARTICLE'   : 5,
               'TRACKING_WARNING' : 6,
               }
 
+
 class Timing:
     def __init__(self):
         self.bdsimTimes = []
         self.comparatorTimes = []
+
 
 def Run(inputDict):
     """ Generate the rootevent output for a given gmad file.
@@ -54,7 +56,7 @@ def Run(inputDict):
     # os.system used as subprocess.call has difficulty with the arguments for some reason.
     bdsimCommand = GlobalData._bdsimExecutable + " --file=" + inputfile + " --output=rootevent --outfile="
     bdsimCommand += outputfile + " --batch"
-    _os.system(bdsimCommand + ' > '+ bdsimLogFile)
+    _os.system(bdsimCommand + ' > ' + bdsimLogFile)
     bdsimTime = _np.float(time.time() - t)
     inputDict['bdsimTime'] = bdsimTime
 
@@ -103,6 +105,7 @@ def Run(inputDict):
             inputDict['Code'] = returnCode['FAILED']  # False
     return inputDict
 
+
 class Test(dict):
     def __init__(self, component, energy, particle, phaseSpace=None,
                  useDefaults=False, testRobustNess=False, eFieldMap='',
@@ -116,21 +119,21 @@ class Test(dict):
         
         # Initialise parameters for the component as empty lists (or defaults) and dynamically
         # create setter functions for those component parameters.
-        if isinstance(component,_np.str) and (GlobalData.components.__contains__(component)):
+        if isinstance(component, _np.str) and (GlobalData.components.__contains__(component)):
             self.Component = component
             for param in GlobalData.hasParams[component]:
                 if not useDefaults:
                     self[param] = []
                 else:
-                    self.__Update(param,GlobalData.paramValues[param])
+                    self.__Update(param, GlobalData.paramValues[param])
                 funcName = "Set" + _string.capitalize(param)
-                setattr(self,funcName,self.__createSetterFunction(name=param))
+                setattr(self, funcName, self.__createSetterFunction(name=param))
             
             # set the parameter values to the kwarg values if defaults are not specified
             if not useDefaults:
-                for key,value in kwargs.iteritems():
+                for key, value in kwargs.iteritems():
                     if GlobalData.hasParams[component].__contains__(key):
-                        self.__Update(key,value)
+                        self.__Update(key, value)
         else:
             raise ValueError("Unknown component type.")
         self.SetEnergy(energy)
@@ -141,7 +144,7 @@ class Test(dict):
         """ Function to return function template for updating component parameters.
             """
         def function_template(value):
-            self.__Update(name,value)
+            self.__Update(name, value)
         return function_template
 
     def __Update(self, parameter, values):
@@ -183,20 +186,20 @@ class Test(dict):
     def _UpdateTestCombinations(self):
         # update number of parameter combinations
         numcomponentVariations = 1
-        for key,values in self.iteritems():
+        for key, values in self.iteritems():
             if key == 'knl' or key == 'ksl':
                 numcomponentVariations *= (len(GlobalData.k1l)*len(values))
             elif len(values) != 0:
                 numcomponentVariations *= len(values)
         if (self.Component == 'rbend' or self.Component == 'sbend') and self._useDefaults:
             numcomponentVariations /= len(GlobalData.paramValues['field'])
-            numcomponentVariations *= 2 #angle or field
+            numcomponentVariations *= 2  # angle or field
         self._numFiles = numcomponentVariations
 
     def __repr__(self):
-        s  = 'pybdsimTest.testParameters.Test instance.\r\n'
-        s += 'This is a test for a/an ' +self.Component+ ' with '+ self.Particle
-        s += ' at an energy of '+ _np.str(self.Energy)+' GeV.\r\n'
+        s = 'pybdsimTest.testParameters.Test instance.\r\n'
+        s += 'This is a test for a/an ' + self.Component + ' with ' + self.Particle
+        s += ' at an energy of ' + _np.str(self.Energy) + ' GeV.\r\n'
         s += 'The component will be test all combinations of the following parameters:\r\n'
         for param in self.keys():
             s += '  '+param+' : ' + self[param].__repr__()+'\r\n'
@@ -206,7 +209,7 @@ class Test(dict):
         """ Set test beam energy.
             """
         try:
-            setattr(self,"Energy",_np.float(energy))
+            setattr(self, "Energy", _np.float(energy))
         except TypeError:
             raise ValueError("Unknown data type.")
 
@@ -214,7 +217,7 @@ class Test(dict):
         """ Set test beam particle.
             """
         if GlobalData.particles.__contains__(particle):
-            setattr(self,"Particle",particle)
+            setattr(self, "Particle", particle)
         else:
             raise ValueError("Unknown particle type")
 
@@ -223,34 +226,34 @@ class Test(dict):
             a PhaseSpace instance. Entry of particle co-ordinates instead
             internally creates a PhaseSpace instance anyway."""
         if phaseSpace is not None:
-            if isinstance(phaseSpace,PhaseSpace.PhaseSpace):
-                setattr(self,"PhaseSpace",phaseSpace)
+            if isinstance(phaseSpace, PhaseSpace.PhaseSpace):
+                setattr(self, "PhaseSpace", phaseSpace)
             else:
                 raise TypeError("phaseSpace can only be a bdsimtesting.PhaseSpace.PhaseSpace instance.")
         else:
-            setattr(self,"PhaseSpace",PhaseSpace.PhaseSpace(x,px,y,py,t,pt))
+            setattr(self, "PhaseSpace", PhaseSpace.PhaseSpace(x, px, y, py, t, pt))
 
-    def AddParameter(self,parameter,values=[]):
+    def AddParameter(self, parameter, values=[]):
         if self.keys().__contains__(parameter):
             raise ValueError("Parameter is already listed as a test parameter.")
         
-        elif isinstance(parameter,_np.str):
+        elif isinstance(parameter, _np.str):
             if GlobalData.parameters.__contains__(parameter):
                 self[parameter] = []
-                funcName = "Set"+_string.capitalize(parameter)
-                setattr(self,funcName,self.__createSetterFunction(name=parameter))
-                self.__Update(parameter,values)
+                funcName = "Set" + _string.capitalize(parameter)
+                setattr(self, funcName, self.__createSetterFunction(name=parameter))
+                self.__Update(parameter, values)
             else:
-                raise ValueError("Unknown parameter type: "+parameter+".")
+                raise ValueError("Unknown parameter type: " + parameter + ".")
         else:
-            raise TypeError("Unknown data type for "+parameter)
+            raise TypeError("Unknown data type for " + parameter)
 
 
 class TestUtilities(object):
-    def __init__(self,directory=''):
-        self._tests = [] # list of test objects
+    def __init__(self, directory=''):
+        self._tests = []  # list of test objects
 
-        if not isinstance(directory,_np.str):
+        if not isinstance(directory, _np.str):
             raise TypeError("Testing directory is not a string")
         else:
             if directory == '':
@@ -311,7 +314,7 @@ class TestUtilities(object):
             _os.system("rm temp.log")
             return outputevent
 
-    def CompareOutput(self, test, originalFile='', newFile='', isSelfComparison=False):
+    def CompareOutput(self, originalFile='', newFile='', isSelfComparison=False):
         """ Compare the output file against another file. This function uses BDSIM's comparator.
             The test gmad file name is needed for updating the appropriate log files.
             If the comparison is successful:
@@ -333,7 +336,7 @@ class TestUtilities(object):
             hasPassed = False
 
         if hasPassed:
-            #_os.system("rm "+newFile)
+            # _os.system("rm "+newFile)
             if isSelfComparison:
                 _os.system("rm " + originalFile)
             _os.system("rm tempComp.log")
@@ -378,20 +381,20 @@ class TestUtilities(object):
         options.SetSamplerDiameter(3)
         options.SetWritePrimaries(True)
         Writer = _pybdsimWriter.Writer()
-        Writer.WriteOptions(options,'Tests/trackingTestOptions.gmad')
+        Writer.WriteOptions(options, 'Tests/trackingTestOptions.gmad')
+
 
 class TestSuite(TestUtilities):
-    def __init__(self,directory):
+    def __init__(self, directory):
         super(TestSuite, self).__init__(directory)
 
-    def AddTest(self,test):
+    def AddTest(self, test):
         """ Add a bdsimtesting.pybdsimTest.Test instance to the test suite.
             """
-        if not isinstance(test,Test):
+        if not isinstance(test, Test):
             raise TypeError("Test is not a bdsimtesting.pybdsimTest.Test instance")
         else:
             self._tests.append(test)
-
 
     def RunTestSuite(self, isSelfComparison=True):
         """ Run all tests in the test suite. This will generate the tests rootevent
@@ -401,17 +404,17 @@ class TestSuite(TestUtilities):
         self.WriteGmadFiles()   # Write all gmad files for all test objects.
 
         # class attributes for storing return & timing data.
-        setattr(self,'timings',Timing())
-        setattr(self,'testResults',[])
+        setattr(self, 'timings', Timing())
+        setattr(self, 'testResults', [])
 
         _os.chdir('BDSIMOutput')
         testfilesDir = '../Tests/*/'
-        componentDirs = _glob.glob(testfilesDir) # get all component dirs in the Tests dir
+        componentDirs = _glob.glob(testfilesDir)  # get all component dirs in the Tests dir
 
         initialTime = time.time()
 
         for direc in componentDirs:
-            t = time.time() # initial time
+            t = time.time()  # initial time
 
             # get all gmad files in a components dir
             testfileStr = direc+'*.gmad'
@@ -432,15 +435,20 @@ class TestSuite(TestUtilities):
             self._multiThread(testlist)
 
             # single threaded option.
-            #self._singleThread(testlist)
+            # self._singleThread(testlist)
 
-            elapsed = time.time() - t # final time
+            elapsed = time.time() - t  # final time
             print('Testing time for '+direc+' = '+_np.str(elapsed))
 
         finalTime = time.time() - initialTime
-        print('Total Testing time  = ' + _np.str(finalTime))
-        print('Average BDSIM time = '+ _np.str(_np.average(self.timings.bdsimTimes)) + " +/- " + _np.str(_np.std(self.timings.bdsimTimes))+ '.')
-        print('Average Comparator time = '+ _np.str(_np.average(self.timings.comparatorTimes)) + " +/- " + _np.str(_np.std(self.timings.comparatorTimes))+ '.')
+        totalTimeStr = 'Total Testing time  = ' + _np.str(finalTime)
+        bdsimTimestr = 'Average BDSIM time = ' + _np.str(_np.average(self.timings.bdsimTimes)) + " +/- "
+        bdsimTimestr += _np.str(_np.std(self.timings.bdsimTimes)) + '.'
+        compaTimestr = 'Average Comparator time = ' + _np.str(_np.average(self.timings.comparatorTimes)) + " +/- "
+        compaTimestr = _np.str(_np.std(self.timings.comparatorTimes))+ '.'
+        print(totalTimeStr)
+        print(bdsimTimestr)
+        print(compaTimestr)
         _os.chdir('../')
 
     def _multiThread(self, testlist):
@@ -473,17 +481,17 @@ class TestSuite(TestUtilities):
             if outputEvent is not None:
                 if isSelfComparison:
                     originalEvent = outputEvent.split('_event.root')[0] + '_event2.root'
-                    copyString = 'cp '+outputEvent+' '+originalEvent
+                    copyString = 'cp ' + outputEvent + ' ' + originalEvent
                     _os.system(copyString)
                 else:
-                    pass # This is where the comparison with the original file will occur.
-                         # TODO: figure out how to process original files that will be compared to.
+                    pass  # This is where the comparison with the original file will occur.
+                    # TODO: figure out how to process original files that will be compared to.
 
-                self.CompareOutput(test,originalEvent,outputEvent)
+                self.CompareOutput(originalEvent, outputEvent)
             else:
                 self._UpdateBDSIMFailLog(test)
                 _os.system("rm temp.log")
-                print("Output for test "+test+" was not generated.")
+                print("Output for test " + test + " was not generated.")
             comparatorTime = time.time() - compTestTime
             eleComparatorTimes.append(comparatorTime)
 
@@ -495,14 +503,14 @@ class TestSuite(TestUtilities):
         writer.SetBeamFilename('trackingTestBeam.madx')
         writer.SetOptionsFilename('trackingTestOptions.gmad')
 
-        self.numFiles =  {}
+        self.numFiles = {}
         self.componentTests = []
         for component in GlobalData.components:
             self.numFiles[component] = 0
         
         TestPS = GlobalData.BeamPhaseSpace
     
-        BeamPhaseSpace = PhaseSpace.PhaseSpace(TestPS['X'],TestPS['PX'],TestPS['Y'],TestPS['PY'],TestPS['T'],TestPS['PT'])
+        BeamPhaseSpace = PhaseSpace.PhaseSpace(TestPS['X'], TestPS['PX'], TestPS['Y'], TestPS['PY'], TestPS['T'], TestPS['PT'])
         BeamPhaseSpace.WriteToInrays('Tests/trackingTestBeam.madx')
 
         for machineInfo in GlobalData.accelerators.values():
@@ -510,7 +518,7 @@ class TestSuite(TestUtilities):
             particle = machineInfo['particle']
         
             for component in GlobalData.components:
-                componentTest = Test(component,energy,particle,BeamPhaseSpace,useDefaults=True)
+                componentTest = Test(component, energy, particle, BeamPhaseSpace, useDefaults=True)
                 self.componentTests.append(componentTest)
                 self.numFiles[component] += componentTest._numFiles
 
