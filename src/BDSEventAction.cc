b7f3af7388dc971253f7aa28d1b9f9b2e0a2e51f
Jochem Snuverink <jochem.snuverink@psi.ch>
2017-02-20 14:00:57 +0100

    bug fix to trajectory storing: add false pair to map if not interesting trajectory; put function in unnamed namespace
    Former-commit-id: 06110793d067dc9f3412cab4bb7c34ffb40085bd

--- src/BDSEventAction.cc
+++ src/BDSEventAction.cc
@@ -24,6 +24,7 @@
 #include <algorithm>
 #include <chrono>
 #include <ctime>
+#include <map>
 #include <string>
 #include <vector>
 
@@ -33,17 +34,19 @@ extern BDSOutputBase* bdsOutput;       // output interface
 
 G4bool FireLaserCompton;  // bool to ensure that Laserwire can only occur once in an event
 
-// Function to recursively connect t
-void connectTraj(const std::map<int, BDSTrajectory*> &trackIDMap, std::map<BDSTrajectory*, bool> &interestingTraj, BDSTrajectory* t)
-{
-  G4int parentID = t->GetParentID();
-  BDSTrajectory *t2 = trackIDMap.at(parentID);
-  interestingTraj.insert(std::pair<BDSTrajectory*, bool>(t2,true));
-  if(t2->GetParentID() != 0) {
-    connectTraj(trackIDMap, interestingTraj, t2);
-  }
-  else {
-    return;
+namespace {
+  // Function to recursively connect t
+  void connectTraj(const std::map<int, BDSTrajectory*> &trackIDMap, std::map<BDSTrajectory*, bool> &interestingTraj, BDSTrajectory* t)
+  {
+    G4int parentID = t->GetParentID();
+    BDSTrajectory *t2 = trackIDMap.at(parentID);
+    interestingTraj.insert(std::pair<BDSTrajectory*, bool>(t2,true));
+    if(t2->GetParentID() != 0) {
+      connectTraj(trackIDMap, interestingTraj, t2);
+    }
+    else {
+      return;
+    }
   }
 }
 
@@ -229,9 +232,6 @@ void BDSEventAction::EndOfEventAction(const G4Event* evt)
 
   if(BDSGlobalConstants::Instance()->StoreTrajectory())
   {
-    std::vector<BDSTrajectory*> interestingTrajectories;
-    std::map<BDSTrajectory*, bool> interestingTraj;
-
     TrajectoryVector* trajVec = trajCont->GetVector();
       
 #ifdef BDSDEBUG
@@ -261,6 +261,8 @@ void BDSEventAction::EndOfEventAction(const G4Event* evt)
       }
     }
 
+    std::vector<BDSTrajectory*> interestingTrajectories;
+    std::map<BDSTrajectory*, bool> interestingTraj;
     // loop over trajectories and determine if traj should be stored
     for (auto iT1 : *trajVec)
     {
@@ -312,6 +314,8 @@ void BDSEventAction::EndOfEventAction(const G4Event* evt)
         interestingTraj.insert(std::pair<BDSTrajectory*,bool>(traj,true));
         continue;
       }
+
+      interestingTraj.insert(std::pair<BDSTrajectory*,bool>(traj,false));
     }
 
     // Connect trajectory graphs
