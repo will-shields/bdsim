da4680ab901e3d805c551a0eb104666f0eb9446e
Lawrence Deacon <lawrence.deacon@ucl.ac.uk>
2017-02-22 13:19:32 +0000

    Allow BDSFieldLoaderBDSIM.cc to read files that loop in any order.
    Former-commit-id: a210698688d954ff71622ac681c13df5ad504f81

--- src/BDSFieldLoaderBDSIM.cc
+++ src/BDSFieldLoaderBDSIM.cc
@@ -42,7 +42,7 @@ void BDSFieldLoaderBDSIM<T>::CleanUp()
   fv = BDSFieldValue();
   std::vector<G4String> allKeys = {"nx", "ny", "nz", "nt",
 				   "xmin", "xmax", "ymin", "ymax",
-				   "zmin", "zmax", "tmin", "tmax"};
+				   "zmin", "zmax", "tmin", "tmax", "loopOrder"};
   for (const std::string s : allKeys)
     {header[s] = 0;}
   result = nullptr;
@@ -111,156 +111,201 @@ void BDSFieldLoaderBDSIM<T>::Load(G4String fileName,
   G4int nY   = 1;
   G4int nZ   = 1;
   G4int nT   = 1;
+    G4int* ind1;
+    G4int* ind2;
+    G4int* ind3;
+    G4int* ind4;
+    G4int* n1;
+    G4int* n2;
+    G4int* n3;
+    G4int* n4; //Pointers to the above, to allow different loop orderings.
+    G4int loopOrder = 1; // The order in which to loop over the axes.
 
-  while (std::getline(file, line))
-    {// read a line only if it's not a blank one
+  while (std::getline(file, line)) {// read a line only if it's not a blank one
 
       // Skip a line if it's only whitespace
-      if (std::all_of(line.begin(), line.end(), isspace))
-	{continue;}
+      if (std::all_of(line.begin(), line.end(), isspace)) { continue; }
 
       // Process a line of field data
-      if (intoData)
-	{
-	  if (dataFinished)
-	    {continue;} // just keep skipping lines after the appropriate amount of data is loaded
-	  
-	  ProcessData(line, xIndex, yIndex, zIndex); // changes member fv
-	    
-	  // Copy into array - we can always use 4d coords even for lower d arrays
-	  // as they all inherit 4d.
-	  (*result)(indX,indY,indZ,indT) = fv;
+      if (intoData) {
+          if (dataFinished) { continue; } // just keep skipping lines after the appropriate amount of data is loaded
 
-        //Loop order changed: from x, y, z, t to  z, y, x, t.
-        indZ++; // increment counter
-        if (indZ == nZ)
-        {// we've completed one set of z
-            indZ = 0; // reset back to 0
-            indY++;   // advance y
-        }
-        if (indY == nY)
-        {// we've completed one set of y
-            indY = 0; // reset back to 0
-            indX++;   // advance x
-        }
-        if (indX == nX)
-        {// we've copmleted one set of x
-            indX = 0; // reset back to 0
-            indT++;   // advance t
-        }
-        if (indT == nT)
-        {// we've completed one set of t
-            indT = 0;
-            dataFinished = true;
-        }
-	  continue;
-	}
+          ProcessData(line, xIndex, yIndex, zIndex); // changes member fv
+
+          // Copy into array - we can always use 4d coords even for lower d arrays
+          // as they all inherit 4d.
+          (*result)(indX, indY, indZ, indT) = fv;
+
+
+
+          //Loop order changed: from x, y, z, t to  z, y, x, t.
+          (*ind1)++; // increment counter
+          if ((*ind1) == (*n1)) {// we've completed one set of z
+              *ind1 = 0; // reset back to 0
+              (*ind2)++;   // advance y
+          }
+          if ((*ind2) == (*n2)) {// we've completed one set of y
+              *ind2 = 0; // reset back to 0
+              (*ind2)++;   // advance x
+          }
+          if ((*ind3) == (*n3)) {// we've copmleted one set of x
+              *ind3 = 0; // reset back to 0
+              (*ind4)++;   // advance t
+          }
+          if ((*ind4) == (*n4)) {// we've completed one set of t
+              *ind4 = 0;
+              dataFinished = true;
+          }
+          continue;
+      }
 
       // key definition
-      if (line.find(">") != std::string::npos)
-	{// must be key definition
-	  std::regex keyValue("(\\w*)\\s*>\\s*([0-9eE.+-]+)");
-	  std::smatch match;
-	  std::regex_search(line, match, keyValue);
-	  if (match.size() < 2)
-	    {G4cerr << "Invalid key definition in field format:\n" << line << G4endl; exit(1);}
-	  else
-	    {
-	      G4String key = G4String(match[1]);
-	      key.toLower();
-	      G4double value = 0;
-	      try
-		{value = std::stod(match[2]);}
-	      catch (std::invalid_argument)
-		{G4cerr << "Invalid argument "    << match[2] << G4endl; exit(1);}
-	      catch (std::out_of_range)
-		{G4cerr << "Number out of range " << match[2] << G4endl; exit(1);}
-		    
-	      header[key] = value;
-	      continue;
-	    }
-	}
-      
+      if (line.find(">") != std::string::npos) {// must be key definition
+          std::regex keyValue("(\\w*)\\s*>\\s*([0-9eE.+-]+)");
+          std::smatch match;
+          std::regex_search(line, match, keyValue);
+          if (match.size() < 2) {
+              G4cerr << "Invalid key definition in field format:\n" << line << G4endl;
+              exit(1);
+          }
+          else {
+              G4String key = G4String(match[1]);
+              key.toLower();
+              G4double value = 0;
+              try { value = std::stod(match[2]); }
+              catch (std::invalid_argument) {
+                  G4cerr << "Invalid argument " << match[2] << G4endl;
+                  exit(1);
+              }
+              catch (std::out_of_range) {
+                  G4cerr << "Number out of range " << match[2] << G4endl;
+                  exit(1);
+              }
+
+              header[key] = value;
+              continue;
+          }
+      }
+
       // if starts with '!' - columns
       // check all required keys have been built up ok
       // set nColumns
       std::regex columnRow("\\s*^!"); // ignore any initial white space and look for '!'
-      if (std::regex_search(line, columnRow))
- 	{
-	  // we only need to record the number of columns and which ones are
-	  // the x,y,z field component ones.
-	  std::regex afterExclamation("\\s*!\\s*(.+)");
-	  std::smatch match;
-	  std::regex_search(line, match, afterExclamation);
-	  std::string restOfLine = match[1];
-	  std::string columnName;
-	  std::istringstream restOfLineSS(restOfLine);
-	  while (restOfLineSS >> columnName)
-	    {
-	      nColumns++;
-	      if (columnName.find("Fx") != std::string::npos)
-		{xIndex = nColumns; continue;}
-	      if (columnName.find("Fy") != std::string::npos)
-		{yIndex = nColumns; continue;}
-	      if (columnName.find("Fz") != std::string::npos)
-		{zIndex = nColumns; continue;}
+      if (std::regex_search(line, columnRow)) {
+          // we only need to record the number of columns and which ones are
+          // the x,y,z field component ones.
+          std::regex afterExclamation("\\s*!\\s*(.+)");
+          std::smatch match;
+          std::regex_search(line, match, afterExclamation);
+          std::string restOfLine = match[1];
+          std::string columnName;
+          std::istringstream restOfLineSS(restOfLine);
+          while (restOfLineSS >> columnName) {
+              nColumns++;
+              if (columnName.find("Fx") != std::string::npos) {
+                  xIndex = nColumns;
+                  continue;
+              }
+              if (columnName.find("Fy") != std::string::npos) {
+                  yIndex = nColumns;
+                  continue;
+              }
+              if (columnName.find("Fz") != std::string::npos) {
+                  zIndex = nColumns;
+                  continue;
+              }
 
-	    }
-	  lineData.resize(nColumns+1); // +1 for default value
-	  intoData = true;
+          }
+          lineData.resize(nColumns + 1); // +1 for default value
+          intoData = true;
 
-	  if (nColumns < (nDim + 3)) // 3 for field components
-	    {G4cerr << "Too few columns for " << nDim << "D field loading" << G4endl; exit(1);}
-	  
-	  // we have all the information now, so initialise the container
-	  switch (nDim)
-	    {
-	    case 1:
-	      {
-		nX = G4int(header["nx"]);
-		result = new BDSArray1DCoords(nX,
-					      header["xmin"]*CLHEP::cm, header["xmax"]*CLHEP::cm);
-		break;
-	      }
-	    case 2:
-	      {
-		nX = G4int(header["nx"]);
-		nY = G4int(header["ny"]);
-		result = new BDSArray2DCoords(nX, nY,
-					      header["xmin"]*CLHEP::cm, header["xmax"]*CLHEP::cm,
-					      header["ymin"]*CLHEP::cm, header["ymax"]*CLHEP::cm);
-		break;
-	      }
-	    case 3:
-	      {
-		nX = G4int(header["nx"]);
-		nY = G4int(header["ny"]);
-		nZ = G4int(header["nz"]);
-		result = new BDSArray3DCoords(nX, nY, nZ,
-					      header["xmin"]*CLHEP::cm, header["xmax"]*CLHEP::cm,
-					      header["ymin"]*CLHEP::cm, header["ymax"]*CLHEP::cm,
-					      header["zmin"]*CLHEP::cm, header["zmax"]*CLHEP::cm);
-		break;
-	      }
-	    case 4:
-	      {
-		nX = G4int(header["nx"]);
-		nY = G4int(header["ny"]);
-		nZ = G4int(header["nz"]);
-		nT = G4int(header["nt"]);
-		result = new BDSArray4DCoords(nX, nY, nZ, nT,
-					      header["xmin"]*CLHEP::cm, header["xmax"]*CLHEP::cm,
-					      header["ymin"]*CLHEP::cm, header["ymax"]*CLHEP::cm,
-					      header["zmin"]*CLHEP::cm, header["zmax"]*CLHEP::cm,
-					      header["tmin"]*CLHEP::s,  header["tmax"]*CLHEP::s);
-		break;
-	      }
-	    default:
-	      break;
-	    }
-	  continue;
-	}
-    }
+          if (nColumns < (nDim + 3)) // 3 for field components
+          {
+              G4cerr << "Too few columns for " << nDim << "D field loading" << G4endl;
+              exit(1);
+          }
+
+          // we have all the information now, so initialise the container
+          switch (nDim) {
+              case 1: {
+                  nX = G4int(header["nx"]);
+                  result = new BDSArray1DCoords(nX,
+                                                header["xmin"] * CLHEP::cm, header["xmax"] * CLHEP::cm);
+                  break;
+              }
+              case 2: {
+                  loopOrder = G4int(header["loopOrder"]);
+                  nX = G4int(header["nx"]);
+                  nY = G4int(header["ny"]);
+                  result = new BDSArray2DCoords(nX, nY,
+                                                header["xmin"] * CLHEP::cm, header["xmax"] * CLHEP::cm,
+                                                header["ymin"] * CLHEP::cm, header["ymax"] * CLHEP::cm);
+                  break;
+              }
+              case 3: {
+                  loopOrder = G4int(header["loopOrder"]);
+                  nX = G4int(header["nx"]);
+                  nY = G4int(header["ny"]);
+                  nZ = G4int(header["nz"]);
+                  result = new BDSArray3DCoords(nX, nY, nZ,
+                                                header["xmin"] * CLHEP::cm, header["xmax"] * CLHEP::cm,
+                                                header["ymin"] * CLHEP::cm, header["ymax"] * CLHEP::cm,
+                                                header["zmin"] * CLHEP::cm, header["zmax"] * CLHEP::cm);
+                  break;
+              }
+              case 4: {
+                  loopOrder = G4int(header["loopOrder"]);
+                  nX = G4int(header["nx"]);
+                  nY = G4int(header["ny"]);
+                  nZ = G4int(header["nz"]);
+                  nT = G4int(header["nt"]);
+                  result = new BDSArray4DCoords(nX, nY, nZ, nT,
+                                                header["xmin"] * CLHEP::cm, header["xmax"] * CLHEP::cm,
+                                                header["ymin"] * CLHEP::cm, header["ymax"] * CLHEP::cm,
+                                                header["zmin"] * CLHEP::cm, header["zmax"] * CLHEP::cm,
+                                                header["tmin"] * CLHEP::s, header["tmax"] * CLHEP::s);
+                  break;
+              }
+              default:
+                  break;
+          }
+
+
+          switch(loopOrder){
+              case 321:
+                  ind1 = &indZ;
+                  n1 = &nZ;
+                  ind2 = &indY;
+                  n2 = &nY;
+                  ind3 = &indX;
+                  n3 = &nX;
+                  ind4 = &indT;
+                  n4 = &nT;
+                  break;
+              case 123:
+                  ind1 = &indX;
+                  n1 = &nX;
+                  ind2 = &indY;
+                  n2 = &nY;
+                  ind3 = &indZ;
+                  n3 = &nZ;
+                  ind4 = &indT;
+                  n4 = &nT;
+                  break;
+              default:
+                  ind1 = &indZ;
+                  n1 = &nZ;
+                  ind2 = &indY;
+                  n2 = &nY;
+                  ind3 = &indX;
+                  n3 = &nX;
+                  ind4 = &indT;
+                  n4 = &nT;
+          }
+          continue;
+      }
+
+  }
 
   file.close();
 }
