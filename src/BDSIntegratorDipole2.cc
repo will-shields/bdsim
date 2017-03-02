66ddecd2dc4ada93ca1606982faef003cb5f74a3
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2017-03-02 15:23:43 +0000

    Remove g4 classical usage for low momenta in new dipole integrator.  Use h instead of stepLength for consistency.
    Former-commit-id: 4e0ad46eefc42a72d2c4d4d2746fb4a615da6ba7

--- src/BDSIntegratorDipole2.cc
+++ src/BDSIntegratorDipole2.cc
@@ -30,24 +30,12 @@ void BDSIntegratorDipole2::DumbStepper(const G4double yIn[],
 
 void BDSIntegratorDipole2::Stepper(const G4double yIn[],
 				   const G4double dydx[],
-				   G4double       stepLength,
+				   G4double       h,
 				   G4double       yOut[],
 				   G4double       yErr[])
 {
   // Extra storage arrays.
   G4double yTemp[7], yTemp2[7];
-
-  // If a low momentum particle, use the backup stepper as more robust
-  G4ThreeVector momentum = G4ThreeVector(yIn[3], yIn[4], yIn[5]);
-  if (momentum.mag() < 40*CLHEP::MeV)
-    {
-      backupStepper->Stepper(yIn, dydx, stepLength, yOut, yErr);
-      // G4MagHelicaStepper provides no way to overload DistChord,
-      // so exploit the logic in it. 7rad forces return 2*GetRadHelix()
-      SetAngCurve(7);
-      SetRadHelix(backupStepper->DistChord()*0.5);
-      return;
-    }
   
   // Arrays for field querying (g4 interface)
   G4double bO[4], bM[4]; // original and mid point
@@ -55,7 +43,7 @@ void BDSIntegratorDipole2::Stepper(const G4double yIn[],
   G4ThreeVector bOriginal = G4ThreeVector(bO[0],bO[1],bO[2]);
 
   // Do a full step - the result we use
-  AdvanceHelix(yIn, bOriginal, stepLength, yOut);
+  AdvanceHelix(yIn, bOriginal, h, yOut);
 
   // Now we have the radius of curvature to check on.
   // If it's smaller than limit, we artificially advance the particle
@@ -64,20 +52,20 @@ void BDSIntegratorDipole2::Stepper(const G4double yIn[],
   const G4double radiusOfCurvature = GetRadHelix();
   if (radiusOfCurvature < minimumRadiusOfCurvature)
     {
-      AdvanceHelixForSpiralling(yIn, bOriginal, stepLength, yOut, yErr);
+      AdvanceHelixForSpiralling(yIn, bOriginal, h, yOut, yErr);
       return;
     }
 
   // error estimation - do two half steps and compare difference to
   // the result from one full step
-  AdvanceHelix(yIn, bOriginal, stepLength*0.5, yTemp); // first step
+  AdvanceHelix(yIn, bOriginal, h*0.5, yTemp); // first step
 
   // resample field at midway point (although if pure dipole, this is
   // unnecessary) - could go outside the range of the field though
   GetEquationOfMotion()->GetFieldValue(yTemp, bM);
   G4ThreeVector bMid = G4ThreeVector(bM[0],bM[1],bM[2]);
 
-  AdvanceHelix(yTemp, bMid, stepLength*0.5, yTemp2); // second step
+  AdvanceHelix(yTemp, bMid, h*0.5, yTemp2); // second step
   
   // Error estimation
   for(G4int i = 0; i < 6; i++)
@@ -86,19 +74,19 @@ void BDSIntegratorDipole2::Stepper(const G4double yIn[],
 
 void BDSIntegratorDipole2::AdvanceHelixForSpiralling(const G4double yIn[],
 						     G4ThreeVector  field,
-						     G4double       stepLength,
+						     G4double       h,
 						     G4double       yOut[],
 						     G4double       yErr[])
 {
-  AdvanceHelix(yIn, field, stepLength, yOut);
+  AdvanceHelix(yIn, field, h, yOut);
 
   G4ThreeVector unitMomentum    = G4ThreeVector(yIn[3], yIn[4], yIn[5]);
   G4ThreeVector unitField       = field.unit();
   G4ThreeVector unitSideways    = unitField.cross(unitMomentum.unit());
   G4ThreeVector correctPosition = G4ThreeVector(yOut[0], yOut[1], yOut[2]);
   //G4ThreeVector delta2 = 1*CLHEP::mm * unitField;// + 1*CLHEP::um * unitMomentum;
-  //G4ThreeVector delta1 = std::max(stepLength, 1*CLHEP::mm)*unitField;
-  G4ThreeVector delta = stepLength*unitSideways;
+  //G4ThreeVector delta1 = std::max(h, 1*CLHEP::mm)*unitField;
+  G4ThreeVector delta = h*unitSideways;
   G4ThreeVector newPosition     = correctPosition + delta;
   //G4cout << newPosition << delta << G4endl;
   //SetAngCurve(7);
@@ -106,6 +94,9 @@ void BDSIntegratorDipole2::AdvanceHelixForSpiralling(const G4double yIn[],
   yOut[0] = newPosition[0];
   yOut[1] = newPosition[1];
   yOut[2] = newPosition[2];
+
+  //SetAngCurve(7);
+  //SetRadHelix(backupStepper->DistChord()*0.5);
 
   // empirically chosen error values that give the fewest warnings
   // for spiralling particles in showers in strong dipole fields
