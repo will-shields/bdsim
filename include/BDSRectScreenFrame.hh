dd29af0e39407e1b34f368357b40eb5072f02d1a
Lawrence Deacon <lawrence.deacon@ucl.ac.uk>
2017-03-05 09:36:37 +0000

    Improved spectrometer window frame geometry description.
    Former-commit-id: 50968f4c3289f195ccbe0832f728c519af03befb

--- include/BDSRectScreenFrame.hh
+++ include/BDSRectScreenFrame.hh
@@ -12,9 +12,15 @@ public:
     ~BDSRectScreenFrame();
     BDSRectScreenFrame(G4String name, G4ThreeVector size, G4TwoVector windowSize, G4TwoVector windowOffset,
             G4Material* material);
+    G4LogicalVolume* CavityLogVol(){return _cavityLogVol;}
 virtual void Build();
+    void Place(G4RotationMatrix* rot, G4ThreeVector pos, G4LogicalVolume* motherVol);
 private:
     BDSRectScreenFrame();
+    G4LogicalVolume* _cavityLogVol;
+    G4String _cavityName;
+    G4ThreeVector _cavityPos;
+    G4RotationMatrix* _zeroRot;
 };
 
 
