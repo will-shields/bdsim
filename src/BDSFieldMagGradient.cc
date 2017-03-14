3568e8445d49a300c8ffaf41a4717be483303da4
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2017-03-14 17:32:52 +0000

    Unit fixes.  Use PrepareValues recursively.
    Former-commit-id: cac6db602786c87a3a6cae55a52aae099cc4bb81

--- src/BDSFieldMagGradient.cc
+++ src/BDSFieldMagGradient.cc
@@ -38,7 +38,7 @@ BDSMagnetStrength* BDSFieldMagGradient::CalculateMultipoles(BDSFieldMag* BField,
     BDSMagnetStrength* outputstrengths = new BDSMagnetStrength();
     G4double h =0.5; //distance apart in CLHEP distance units (mm) to place query points.
 
-    G4double brhoinv = 1./Brho;
+    G4double brhoinv = 1./(Brho / CLHEP::tesla / CLHEP::m);
     G4int centreIndex = 0;
     std::vector<G4double> d = PrepareValues(BField, 5, 0, h, centreIndex);
     G4int centreIndexSkew = 0;
@@ -46,10 +46,13 @@ BDSMagnetStrength* BDSFieldMagGradient::CalculateMultipoles(BDSFieldMag* BField,
     // o+1 as we start from k1 upwards - ie, 0th order isn't calculated
     for (G4int o = 0; o < order; ++o)
       {
-        (*outputstrengths)["k" + std::to_string(o+1)] = Derivative(d, o+1, centreIndex, h) * brhoinv * pow(CLHEP::meter,o+1);
+	(*outputstrengths)["k" + std::to_string(o+1)] = Derivative(d, o+1, centreIndex, h) * brhoinv;
+        (*outputstrengths)["k" + std::to_string(o+1) + "s"] = Derivative(dskew[o], o+1, centreIndex, h) * brhoinv;
+
+#ifdef BDSDEBUG
         G4cout << "k" << o+1 << " = " << (*outputstrengths)["k" + std::to_string(o+1)] << G4endl;
-        (*outputstrengths)["k" + std::to_string(o+1) + "s"] = Derivative(dskew[o], o+1, centreIndex, h)*brhoinv*pow(CLHEP::meter,o+1);
-        G4cout << "k" << o+1 << "s"<< " = " << (*outputstrengths)["k" + std::to_string(o+1) + "s"]<< G4endl;
+	G4cout << "k" << o+1 << "s"<< " = " << (*outputstrengths)["k" + std::to_string(o+1) + "s"]<< G4endl;
+#endif
       }
     return outputstrengths;
 }
@@ -66,9 +69,7 @@ std::vector<G4double> BDSFieldMagGradient::PrepareValues(BDSFieldMag* field,
   std::vector<G4double> data(2*maxN+1); // must initialise vector as not using push_back
   
   for (G4int i = -maxN; i <= maxN; i++)
-    {
-    data[maxN + i] = GetBy(field, centreX+(G4double)i*h);
-    }
+    {data[maxN + i] = GetBy(field, centreX+(G4double)i*h);}
   return data;
 }
 
@@ -78,25 +79,16 @@ std::vector<std::vector<G4double>> BDSFieldMagGradient::PrepareSkewValues(BDSFie
                                                                           G4double     h,
                                                                           G4int&       centreIndex) const
 {
-    G4double rotation[5] = {CLHEP::pi/4, CLHEP::pi/6, CLHEP::pi/8, CLHEP::pi/10, CLHEP::pi/12};
-    G4int maxN = 2*order + 1;
-    centreIndex = maxN;
-    std::vector<G4double> data(2*maxN+1);
-    std::vector<std::vector<G4double>> SkewValues(order+1);
-    for (G4int j=0; j<order; j++)
+  G4double rotation[5] = {CLHEP::pi/4, CLHEP::pi/6, CLHEP::pi/8, CLHEP::pi/10, CLHEP::pi/12};
+  std::vector<std::vector<G4double>> skewValues(order);
+  for (G4int j=0; j<order; j++)
     {
-        BDSFieldMagSkew* skewField = new BDSFieldMagSkew(field, rotation[j]);
-        for (G4int i = -maxN; i < maxN; i++)
-        {
-            data[maxN + i] = GetBy(skewField, centreX + (G4double) i * h);
-            SkewValues[j].push_back(data[maxN + i]);
-        }
-        delete skewField;
+      BDSFieldMagSkew* skewField = new BDSFieldMagSkew(field, rotation[j]);
+      skewValues[j] = PrepareValues(skewField, order, centreX, h, centreIndex);
+      delete skewField;
     }
-    return SkewValues;
+  return skewValues;
 }
-
-
 
 G4double BDSFieldMagGradient::Derivative(const std::vector<G4double>& data,
 					 const G4int                  order,
@@ -110,6 +102,6 @@ G4double BDSFieldMagGradient::Derivative(const std::vector<G4double>& data,
     + 8*Derivative(data, subOrder,startIndex+1, h)
     - 8*Derivative(data, subOrder,startIndex-1, h)
     + Derivative(data, subOrder,startIndex-2, h);
-  result /= 12*(h);
+  result /= 12*(h/CLHEP::m);
   return result;
 }
