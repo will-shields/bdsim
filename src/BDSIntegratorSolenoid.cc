17b0f17555ee5f3e83bb185e77715444fed56fca
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2017-03-21 23:28:56 +0000

    Do paraxial check up front in solenoid stepper to avoid long if else.  Protection against possible nan from sqrt.
    Former-commit-id: c8d8ae7038e04646804dbe4cb7a9bba7e4736775

--- src/BDSIntegratorSolenoid.cc
+++ src/BDSIntegratorSolenoid.cc
@@ -7,6 +7,8 @@
 #include "G4Mag_EqRhs.hh"
 #include "G4ThreeVector.hh"
 
+#include <cmath>
+
 
 BDSIntegratorSolenoid::BDSIntegratorSolenoid(BDSMagnetStrength const* strength,
 					     G4double                 brho,
@@ -82,7 +84,7 @@ void BDSIntegratorSolenoid::AdvanceHelix(const G4double yIn[],
   G4cout << " curvature= " << R_1*CLHEP::m << "m^-1" << G4endl;
 #endif
 
-  if (R_1<1e-15)
+  if (R_1 < 1e-15)
     {
       AdvanceDriftMag(yIn, h, yOut, yErr);
       SetDistChord(0);
@@ -101,95 +103,93 @@ void BDSIntegratorSolenoid::AdvanceHelix(const G4double yIn[],
   SetDistChord(dc);
 
   // check for paraxial approximation:
-  if(fabs(zp0)>0.9)
-    {
-      // RMatrix - from Andy Wolszki's Linear Dynamics lectures (#5, slide 43)
-      // http://pcwww.liv.ac.uk/~awolski/main_teaching_Cockcroft_LinearDynamics.htm
-      // note this is actually for one step through the whole solenoid as focussing
-      // comes from edge effects - may need to reconsider this in the future...
-      // maximum step size is set to full length in BDSSolenoid.cc
-      // ( cos^2 (wL)     , (1/2w)sin(2wL)  , (1/2)sin(2wL)  , (1/w)sin^2(wL) ) (x )
-      // ( (w/2)sin(2wL)  , cos^2(wL)       ,  -w sin^2(wL)  , (1/2)sin(2wL)  ) (x')
-      // ( -(1/2)sin(2wL) , (-1/w)sin^2(wL) , cos^2(wL)      , (1/2w)sin(2wL) ) (y )
-      // ( w sin^2(wL)    , (-1/2)sin(2wL)  , (-w/2)sin(2wL) , cos^2(wL)      ) (y')
-      
-      G4double w       = kappa;
-      //need the length along curvilinear s -> project h on z
-      G4ThreeVector positionMove = h * InitMomDir; 
-      G4double dz      = positionMove.z();
-      G4double wL      = kappa*dz; 
-      G4double cosOL   = cos(wL); // w is really omega - so use 'O' to describe - OL = omega*L
-      G4double cosSqOL = cosOL*cosOL;
-      G4double sinOL   = sin(wL);
-      G4double sin2OL  = sin(2.0*wL);
-      G4double sinSqOL = sinOL*sinOL;
-      
-      // calculate thick lens transfer matrix
-      x1  = x0*cosSqOL + (0.5*xp0/w)*sin2OL + (0.5*y0)*sin2OL + (yp0/w)*sinSqOL;
-      xp1 = (0.5*x0*w)*sin2OL + xp0*cosSqOL - (w*y0)*sinSqOL + (0.5*yp0)*sin2OL;
-      y1  = (-0.5*x0)*sin2OL - (xp0/w)*sinSqOL + y0*cosSqOL + (0.5*yp0/w)*sin2OL;
-      yp1 = x0*w*sinSqOL - (0.5*xp0)*sin2OL - (0.5*w*y0)*sin2OL + yp0*cosSqOL;  
-      
-      // ensure normalisation for vector
-      zp1 = std::sqrt(1 - xp1*xp1 -yp1*yp1);
-      
-      // calculate deltas to existing coords
-      G4double dx = x1-x0;
-      G4double dy = y1-y0;
-      
-      // check for precision problems
-      G4double ScaleFac = (dx*dx+dy*dy+dz*dz)/h2;
-#ifdef BDSDEBUG
-      G4cout << "Ratio of calculated to proposed step length: " << ScaleFac << G4endl;
-#endif
-      if(ScaleFac>1.0000001)
-	{
-#ifdef BDSDEBUG
-	  G4cout << __METHOD_NAME__ << " normalising to conserve step length" << G4endl;
-#endif
-	  ScaleFac = std::sqrt(ScaleFac);
-	  dx /= ScaleFac;
-	  dy /= ScaleFac;
-	  dz /= ScaleFac;
-	  x1 =  x0+dx;
-	  y1 =  y0+dy;
-	}
-      z1 = z0+dz;
-
-      //write the final coordinates
-      LocalR.setX(x1);
-      LocalR.setY(y1);
-      LocalR.setZ(z1);
-      LocalRp.setX(xp1);
-      LocalRp.setY(yp1);
-      LocalRp.setZ(zp1);
-      
-#ifdef BDSDEBUG 
-      G4cout << "BDSIntegratorSolenoid: final point in local coordinates:" << G4endl
-	     << " x  = " << LocalR[0]/CLHEP::m << " m" << G4endl
-	     << " y  = " << LocalR[1]/CLHEP::m << " m" << G4endl
-	     << " z  = " << LocalR[2]/CLHEP::m << " m" << G4endl
-	     << " x' = " << LocalRp[0]         << G4endl
-	     << " y' = " << LocalRp[1]         << G4endl
-	     << " z' = " << LocalRp[2]         << G4endl
-	     << G4endl; 
-#endif
-      
-      ConvertToGlobal(LocalR,LocalRp,InitPMag,yOut);
-
-      for(G4int i = 0; i < nVariables; i++)
-	{yErr[i]=0;} //set error to be zero - not strictly correct
-    }
-  else
-    // perform local helical steps (paraxial approx not safe)
-    {
-#ifdef BDSDEBUG
-      G4cout << __METHOD_NAME__ << " local helical steps - using G4ClassicalRK4" << G4endl;
-#endif
-      // use a classical Runge Kutta stepper here
+  if(std::abs(zp0) < 0.9)
+    {// use a classical Runge Kutta stepper here
       backupStepper->Stepper(yIn, dydx, h, yOut, yErr);
       SetDistChord(backupStepper->DistChord());
+      return;
     }  
+
+  // RMatrix - from Andy Wolszki's Linear Dynamics lectures (#5, slide 43)
+  // http://pcwww.liv.ac.uk/~awolski/main_teaching_Cockcroft_LinearDynamics.htm
+  // note this is actually for one step through the whole solenoid as focussing
+  // comes from edge effects - may need to reconsider this in the future...
+  // maximum step size is set to full length in BDSSolenoid.cc
+  // ( cos^2 (wL)     , (1/2w)sin(2wL)  , (1/2)sin(2wL)  , (1/w)sin^2(wL) ) (x )
+  // ( (w/2)sin(2wL)  , cos^2(wL)       ,  -w sin^2(wL)  , (1/2)sin(2wL)  ) (x')
+  // ( -(1/2)sin(2wL) , (-1/w)sin^2(wL) , cos^2(wL)      , (1/2w)sin(2wL) ) (y )
+  // ( w sin^2(wL)    , (-1/2)sin(2wL)  , (-w/2)sin(2wL) , cos^2(wL)      ) (y')
+  
+  G4double w       = kappa;
+  //need the length along curvilinear s -> project h on z
+  G4ThreeVector positionMove = h * InitMomDir; 
+  G4double dz      = positionMove.z();
+  G4double wL      = kappa*dz; 
+  G4double cosOL   = cos(wL); // w is really omega - so use 'O' to describe - OL = omega*L
+  G4double cosSqOL = cosOL*cosOL;
+  G4double sinOL   = sin(wL);
+  G4double sin2OL  = sin(2.0*wL);
+  G4double sinSqOL = sinOL*sinOL;
+  
+  // calculate thick lens transfer matrix
+  x1  = x0*cosSqOL + (0.5*xp0/w)*sin2OL + (0.5*y0)*sin2OL + (yp0/w)*sinSqOL;
+  xp1 = (0.5*x0*w)*sin2OL + xp0*cosSqOL - (w*y0)*sinSqOL + (0.5*yp0)*sin2OL;
+  y1  = (-0.5*x0)*sin2OL - (xp0/w)*sinSqOL + y0*cosSqOL + (0.5*yp0/w)*sin2OL;
+  yp1 = x0*w*sinSqOL - (0.5*xp0)*sin2OL - (0.5*w*y0)*sin2OL + yp0*cosSqOL;  
+  
+  // ensure normalisation for vector
+  zp1 = std::sqrt(1 - xp1*xp1 -yp1*yp1);
+  if (std::isnan(zp1))
+    {zp1 = zp0;}
+  
+  // calculate deltas to existing coords
+  G4double dx = x1-x0;
+  G4double dy = y1-y0;
+  
+  // check for precision problems
+  G4double ScaleFac = (dx*dx+dy*dy+dz*dz)/h2;
+#ifdef BDSDEBUG
+  G4cout << "Ratio of calculated to proposed step length: " << ScaleFac << G4endl;
+#endif
+  if(ScaleFac>1.0000001)
+    {
+#ifdef BDSDEBUG
+      G4cout << __METHOD_NAME__ << " normalising to conserve step length" << G4endl;
+#endif
+      ScaleFac = std::sqrt(ScaleFac);
+      if (std::isnan(ScaleFac))
+	{ScaleFac = 1;}
+      dx /= ScaleFac;
+      dy /= ScaleFac;
+      dz /= ScaleFac;
+      x1 =  x0+dx;
+      y1 =  y0+dy;
+    }
+  z1 = z0+dz;
+  
+  //write the final coordinates
+  LocalR.setX(x1);
+  LocalR.setY(y1);
+  LocalR.setZ(z1);
+  LocalRp.setX(xp1);
+  LocalRp.setY(yp1);
+  LocalRp.setZ(zp1);
+  
+#ifdef BDSDEBUG 
+  G4cout << "BDSIntegratorSolenoid: final point in local coordinates:" << G4endl
+	 << " x  = " << LocalR[0]/CLHEP::m << " m" << G4endl
+	 << " y  = " << LocalR[1]/CLHEP::m << " m" << G4endl
+	 << " z  = " << LocalR[2]/CLHEP::m << " m" << G4endl
+	 << " x' = " << LocalRp[0]         << G4endl
+	 << " y' = " << LocalRp[1]         << G4endl
+	 << " z' = " << LocalRp[2]         << G4endl
+	 << G4endl; 
+#endif
+  
+  ConvertToGlobal(LocalR,LocalRp,InitPMag,yOut);
+  
+  for(G4int i = 0; i < nVariables; i++)
+    {yErr[i]=0;} //set error to be zero
 }
 
 
