1ebf08e1e1fe843d5fdbdd128b41f1d2a7e3294c
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2017-05-03 11:48:27 +0100

    Typedefs for commonly used long map definitions.
    Former-commit-id: 7dea3e607573ecdd82df08fd273cc3c4dd6ec2e6

---
+++ analysis/RebdsimTypes.hh
@@ -0,0 +1,14 @@
+#ifndef REBDSIMTYPES_H
+#define REBDSIMTYPES_H
+
+#include <map>
+#include <string>
+#include <vector>
+
+namespace RBDS
+{
+  typedef std::map<std::string, std::vector<std::string> > BranchMap;
+  typedef std::vector<std::string> VectorString;
+}
+
+#endif
