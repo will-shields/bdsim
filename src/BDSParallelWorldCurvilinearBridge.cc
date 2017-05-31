8e1827a4af690cb38c17a75157bc89cb53888a79
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2017-05-31 17:46:37 +0100

    Place the curvilinear bridge volumes in their world.
    Former-commit-id: 7c23bdf482a31025298484790bcf83e31a56ad98

--- src/BDSParallelWorldCurvilinearBridge.cc
+++ src/BDSParallelWorldCurvilinearBridge.cc
@@ -1,18 +1,14 @@
-#include "BDSAcceleratorComponent.hh"
 #include "BDSAcceleratorModel.hh"
 #include "BDSAuxiliaryNavigator.hh"
-#include "BDSBeamline.hh"
-#include "BDSBeamlineElement.hh"
 #include "BDSDebug.hh"
 #include "BDSGlobalConstants.hh"
 #include "BDSParallelWorldCurvilinearBridge.hh"
-#include "BDSPhysicalVolumeInfo.hh"
-#include "BDSPhysicalVolumeInfoRegistry.hh"
 
 #include "G4LogicalVolume.hh"
 #include "G4VisAttributes.hh"
 #include "G4VPhysicalVolume.hh"
-#include "G4PVPlacement.hh"
+
+class BDSBeamline;
 
 
 BDSParallelWorldCurvilinearBridge::BDSParallelWorldCurvilinearBridge():
@@ -31,19 +27,19 @@ void BDSParallelWorldCurvilinearBridge::Construct()
   G4cout << __METHOD_NAME__ << G4endl;
 #endif
 
-  G4VPhysicalVolume* clbWorld   = GetWorld();
-  G4LogicalVolume*   clbWorldLV = clbWorld->GetLogicalVolume();
+  G4VPhysicalVolume* clbWorld = GetWorld();
 
-  // Register read out world PV with our auxiliary navigator. This gives curvilinear
-  // coordinates for multiple applications - CL = curvilinear.
-  //BDSAuxiliaryNavigator::AttachWorldVolumeToNavigatorCL(clWorld);
-  //BDSAuxiliaryNavigator::RegisterCurvilinearBridgeWorld(clbworld);
+  // Register read out world PV with our auxiliary navigator.
+  BDSAuxiliaryNavigator::RegisterCurvilinearBridgeWorld(clbWorld);
 
   // Visualisation
+  G4LogicalVolume* clbWorldLV = clbWorld->GetLogicalVolume();
   const BDSGlobalConstants* globals = BDSGlobalConstants::Instance();
   clbWorldVis = new G4VisAttributes(*(globals->GetVisibleDebugVisAttr()));
   clbWorldVis->SetForceWireframe(true);//just wireframe so we can see inside it
   clbWorldLV->SetVisAttributes(clbWorldVis);
 
-  //PlaceBeamlineInWorld(clbWorld, beamline, globals);
+  BDSBeamline* clbBeamline = BDSAcceleratorModel::Instance()->GetCurvilinearBridgeBeamline();
+
+  PlaceBeamlineInWorld(clbWorld, clbBeamline, globals);
 }
