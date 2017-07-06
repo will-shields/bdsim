0119d5a4a990d878f7e9606810956c2641bbe0eb
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2017-07-06 09:58:49 +0100

    Fix field attachment and remove old code.
    Former-commit-id: 606e12e6c8aa89f13a2bd0bc844d282e90b5642b

--- src/BDSCavityElement.cc
+++ src/BDSCavityElement.cc
@@ -53,49 +53,6 @@ void BDSCavityElement::BuildContainerLogicalVolume()
 void BDSCavityElement::BuildField()
 {
   BDSFieldBuilder::Instance()->RegisterFieldForConstruction(vacuumField,
-							    vacuumLV,
+							    GetAcceleratorVacuumLogicalVolume(),
 							    true);
 }
-/*
-void BDSCavityElement::PlaceComponents()
-{
-  G4PVPlacement* vacuumPV = new G4PVPlacement(nullptr,               //Rotation
-					      G4ThreeVector(0,0,0),  //Position
-					      vacuumLV,              //Logical Volume to be place
-					      name + "_vacuum_pv",   //placement name
-					      containerLogicalVolume,//mother volume
-					      false,                 //pMany unused
-					      0,                     //copy number
-					      checkOverlaps);        //check overlaps
-  
-  RegisterPhysicalVolume(vacuumPV);
-  
-  G4PVPlacement* cavityPV = new G4PVPlacement(nullptr,               //Rotation
-					      G4ThreeVector(0,0,0),  //Position
-					      cavityLV,              //Logical Volume to be place
-					      name + "_cavity_pv",   //placement name
-					      containerLogicalVolume,//mother volume
-					      false,                 //pMany unused
-					      0,                     //copy number
-					      checkOverlaps);        //check overlaps
-  
-  RegisterPhysicalVolume(cavityPV); 
-}
-
-void BDSCavityElement::BuildContainerLogicalVolume() 
-{
-  G4double outerRadius = cavityRadius + thickness + lengthSafety;
-  containerSolid = new G4Tubs(name + "_container_solid",   //name
-			      0.0,                         //innerRadius
-			      outerRadius,                 //outerRadius
-			      chordLength*0.5,             //half length
-			      0.0,                         //starting angle
-			      2.0*CLHEP::pi);              //spanning angle
-
-  SetExtent(BDSExtent(outerRadius, outerRadius,  chordLength*0.5));
-  
-  containerLogicalVolume = new G4LogicalVolume(containerSolid,
-					       emptyMaterial,
-					       name + "_container_lv");
-}
-*/
