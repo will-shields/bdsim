23a91b67a7a1bb95cd16660c476e92b55720be11
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2017-07-17 01:12:17 +0100

    Update none output for changes.
    Former-commit-id: c9e116142403091b577843321fb03e6b77f05236

--- include/BDSOutputNone.hh
+++ include/BDSOutputNone.hh
@@ -19,22 +19,11 @@ public:
 
   /// @{ No action.
   virtual void NewFile(){;}
-  virtual void InitialiseGeometryDependent(){;}
+  virtual void WriteOptions(){;}
+  virtual void WriteModel(){;}
+  virtual void WriteFileEventLevel(){;}
+  virtual void WriteFileRunLevel(){;}
   virtual void CloseFile(){;}
-  virtual G4bool FileIsOpen() {return false;}
-  virtual void FillEventInfo(const BDSEventInfo *){;}
-  virtual void FillSamplerHits(const BDSSamplerHitsCollection *, const HitsType){;}
-  virtual void FillEnergyLoss(const BDSEnergyCounterHitsCollection *, const LossType){;}
-  virtual void FillPrimaryHit(const BDSTrajectoryPoint *){;}
-  virtual void FillPrimaryLoss(const BDSTrajectoryPoint *){;}
-  virtual void FillTrajectories(const std::vector<BDSTrajectory *> &){;}
-  virtual void FillRunInfo(const BDSEventInfo *){;}
-  virtual void WriteFileEventLevel(){}
-  virtual void WriteFileRunLevel(){}
-  virtual void FillPrimary(const G4double, const G4double, const G4double,
-			   const G4double, const G4double, const G4double,
-			   const G4double, const G4double, const G4double,
-			   const G4int,    const G4int,    const G4int){;}
   /// @}
 };
 
