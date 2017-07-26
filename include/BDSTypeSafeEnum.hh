5ca84c47beba7abbcefc9ac070e240ef743574f5
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2017-07-26 11:51:44 +0100

    Potential fix for clang 8.1 warning.
    Former-commit-id: 413a8747a07d28b9ccf6e4b0482cffda605592b9

--- include/BDSTypeSafeEnum.hh
+++ include/BDSTypeSafeEnum.hh
@@ -65,9 +65,8 @@ public:
   }
 };
 
-/// general declaration, can be overwritten or appended
-// this seemed to cause a problem, so commented out and every class needs to define its map
-// template<typename def, typename inner>
-// std::map<BDSTypeSafeEnum<def,inner>,std::string>* BDSTypeSafeEnum<def,inner>::dictionary=nullptr;
+/// Initialisation for clang 8.1 warning suppression.
+template<typename def, typename inner>
+std::map<BDSTypeSafeEnum<def,inner>, std::string>* BDSTypeSafeEnum<def, inner>::dictionary = nullptr;
 
 #endif
