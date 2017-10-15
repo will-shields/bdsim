af9821468e1ca80180a4a532a904031ddd308add
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2017-10-15 21:35:57 +0200

    Secondary constructor for when we don't have a G4ParticleDefinition (ie an ion).  Common code in functions.  More accessors.
    Former-commit-id: e57190ab7bc1dbf12772b586da45c9bd814decc3

--- src/BDSParticleDefinition.cc
+++ src/BDSParticleDefinition.cc
@@ -1,5 +1,4 @@
 #include "BDSDebug.hh"
-#include "BDSGlobalConstants.hh"
 #include "BDSIonDefinition.hh"
 #include "BDSParticleDefinition.hh"
 #include "BDSPhysicalConstants.hh"
@@ -8,48 +7,53 @@
 #include "G4ParticleDefinition.hh"
 
 #include <cmath>
+#include <limits>
 #include <ostream>
 #include <stdexcept>
 
 
 BDSParticleDefinition::BDSParticleDefinition(G4ParticleDefinition* particleIn,
 					     G4double              totalEnergyIn,
+					     G4double              ffact,
 					     BDSIonDefinition*     ionDefinitionIn):
   particle(particleIn),
   ionDefinition(ionDefinitionIn),
+  name(particleIn->GetParticleName()),
+  mass(particleIn->GetPDGMass()),
   totalEnergy(totalEnergyIn),
-  brho(DBL_MAX)// if zero charge infinite magnetic rigidity
+  brho(std::numeric_limits<double>::max())// if zero charge infinite magnetic rigidity
 {
-  // charge
   charge = particle->GetPDGCharge();
   if (ionDefinition) // may be nullptr
     {
       if (ionDefinition->OverrideCharge()) // if override for ions
 	{charge = ionDefinition->Charge();}
     }
-
-  // kinetic energy
-  G4double mass = particle->GetPDGMass();
+  
   kineticEnergy = totalEnergy - mass;
 
-  // momentum
-  try
-    {momentum = std::sqrt(std::pow(totalEnergy,2) - std::pow(mass,2));}
-  catch (std::domain_error) // sqrt(-ve)
-    {
-      G4cerr << __METHOD_NAME__ << "Total energy insufficient to include mass or particle" << G4endl;
-      exit(1);
-    }
+  CalculateMomentum();
+  CalculateRigidity(ffact);
+}
 
-  // magnetic rigidity (brho)
-  // formula: B(Tesla)*rho(m) = p(GeV)/(0.299792458 * charge(e))
-  // charge (in e units); rigidity (in T*m)
-  if (BDS::IsFinite(charge))
-    {
-      BDSGlobalConstants* globals = BDSGlobalConstants::Instance();
-      brho = globals->FFact() * momentum / CLHEP::GeV / BDS::cOverGeV / charge;
-      brho *= CLHEP::tesla*CLHEP::m; // rigidity (in Geant4 units)
-    }
+BDSParticleDefinition::BDSParticleDefinition(G4String          nameIn,
+					     G4double          massIn,
+					     G4double          chargeIn,
+					     G4double          totalEnergyIn,
+					     G4double          ffact,
+					     BDSIonDefinition* ionDefinitionIn):
+  particle(nullptr),
+  ionDefinition(ionDefinitionIn),
+  name(nameIn),
+  mass(massIn),
+  charge(chargeIn),
+  totalEnergy(totalEnergyIn),
+  brho(std::numeric_limits<double>::max())// if zero charge infinite magnetic rigidity
+{
+  kineticEnergy = totalEnergy - mass;
+
+  CalculateMomentum();
+  CalculateRigidity(ffact);
 }
 
 BDSParticleDefinition::~BDSParticleDefinition()
@@ -59,8 +63,8 @@ BDSParticleDefinition::~BDSParticleDefinition()
 
 std::ostream& operator<<(std::ostream& out, const BDSParticleDefinition& def)
 {
-  out << "Particle:        " << def.particle->GetParticleName() << G4endl;
-  out << "Mass:            " << def.particle->GetPDGMass()/CLHEP::GeV << " GeV" << G4endl;
+  out << "Particle:        " << def.name << G4endl;
+  out << "Mass:            " << def.mass/CLHEP::GeV << " GeV" << G4endl;
   out << "Charge:          " << def.charge                       << " e" << G4endl;
   out << "Total Energy:    " << def.totalEnergy/CLHEP::GeV       << " GeV" << G4endl;
   out << "Kinetic Energy:  " << def.kineticEnergy/CLHEP::GeV     << " GeV" << G4endl;
@@ -69,4 +73,25 @@ std::ostream& operator<<(std::ostream& out, const BDSParticleDefinition& def)
   return out;
 }
 
+void BDSParticleDefinition::CalculateMomentum()
+{
+  try
+    {momentum = std::sqrt(std::pow(totalEnergy,2) - std::pow(mass,2));}
+  catch (std::domain_error) // sqrt(-ve)
+    {
+      G4cerr << __METHOD_NAME__ << "Total energy insufficient to include mass or particle" << G4endl;
+      exit(1);
+    }
+}
 
+void BDSParticleDefinition::CalculateRigidity(const G4double& ffact)
+{
+  // magnetic rigidity (brho)
+  // formula: B(Tesla)*rho(m) = p(GeV)/(0.299792458 * charge(e))
+  // charge (in e units); rigidity (in T*m)
+  if (BDS::IsFinite(charge))
+    {
+      brho = ffact * momentum / CLHEP::GeV / BDS::cOverGeV / charge;
+      brho *= CLHEP::tesla*CLHEP::m; // rigidity (in Geant4 units)
+    }
+}
