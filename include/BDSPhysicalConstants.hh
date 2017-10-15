ff65eb407d1580069f475cc702140ee0b0f279ba
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2017-10-15 20:39:26 +0200

    Fix.
    Former-commit-id: 48c2fb375f1a4501da762858241af44018ffe7d6

--- include/BDSPhysicalConstants.hh
+++ include/BDSPhysicalConstants.hh
@@ -3,6 +3,9 @@
 
 #include "G4Types.hh"
 
+#include "CLHEP/Units/SystemOfUnits.h"
+#include "CLHEP/Units/PhysicalConstants.h"
+
 namespace BDS
 {
   /// speed of light / 1 GeV, used for scaling in brho calculation
