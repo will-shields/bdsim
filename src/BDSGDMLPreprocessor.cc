cfb57197f91c2e54967116fafd976423164c8c9e
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2017-12-03 10:36:55 +0100

    Import of Stewart's gdml preprocessor.
    Former-commit-id: cd504cd91ed6b5fb19d3ee8ab732ce138c8e9157

---
+++ src/BDSGDMLPreprocessor.cc
@@ -0,0 +1,172 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2017.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "BDSGDMLPreprocessor.hh"
+
+#include <xercesc/parsers/XercesDOMParser.hpp>
+#include <xercesc/dom/DOM.hpp>
+#include <xercesc/sax/HandlerBase.hpp>
+#include <xercesc/util/XMLString.hpp>
+#include <xercesc/util/PlatformUtils.hpp>
+#include <xercesc/framework/StdOutFormatTarget.hpp>
+#include <xercesc/framework/LocalFileFormatTarget.hpp>
+
+#include "globals.hh"
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include <algorithm>
+
+using namespace xercesc;
+
+G4String BDS::PreprocessGDML(const G4String& file,
+			     const G4String& prefix)
+{
+  try
+    {XMLPlatformUtils::Initialize();}
+  catch (const XMLException& toCatch)
+    {
+      char* message = XMLString::transcode(toCatch.getMessage());
+      cout << "Error during initialization! :\n"
+	   << message << "\n";
+      XMLString::release(&message);
+      exit(1);
+    }
+  
+  XercesDOMParser* parser = new XercesDOMParser();
+  parser->setValidationScheme(XercesDOMParser::Val_Always);
+  parser->setDoNamespaces(true);    // optional
+
+  ErrorHandler* errHandler = (ErrorHandler*) new HandlerBase();
+  parser->setErrorHandler(errHandler);
+
+  //  char* xmlFile = "./iba/GDML/B1G_Coils.gdml;
+  char *xmlFile = args[1];
+  char *prefix  = args[2];
+
+  try
+    {parser->parse(xmlFile);}
+  catch (const XMLException& toCatch)
+    {
+      char* message = XMLString::transcode(toCatch.getMessage());
+      cout << "Exception message is: \n"
+	   << message << "\n";
+      XMLString::release(&message);
+      exit(1);
+    }
+  catch (const DOMException& toCatch)
+    {
+      char* message = XMLString::transcode(toCatch.msg);
+      cout << "Exception message is: \n"
+	   << message << "\n";
+      XMLString::release(&message);
+      exit(1);
+    }
+  catch (...)
+    {
+    cout << "Unexpected Exception \n" ;
+    return -1;
+    }
+  
+  // walk through all nodes to extract names and attributes
+  DOMDocument *doc           = parser->getDocument();
+  DOMElement *docRootNode    = doc->getDocumentElement();
+  DOMNodeIterator *docWalker = doc->createNodeIterator(docRootNode, DOMNodeFilter::SHOW_ELEMENT,NULL,true);
+
+  std::vector<std::string> names;
+  std::map<std::string,int> count;  
+
+  for (DOMNode *current_node = docWalker->nextNode(); current_node != 0; current_node = docWalker->nextNode())
+    {
+      string thisNodeName = XMLString::transcode(current_node->getNodeName());
+      DOMNamedNodeMap *attrMap = current_node->getAttributes();
+      
+      // loop over attributes
+      for(int i=0;i<attrMap->getLength();i++)
+	{
+	  DOMNode *attr = attrMap->item(i);
+	  // cout << thisNodeName << " " << XMLString::transcode(attr->getNodeName()) << " " <<XMLString::transcode(attr->getNodeValue()) << std::endl;
+	  string name = XMLString::transcode(attr->getNodeValue());
+	  
+	  if(XMLString::compareIString(attr->getNodeName(),XMLString::transcode("name")) == 0)
+	    {
+	      names.push_back(name);
+	      count[name] = 0;
+	    }
+	  else
+	    {
+	      count[name]++;
+	    }
+	}
+    } 
+  
+  // print label and reference usage
+  for(vector<string>::iterator i = names.begin(); i != names.end(); ++i)
+    {
+      cout << *i << " " << count[*i] << endl;
+    }
+
+  // walk through nodes again to replace names and references
+  docWalker->detach();
+  docWalker = doc->createNodeIterator(docRootNode, DOMNodeFilter::SHOW_ELEMENT,NULL,true);
+  
+  for (DOMNode *current_node = docWalker->nextNode(); current_node != 0; current_node = docWalker->nextNode())
+    {
+      DOMNamedNodeMap *attrMap = current_node->getAttributes();
+      
+      // loop over attributes
+      for(int i=0;i<attrMap->getLength();i++)
+	{
+	  DOMNode *attr = attrMap->item(i);
+	  string name = XMLString::transcode(attr->getNodeValue());
+	  
+	  if(XMLString::compareIString(attr->getNodeName(),XMLString::transcode("name")) == 0)
+	    {
+	      string name = XMLString::transcode(attr->getNodeValue());
+	      name = prefix+string("_")+name;
+	      attr->setNodeValue(XMLString::transcode(name.c_str()));
+	    }
+	  else
+	    {
+	      string value = XMLString::transcode(attr->getNodeValue());
+	      if(find(names.begin(), names.end(), value) != names.end()) {
+		attr->setNodeValue(XMLString::transcode((string(prefix)+string("_")+value).c_str()));
+	      }
+	    }
+	}
+    }
+  
+  docWalker->detach();
+
+  // write file from DOM
+  DOMImplementation *pImplement        = DOMImplementationRegistry::getDOMImplementation(XMLString::transcode("LS"));
+  DOMLSSerializer   *pSerializer       = ((DOMImplementationLS*)pImplement)->createLSSerializer();
+  DOMConfiguration  *pDomConfiguration = pSerializer->getDomConfig();
+  pDomConfiguration->setParameter(XMLUni::fgDOMWRTFormatPrettyPrint, true);
+  XMLFormatTarget   *pTarget           = new LocalFileFormatTarget("./test.xml");
+  DOMLSOutput       *pDomLsOutput      = ((DOMImplementationLS*)pImplement)->createLSOutput();
+  pDomLsOutput->setByteStream(pTarget);  
+  pSerializer->write(doc, pDomLsOutput);
+
+  delete parser;
+  delete errHandler;
+  return G4String("temp");
+}
+
+
