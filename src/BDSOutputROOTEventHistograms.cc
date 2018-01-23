3afce4dbc6157a615e2cc191a7fc0e0a2d7d9356
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2018-01-23 20:57:24 +0100

    Code style matching.
    Former-commit-id: 537ae37fffda54e55e778fcdd189bce25067b743

--- src/BDSOutputROOTEventHistograms.cc
+++ src/BDSOutputROOTEventHistograms.cc
@@ -51,8 +51,8 @@ BDSOutputROOTEventHistograms::~BDSOutputROOTEventHistograms()
 G4int BDSOutputROOTEventHistograms::Create1DHistogram(G4String name, G4String title,
                                                       G4int nbins, G4double xmin, G4double xmax)
 {
-  this->histograms1D.push_back(new TH1D(name,title, nbins, xmin, xmax));
-  return (G4int)this->histograms1D.size();
+  histograms1D.push_back(new TH1D(name,title, nbins, xmin, xmax));
+  return (G4int)histograms1D.size();
 }
 
 G4int BDSOutputROOTEventHistograms::Create1DHistogram(G4String name, G4String title,
@@ -60,22 +60,21 @@ G4int BDSOutputROOTEventHistograms::Create1DHistogram(G4String name, G4String ti
 {
 
   Double_t* edgesD = new Double_t[edges.size()];
-  for(int i=0;i<(int)edges.size();++i) {
-    edgesD[i] = edges[i];
-//    G4cout << edgesD[i] << G4endl;
-  }
-  this->histograms1D.push_back(new TH1D(name,title,(Int_t)edges.size()-1,edgesD));
+  for(int i=0;i<(int)edges.size();++i)
+    {edgesD[i] = edges[i];}
+    
+  histograms1D.push_back(new TH1D(name,title,(Int_t)edges.size()-1,edgesD));
   delete[] edgesD;
 
-  return (G4int)this->histograms1D.size();
+  return (G4int)histograms1D.size();
 }
 
 G4int BDSOutputROOTEventHistograms::Create2DHistogram(G4String name, G4String title,
                                                       G4int nxbins, G4double xmin, G4double xmax,
                                                       G4int nybins, G4double ymin, G4double ymax)
 {
-  this->histograms2D.push_back(new TH2D(name,title, nxbins, xmin, xmax, nybins, ymin, ymax));
-  return (G4int)this->histograms2D.size();
+  histograms2D.push_back(new TH2D(name,title, nxbins, xmin, xmax, nybins, ymin, ymax));
+  return (G4int)histograms2D.size();
 }
 
 G4int BDSOutputROOTEventHistograms::Create2DHistogram(G4String name, G4String title,
@@ -83,20 +82,18 @@ G4int BDSOutputROOTEventHistograms::Create2DHistogram(G4String name, G4String ti
                                                       std::vector<double>& yedges)
 {
   Double_t* xedgesD = new Double_t[xedges.size()];
-  for(int i=0;i<(int)xedges.size();++i) {
-    xedgesD[i] = xedges[i];
-  }
+  for(int i=0;i<(int)xedges.size();++i)
+    {xedgesD[i] = xedges[i];}
 
   Double_t* yedgesD = new Double_t[yedges.size()];
-  for(int i=0;i<(int)yedges.size();++i) {
-    yedgesD[i] = yedges[i];
-  }
+  for(int i=0;i<(int)yedges.size();++i)
+    {yedgesD[i] = yedges[i];}
 
-  this->histograms2D.push_back(new TH2D(name.data(),title.data(), (Int_t)xedges.size()-1, xedgesD, (Int_t)yedges.size()-1, yedgesD));
+  histograms2D.push_back(new TH2D(name.data(),title.data(), (Int_t)xedges.size()-1, xedgesD, (Int_t)yedges.size()-1, yedgesD));
   delete[] xedgesD;
   delete[] yedgesD;
 
-  return (G4int)this->histograms2D.size();
+  return (G4int)histograms2D.size();
 }
 
 G4int BDSOutputROOTEventHistograms::Create3DHistogram(G4String name, G4String title,
@@ -117,19 +114,16 @@ G4int BDSOutputROOTEventHistograms::Create3DHistogram(G4String name, G4String ti
 						      std::vector<double>& zedges)
 {
   Double_t* xedgesD = new Double_t[xedges.size()];
-  for(int i=0;i<(int)xedges.size();++i) {
-    xedgesD[i] = xedges[i];
-  }
+  for(int i=0;i<(int)xedges.size();++i)
+    {xedgesD[i] = xedges[i];}
 
   Double_t* yedgesD = new Double_t[yedges.size()];
-  for(int i=0;i<(int)yedges.size();++i) {
-    yedgesD[i] = yedges[i];
-  }
+  for(int i=0;i<(int)yedges.size();++i)
+    {yedgesD[i] = yedges[i];}
 
   Double_t* zedgesD = new Double_t[zedges.size()];
-  for(int i=0;i<(int)zedges.size();++i) {
-    zedgesD[i] = zedges[i];
-  }
+  for(int i=0;i<(int)zedges.size();++i)
+    {zedgesD[i] = zedges[i];}
   
   histograms3D.push_back(new TH3D(name.data(),title.data(),
 				  (Int_t)xedges.size()-1, xedgesD,
@@ -141,13 +135,13 @@ G4int BDSOutputROOTEventHistograms::Create3DHistogram(G4String name, G4String ti
 void BDSOutputROOTEventHistograms::Fill1DHistogram(G4int histoId, G4double value,
                                                    G4double weight)
 {
-  this->histograms1D[histoId]->Fill(value,weight);
+  histograms1D[histoId]->Fill(value,weight);
 }
 
 void BDSOutputROOTEventHistograms::Fill2DHistogram(G4int histoId, G4double xValue, G4double yValue,
                                                    G4double weight)
 {
-  this->histograms2D[histoId]->Fill(xValue,yValue,weight);
+  histograms2D[histoId]->Fill(xValue,yValue,weight);
 }
 
 void BDSOutputROOTEventHistograms::Fill3DHistogram(G4int    histoId,
