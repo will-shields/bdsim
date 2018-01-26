52bf714394011a4e71801c2af5ee29a4e4b90b37
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2018-01-26 17:50:49 +0100

    Doxygen.
    Former-commit-id: 556eaa5a5f343f8921603e30e34f59546ebb173a

--- analysis/PerEntryHistogram.hh
+++ analysis/PerEntryHistogram.hh
@@ -26,17 +26,36 @@ class HistogramDef;
 class TChain;
 class TH1;
 
+/**
+ * @brief Holder for information to calculate per entry histograms.
+ *
+ * This creates a histogram per event and accumualtes the mean and variance
+ * for every bin in a cloned histogram. This produces the mean and variance
+ * per event. Minimum number of events required is 2. 
+ * 
+ * The algorithm used to calculate the mean and variance is one that supports
+ * online calculation and is numerically stable.
+ * 
+ * @author Laurie Nevay
+ */
 class PerEntryHistogram
 {
 public:
+  /// Constructor with a histogram definition and the chain to operate on.
   explicit PerEntryHistogram(const HistogramDef* definition,
 			     TChain*             chain);
   ~PerEntryHistogram(){;}
 
+  /// Create a histogram of the approprate dimensions for the currently loaded
+  /// event then add it to the online (ie running) means and variances.
   void AccumulateCurrentEntry();
 
+  /// Calculate the standard deviation from the online variance calculation
+  /// then create an output histogram in the member result and resultSTD.
   void Terminate();
 
+  /// Forwarding function - call Write on result histograms on the currently
+  /// open file.
   void Write();
 
   TChain*       chain;        ///< Cache of chain pointer that provides data.
@@ -54,8 +73,12 @@ public:
   bool          terminated;   ///< Record of whether the instance has been terminated (single use).
   
 private:
+  /// No need for the default constructor.
   PerEntryHistogram() = delete;
 
+  /// Function to accumulate for a single bin. Old mean and variance passed by const reference
+  /// and new value x is added to these. Resultant values of mean and variance are written to the
+  /// newMean and newVari parameters.
   void AccumulateSingleValue(const double&  oldMean,
 			     const double&  oldVari,
 			     const double&  x,
