76a5db16eadaedac8216307af7fea0ebd39aba7a
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2018-02-13 17:08:18 +0100

    Rename test macros and tests to be more inline.  Make semi-private ones public (by name convention).
    Former-commit-id: fed99d4d8085ff83600175fb886072150b20f344

--- cmake/Modules/comparator_test_macros.cmake
+++ cmake/Modules/comparator_test_macros.cmake
@@ -1,7 +1,7 @@
 # comparator ctests
 
 # run a comparison job
-macro(_comparatorTest testName refFile testFile)
+macro(comparator_test testName refFile testFile)
   add_test(NAME ${testName} COMMAND comparatorexec ${refFile} ${testFile})
   set_tests_properties(${test_name} PROPERTIES FAIL_REGULAR_EXPRESSION "TEST_FAILED")
 endmacro()
