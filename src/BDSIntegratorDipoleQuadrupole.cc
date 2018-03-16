1da2eb739c5663a143bf0624eb8dd42e5e2ba5a7
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2018-03-16 13:57:38 +0100

    Fix curvilinear coordinates transform usage of curvilinear world.  We must use this for tilted bends.  This also fixes bad tracking when a particle hits the beam pipe.
    Former-commit-id: 07ec4ab215f401f03bf354a42c20aa2a82d19300

--- src/BDSIntegratorDipoleQuadrupole.cc
+++ src/BDSIntegratorDipoleQuadrupole.cc
@@ -101,7 +101,7 @@ void BDSIntegratorDipoleQuadrupole::Stepper(const G4double yIn[6],
   // convert to true curvilinear
   G4ThreeVector globalPos   = G4ThreeVector(yIn[0], yIn[1], yIn[2]);
   G4ThreeVector globalMom   = G4ThreeVector(yIn[3], yIn[4], yIn[5]);
-  BDSStep       localCL     = GlobalToCurvilinear(strength, globalPos, globalMom, h, false, fcof);
+  BDSStep       localCL     = GlobalToCurvilinear(strength, globalPos, globalMom, h, true, fcof);
   G4ThreeVector localCLPos  = localCL.PreStepPoint();
   G4ThreeVector localCLMom  = localCL.PostStepPoint();
   G4ThreeVector localCLMomU = localCLMom.unit();
@@ -121,7 +121,7 @@ void BDSIntegratorDipoleQuadrupole::Stepper(const G4double yIn[6],
   OneStep(localCLPos, localCLMom, localCLMomU, h, fcof, localCLPosOut, localCLMomOut);
 
   // convert to global coordinates for output
-  BDSStep globalOut = CurvilinearToGlobal(strength, localCLPosOut, localCLMomOut, false, fcof);
+  BDSStep globalOut = CurvilinearToGlobal(strength, localCLPosOut, localCLMomOut, true, fcof);
 
   G4ThreeVector globalPosOut = globalOut.PreStepPoint();
   G4ThreeVector globalMomOut = globalOut.PostStepPoint();
