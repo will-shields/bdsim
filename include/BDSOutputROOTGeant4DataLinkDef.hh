1381cdf691f9c0d8f609930bc6eae3f3c9e22ed9
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2018-03-19 23:08:17 +0100

    Declare sub classes for root.
    Former-commit-id: 0f7b3eb88c63d807bc02ccd542115a6236d53d20

--- include/BDSOutputROOTGeant4DataLinkDef.hh
+++ include/BDSOutputROOTGeant4DataLinkDef.hh
@@ -17,3 +17,5 @@ You should have received a copy of the GNU General Public License
 along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
 */
 #pragma link C++ class BDSOutputROOTGeant4Data+;
+#pragma link C++ class BDSOutputROOTGeant4Data::ParticleInfo+;
+#pragma link C++ class BDSOutputROOTGeant4Data::IonInfo+;
