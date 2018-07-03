c9b5e163b3770e53020ffc7f6dfa35d97e160e53
Jochem Snuverink <jochem.snuverink@psi.ch>
2018-07-03 11:55:31 +0200

    fix #236 as discussed
    Former-commit-id: e0a9f794eaddcd3738a40ec1df670b7587c66c05

--- include/BDSMaterials.hh
+++ include/BDSMaterials.hh
@@ -56,7 +56,9 @@ public:
 protected:
   BDSMaterials();
   /// Add G4Material
-  void AddMaterial(G4Material* aMaterial,G4String aName);
+  void AddMaterial(G4Material* aMaterial, G4String aName);
+  /// Add alias to a material
+  void AddMaterial(G4String    aMaterial, G4String aName);
   /** Add materials
       @param[in] Z        atomic number
       @param[in] A        mole mass in g/mole
@@ -90,21 +92,14 @@ protected:
 		   std::list<G4String> components,
 		   std::list<Type> componentsFractions);
 
-  /// Check if material is defined
-  G4bool CheckMaterial(G4String aMaterial)const;
-  /// Check if element is defined
-  G4bool CheckElement(G4String aSymbol)const;
+  /// Return element if defined (nullptr if not)
+  G4Element* CheckElement(G4String aSymbol)const;
 
-  /// map of materials, convention name lowercase
-  std::map<G4String,G4Material*> materials; 
-  /// map of elements, convention name lowercase
-  std::map<G4String,G4Element*>  elements; 
 private:
   /// Singleton instance
   static BDSMaterials* _instance;
 
   ///@{ Methods called by constructor
-  void DefineElements();
   void DefineMetals();
   void DefineSuperconductors();
   void DefineNonMetalSolids();
@@ -120,6 +115,11 @@ private:
   void AddElement(G4Element* aElement,G4String aName);
   void AddElement(G4String aName, G4String aSymbol, G4double itsZ, G4double itsA);
   ///@}
+
+  /// map of materials, convention name lowercase
+  std::map<G4String,G4Material*> materials;
+  /// map of elements, no lowercase convention
+  std::map<G4String,G4Element*>  elements;
   /// Material tables for storing pointers
   std::vector<G4MaterialPropertiesTable*> propertiesTables;
   /// Create new properties table and store in vector
