8cc6bbe4647bde459af7103d2877360f166f91f4
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2018-10-01 09:25:52 +0200

    New volume exit hit.
    Former-commit-id: 00230f14b3c75ef631ef6629afac5c3d3f58a73c

---
+++ src/BDSVolumeExitHit.cc
@@ -0,0 +1,47 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2018.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "BDSVolumeExitHit.hh"
+
+#include "G4Allocator.hh"
+
+G4Allocator<BDSVolumeExitHit> BDSVolumeExitHitAllocator;
+
+BDSVolumeExitHit::BDSVolumeExitHit(G4double totalEnergyIn,
+				   G4double postStepKineticEnergyIn,
+				   G4double XIn,
+				   G4double YIn,
+				   G4double ZIn,
+				   G4double TIn,
+				   G4int    partIDIn,
+				   G4int    trackIDIn,
+				   G4int    parentIDIn,
+				   G4double weightIn,
+				   G4int    turnsTakenIn):
+  totalEnergy(totalEnergyIn),
+  postStepKineticEnergy(postStepKineticEnergyIn),
+  X(XIn),
+  Y(YIn),
+  Z(ZIn),
+  T(TIn),
+  partID(partIDIn),
+  trackID(trackIDIn),
+  parentID(parentIDIn),
+  weight(weightIn),
+  turnsTaken(turnsTakenIn)
+{;}
