779befb46aabb2ea3b7c8b2a7ceedf4a719ef30d
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2018-10-07 15:07:43 +0200

    Extra functions for extents to find minimums.
    Former-commit-id: d551217ef37ee7dec767e4207fd5121c6dd9d58a

--- src/BDSExtent.cc
+++ src/BDSExtent.cc
@@ -161,11 +161,26 @@ G4double BDSExtent::MaximumAbs() const
   return *std::max_element(exts.begin(), exts.end());
 }
 
+G4double BDSExtent::MinimumAbs() const
+{
+  std::vector<G4double> exts = {std::abs(extXNeg), extXPos,
+				std::abs(extYNeg), extYPos,
+				std::abs(extZNeg), extZPos};
+  return *std::min_element(exts.begin(), exts.end());
+}
+
 G4double BDSExtent::MaximumAbsTransverse() const
 {
   std::vector<G4double> exts = {std::abs(extXNeg), extXPos,
 				std::abs(extYNeg), extYPos};
   return *std::max_element(exts.begin(), exts.end());
+}
+
+G4double BDSExtent::MinimumAbsTransverse() const
+{
+  std::vector<G4double> exts = {std::abs(extXNeg), extXPos,
+				std::abs(extYNeg), extYPos};
+  return *std::min_element(exts.begin(), exts.end());
 }
 
 G4bool BDSExtent::Encompasses(const G4ThreeVector& point) const
