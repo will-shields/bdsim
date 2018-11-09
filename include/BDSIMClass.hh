19a31cc04b4716c832c41ce23f89b3cac6011e6d
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2018-11-09 13:53:28 +0100

    Make bdsim a class that can be initialised and called.
    Former-commit-id: 36f6796f55a1d49a2cee469341075d504aebf8b6

---
+++ include/BDSIMClass.hh
@@ -0,0 +1,63 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2018.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef BDSIMCLASS_H
+#define BDSIMCLASS_H
+
+class BDSBunch;
+class BDSOutput;
+class BDSParser;
+class BDSRunManager;
+
+class BDSIM
+{
+public:
+  BDSIM();
+  BDSIM(int argc, char** argv, bool usualPrintOut=true);
+  ~BDSIM();
+  
+  int Initialise();
+  inline bool Initialised() const {return initialised;};
+
+  void BeamOn(int nGenerate=-1);
+
+  /*
+  void TrackEnergy(double pdgID, double totalEnergy,
+		   double x, double px,
+		   double y, double py,
+		   double s, double t);
+
+  void TrackMomentum(double pdgID, double momentum,
+		     double x, double px,
+		     double y, double py,
+		     double s, double t);
+  */
+private:
+  bool   ignoreSIGINT;
+  bool   usualPrintOut;
+  bool   initialised;
+  int    argcCache;
+  char** argvCache;
+
+  BDSParser*     parser;
+  BDSOutput*     bdsOutput;
+  BDSBunch*      bdsBunch;
+  BDSRunManager* runManager;
+};
+
+#endif
