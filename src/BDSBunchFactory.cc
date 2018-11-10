c58d4a4aa9cb96923230f351ef24e478115dba3e
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2018-11-10 14:47:05 +0100

    Fix includes given changes to gzstream package location.
    Former-commit-id: 15c9d0b06cd60ec2027a8daffe39db8948d2cf02

--- src/BDSBunchFactory.cc
+++ src/BDSBunchFactory.cc
@@ -37,7 +37,7 @@ along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
 #include "parser/beam.h"
 
 #ifdef USE_GZSTREAM
-#include "gzstream.h"
+#include "src-external/gzstream/gzstream.h"
 #endif
 
 BDSBunch* BDSBunchFactory::CreateBunch(const BDSParticleDefinition* beamParticle,
