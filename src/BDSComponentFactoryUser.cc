8849624484a02a4731ea25b6f7f59fd20d010065
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2018-11-12 17:14:51 +0100

    Introduce a user component factory class.
    Former-commit-id: cc9f611b8240fdbe76972d6ed720c1e579ae7d40

---
+++ src/BDSComponentFactoryUser.cc
@@ -0,0 +1,65 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2018.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "BDSComponentConstructor.hh"
+#include "BDSComponentFactoryUser.hh"
+
+#include "globals.hh"
+
+#include <map>
+#include <string>
+
+BDSComponentFactoryUser::BDSComponentFactoryUser():
+  brho(0),
+  beta0(0)
+{;}
+
+BDSComponentFactoryUser::~BDSComponentFactoryUser()
+{;}
+
+void BDSComponentFactoryUser::SetRigidityAndLorentzBeta(const G4double brhoIn,
+							const G4double beta0In)
+{
+  brho = brhoIn;
+  beta0 = beta0In;
+}
+
+void BDSComponentFactoryUser::RegisterComponent(G4String componentTypeName,
+						BDSComponentConstructor* componentConstructor)
+{
+  userFunctions[componentTypeName] = componentConstructor;
+}
+
+G4bool BDSComponentFactoryUser::CanConstructComponentByName(G4String componentTypeName) const 
+{
+  return userFunctions.find(componentTypeName) != userFunctions.end();
+}
+
+BDSAcceleratorComponent* BDSComponentFactoryUser::ConstructComponent(G4String componentTypeName,
+								     GMAD::Element const* elementIn,
+								     GMAD::Element const* prevElementIn,
+								     GMAD::Element const* nextElementIn,
+								     G4double currentArcLengthIn)
+{
+  return userFunctions[componentTypeName]->Construct(elementIn,
+						     prevElementIn,
+						     nextElementIn,
+						     currentArcLengthIn,
+						     brho,
+						     beta0);
+}
