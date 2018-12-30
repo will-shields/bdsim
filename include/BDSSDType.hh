cc18be0380cf8840b5cfda8d59202ad5b4151a74
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2018-12-30 16:51:54 +0000

    Start of enum for sensitive detectors - need more flexibility on what's assigned to what.
    Former-commit-id: 839cd77d7e3e2dad5ac3d0ec6d7a99a062fd1b0d

---
+++ include/BDSSDType.hh
@@ -0,0 +1,47 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2018.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef BDSSDTYPE_H
+#define BDSSDTYPE_H
+
+#include "BDSTypeSafeEnum.hh"
+#include "globals.hh"         // geant4 globals / types
+
+/**
+ * @brief Type definition for sensitive detectors.
+ * 
+ * @author Laurie Nevay
+ */
+
+struct sdtypes_def
+{
+  enum type {samplerplane, samplercylinder, energydep, energydeptunnel, energydepworld,
+	     worldexit, worldcomplete, collimator, collimatorcomplete};
+};
+
+typedef BDSTypeSafeEnum<sdtypes_def,int> BDSSDType;
+
+/*
+namespace BDS
+{
+  /// Function that gives corresponding enum value for string (case-insensitive).
+  BDSSDType DetermineSDType(const G4String& sdType);
+}
+*/
+
+#endif
