749671c820422615bec40a119c480af1687fdf26
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2019-01-06 22:53:28 +0100

    Tidying.
    Former-commit-id: ceb2f1838394f0acc5822ef63dc6fe43808d6a73

--- include/BDSOutputStructures.hh
+++ include/BDSOutputStructures.hh
@@ -136,9 +136,9 @@ protected:
   BDSOutputROOTEventInfo*       evtInfo;    ///< Event information.
 
   std::vector<BDSOutputROOTEventCollimator*> collimators; ///< Collimator output struectures.
-  std::vector<std::string> collimatorNames; ///< Names of collimators in output structures.
-  G4int nCollimators;                       ///< Number of collimators in beam line.
-  std::vector<G4int> collimatorIndices;     ///< Indices in the beam line that are collimators.
+  std::vector<std::string>  collimatorNames;   ///< Names of collimators in output structures.
+  G4int                     nCollimators;      ///< Number of collimators in beam line.
+  std::vector<G4int>        collimatorIndices; ///< Indices in beam line that are collimators.
   std::map<G4String, G4int> collimatorIndicesByName; ///< Indices mapped to their name.
   
 private:
