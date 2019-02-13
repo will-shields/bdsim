fc4455bbeb62c0938b3a47ceda3bbfb07cfca3bd
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2019-02-13 16:31:43 +0100

    Introduce a basic exception with message and possible name.
    Former-commit-id: 7bfa329bb1fcd8fa2027b1c77d18e425a6298252

---
+++ include/BDSException.hh
@@ -0,0 +1,59 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2019.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef BDSEXCEPTION_H
+#define BDSEXCEPTION_H
+
+#include <exception>
+#include <string>
+
+/**
+ * @brief General exception with possible name of object and message.
+ * 
+ * Provide optional name of objec the message is associated with. This
+ * Can be appended later with SetName() function. 
+ *
+ * @author Laurie Nevay
+ */
+
+class BDSException: public std::exception
+{
+public:
+  BDSException(const std::string& messageIn):
+    name(""),
+    message(messageIn)
+  {;}
+  BDSException(const std::string& nameIn, const std::string& messageIn):
+    name(nameIn),
+    message(messageIn)
+  {;}
+  virtual ~BDSException(){;}
+
+  /// Override message in std::exception.
+  const char* what() const noexcept override {return name.empty() ? message.c_str() : (name + " : " + message).c_str();}
+
+  /// Allow setting of name later.
+  void SetName(const std::string& nameIn) {name = nameIn;}
+
+  /// @{ Data to print.
+  std::string name;
+  std::string message;
+  /// @}
+};
+
+#endif
