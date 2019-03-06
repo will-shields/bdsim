a98b9c8e08ed962750a8230f0d7f7c4610ce11bb
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2019-03-06 09:28:14 +0100

    Add missing files for model-model example.
    Former-commit-id: 41a09965493d29b1329288338b069e5f8e70d589

--- examples/model-model/bdsim/convert.py
+++ examples/model-model/bdsim/convert.py
@@ -6,18 +6,18 @@ import pymadx
 import makecollimatorgaps
 
 # aperture information
-ap = pymadx.Data.Aperture('../madx/ring_aperture.tfs.tar.gz')
+ap = pymadx.Data.Aperture('../madx/ring_aperture.tfs.gz')
 ap = ap.RemoveBelowValue(0.005)
 
 # collimator settings
 # uses pybdsim.Data.BDSAsciiData which behaves like dictionaries of dictionaries
 cols = pybdsim.Data.Load('collimatorSettings.dat')
 
-a,b,c = pybdsim.Convert.MadxTfs2Gmad('../madx/ring.tfs.tar.gz', 'linear/bmm-linear', linear=True, aperturedict=ap, collimatordict=cols)
+a,b,c = pybdsim.Convert.MadxTfs2Gmad('../madx/ring.tfs.gz', 'linear/bmm-linear', linear=True, aperturedict=ap, collimatordict=cols)
 
-a,b,c = pybdsim.Convert.MadxTfs2Gmad('../madx/ring.tfs.tar.gz', 'nl/bmm-nl', aperturedict=ap, collimatordict=cols)
+a,b,c = pybdsim.Convert.MadxTfs2Gmad('../madx/ring.tfs.gz', 'nl/bmm-nl', aperturedict=ap, collimatordict=cols)
 
-a,b,c = pybdsim.Convert.MadxTfs2Gmad('../madx/ring.tfs.tar.gz', 'halo/bmm-nl-halo', aperturedict=ap, collimatordict=cols)
+a,b,c = pybdsim.Convert.MadxTfs2Gmad('../madx/ring.tfs.gz', 'halo/bmm-nl-halo', aperturedict=ap, collimatordict=cols)
 
 # make collimator beams in gmad
 import makecollimatorbeams
