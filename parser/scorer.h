2849ce935bc9301e249fd7b0fc24343f6d9d3872
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2019-04-02 09:46:26 +0200

    Scorer structure for parser.
    Former-commit-id: a8b6a9c979ae6bc68ab260f22152fce517ac3690

---
+++ parser/scorer.h
@@ -0,0 +1,80 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2019.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef SCORER_H
+#define SCORER_H
+
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+#include "published.h"
+
+namespace GMAD
+{
+  /**
+   * @brief Scorer class for parser
+   * 
+   * @author Laurie Nevay
+   */
+  
+  class Scorer : public Published<Scorer>
+  {
+  public:
+    std::string name;                 ///< Name.
+    std::string type;
+    std::string particle;
+    double      minimumEnergy;
+    double      maximumEnergy;
+    std::string conversionFactorFile;
+    
+    /// Constructor
+    Scorer();
+    /// Reset
+    void clear();
+    /// Print some properties
+    void print()const;
+    /// Set methods by property name and value
+    template <typename T>
+    void set_value(std::string property, T value);
+
+  private:
+    /// publish members
+    void PublishMembers();
+  };
+  
+  template <typename T>
+    void Scorer::set_value(std::string property, T value)
+    {
+#ifdef BDSDEBUG
+      std::cout << "field> Setting value " << std::setw(25) << std::left << property << value << std::endl;
+#endif
+      // member method can throw runtime_error, catch and exit gracefully
+      try
+	{
+	  set(this,property,value);
+	}
+      catch(const std::runtime_error&)
+	{
+	  std::cerr << "Error: field> unknown option \"" << property << "\" with value " << value  << std::endl;
+	  exit(1);
+	}
+    }
+}
+
+#endif
