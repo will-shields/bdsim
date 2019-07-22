d3a34441d47fad41bce850a02cbee589142bbed1
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2019-07-22 18:42:43 +0200

    Remove explicit setting of field for BDSElement.  It gets it anyway in the generic way that other elements do.  Fixes the rotation for it to work like the others.  Tidying.
    Former-commit-id: 4e8876d1734aa06c052bcea7b3578c53ce244c70

--- src/BDSElement.cc
+++ src/BDSElement.cc
@@ -19,25 +19,20 @@ along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
 #include "BDSDebug.hh"
 #include "BDSExtent.hh"
 #include "BDSElement.hh"
-#include "BDSFieldBuilder.hh"
-#include "BDSFieldFactory.hh"
+#include "BDSException.hh"
 #include "BDSGeometryExternal.hh"
 #include "BDSGeometryFactory.hh"
 
 #include "globals.hh" // geant4 globals / types
 
-class BDSFieldInfo;
-
 BDSElement::BDSElement(G4String nameIn,
 		       G4double arcLengthIn,
 		       G4double horizontalWidthIn,
 		       G4String geometryIn,
-		       G4String fieldNameIn,
 		       G4double angleIn):
   BDSAcceleratorComponent(nameIn, arcLengthIn, angleIn, "element"),
   horizontalWidth(horizontalWidthIn),
-  geometryFileName(geometryIn),
-  fieldName(fieldNameIn)
+  geometryFileName(geometryIn)
 {;}
 
 void BDSElement::BuildContainerLogicalVolume()
@@ -48,11 +43,7 @@ void BDSElement::BuildContainerLogicalVolume()
 									    chordLength, horizontalWidth);
   
   if (!geom)
-    {
-      G4cerr << __METHOD_NAME__ << "Error loading geometry in component \""
-	     << name << "\"" << G4endl;
-      exit(1);
-    }
+    {throw BDSException(__METHOD_NAME__, "Error loading geometry in component \"" + name + "\"");}
   
   // We don't register the geometry as a daughter as the geometry factory retains
   // ownership of the geometry and will clean it up at the end.
@@ -79,10 +70,6 @@ void BDSElement::BuildContainerLogicalVolume()
 	     << " of the element, which will cause overlaps!" << G4endl
 	     << "Calculated extent along z: " << extLength << " mm, vs specified "
 	     << chordLength << G4endl;
-      //exit(1);
+      throw BDSException(__METHOD_NAME__, "overlaps in element \"" + name + "\"");
     }
-
-  // Get the field definition from the parser
-  // Note, the field factory manages the deletion of this info instance.
-  SetField(BDSFieldFactory::Instance()->GetDefinition(fieldName));
 }
