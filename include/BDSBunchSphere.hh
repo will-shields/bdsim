c77eaaafca6c2f570fbb3c09ce9aaa92f527d983
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2019-08-06 09:17:04 +0200

    Implement sphere bunch.
    Former-commit-id: 3243b037bd2f96447f2f1b007831b92fa748b71a

---
+++ include/BDSBunchSphere.hh
@@ -0,0 +1,47 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2019.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef BDSBUNCHSPHERE_H
+#define BDSBUNCHSPHERE_H 
+
+#include "BDSBunch.hh"
+
+/**
+ * @brief A bunch distribution that produces an uncorrelated uniform
+ * random distribution over a sphere.
+ * 
+ * @author Laurie Nevay
+ */
+
+class BDSBunchSphere: public BDSBunch
+{ 
+public: 
+  BDSBunchSphere(); 
+  virtual ~BDSBunchSphere(){;}
+  
+  /// @{ Assignment and copy constructor not implemented nor used
+  BDSBunchSphere& operator=(const BDSBunchSphere&) = delete;
+  BDSBunchSphere(BDSBunchSphere&) = delete;
+  /// @}
+
+  /// Generate a random unit vector for direction and use reference spatial
+  /// coordinates. No energy spread.
+  virtual BDSParticleCoordsFull GetNextParticleLocal();
+};
+
+#endif
---
+++ src/BDSBunchSphere.cc
@@ -0,0 +1,35 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2019.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "BDSBunchSphere.hh"
+#include "BDSParticleCoordsFull.hh"
+
+#include "globals.hh"
+#include "G4RandomDirection.hh"
+
+BDSBunchSphere::BDSBunchSphere()
+{;}
+
+BDSParticleCoordsFull BDSBunchSphere::GetNextParticleLocal()
+{
+  G4ThreeVector momentumUnitVector = G4RandomDirection();
+  G4double xp = momentumUnitVector.x();
+  G4double yp = momentumUnitVector.y();
+  G4double zp = momentumUnitVector.z();  
+  return BDSParticleCoordsFull(X0,Y0,Z0,xp,yp,zp,T0,S0,E0,/*weight=*/1.0);
+}
