ae6e13dc606cc2a54b5ac5bad0e83bde7c1ce1c6
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2019-08-29 16:42:33 +0200

    Fix print out for usage.
    Former-commit-id: 89bb587cef9761b3d2796b4be694b9bc810b8650

--- analysis/rebdsimOptics.cc
+++ analysis/rebdsimOptics.cc
@@ -33,11 +33,11 @@ along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
 
 void usage()
 { 
-  std::cout << "usage: rebdsimOptics <dataFile> <outputfile> <--emittanceOnFly>" << std::endl;
-  std::cout << " <datafile>   - root file to operate on ie run1.root"            << std::endl;
-  std::cout << " <outputfile> - name of output file ie optics.dat"               << std::endl;
-  std::cout << " --emittanceOnFly - calculate emittance per sampler (optional)"  << std::endl;
-  std::cout << "Quotes should be used if * is used in the input file name."      << std::endl;
+  std::cout << "usage: rebdsimOptics <dataFile> <outputfile> <--emittanceOnFly>"    << std::endl;
+  std::cout << " <datafile>   - root file to operate on ie run1.root"               << std::endl;
+  std::cout << " <outputfile> - name of output file ie optics.dat"                  << std::endl;
+  std::cout << " --emittanceOnTheFly - calculate emittance per sampler (optional)"  << std::endl;
+  std::cout << "Quotes should be used if * is used in the input file name."         << std::endl;
 }
 
 int main(int argc, char* argv[])
