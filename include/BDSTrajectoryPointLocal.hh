aee6d92eb5d47efb97214b4a1095424001b33005
siobhanalden <siobhanalden@gmail.com>
2019-09-04 13:36:11 +0100

    removing unnecessary header include
    Former-commit-id: 067bcc4c746c5c440d4697ad21deabea194582c5

--- include/BDSTrajectoryPointLocal.hh
+++ include/BDSTrajectoryPointLocal.hh
@@ -19,7 +19,6 @@ along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef BDSTRAJECTORYPOINTLOCAL_H
 #define BDSTRAJECTORYPOINTLOCAL_H
 
-#include "globals.hh" // geant4 types / globals
 #include "G4Allocator.hh"
 #include "G4ThreeVector.hh"
 
