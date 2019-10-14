ff1debe11206733eff14e8fd98d253082a68d39b
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2019-10-14 14:00:44 +0200

    New options from Stuart Walker for to filter aperture and collimator hits - minimum kinetic energy required.
    Former-commit-id: 45ac591c6eb89a6d763ce8f6af7724ef74a7a8cd

--- include/BDSGlobalConstants.hh
+++ include/BDSGlobalConstants.hh
@@ -198,17 +198,19 @@ public:
   inline G4bool   StoreApertureImpacts()     const {return G4bool  (options.storeApertureImpacts);}
   inline G4bool   StoreApertureImpactsIons() const {return G4bool  (options.storeApertureImpactsIons);}
   inline G4bool   StoreApertureImpactsAll()  const {return G4bool  (options.storeApertureImpactsAll);}
+  inline G4double ApertureImpactsMinimumKE() const {return G4double(options.apertureImpactsMinimumKE*CLHEP::GeV);}
   inline G4bool   StoreCollimatorInfo()      const {return G4bool  (options.storeCollimatorInfo);}
   inline G4bool   StoreCollimatorHits()      const {return G4bool  (options.storeCollimatorHits);}
   inline G4bool   StoreCollimatorHitsLinks() const {return G4bool  (options.storeCollimatorHitsLinks);}
   inline G4bool   StoreCollimatorHitsIons()  const {return G4bool  (options.storeCollimatorHitsIons);}
   inline G4bool   StoreCollimatorHitsAll()   const {return G4bool  (options.storeCollimatorHitsAll);}
+  inline G4double CollimatorHitsMinimumKE()  const {return G4double(options.collimatorHitsMinimumKE*CLHEP::GeV);}
   inline G4bool   StoreELoss()               const {return G4bool  (options.storeEloss);}
   inline G4bool   StoreELossHistograms()     const {return G4bool  (options.storeElossHistograms);}
   inline G4bool   StoreELossVacuum()         const {return G4bool  (options.storeElossVacuum);}
-  inline G4bool   StoreELossVacuumHistograms() const {return G4bool (options.storeElossVacuumHistograms);}
+  inline G4bool   StoreELossVacuumHistograms()const{return G4bool  (options.storeElossVacuumHistograms);}
   inline G4bool   StoreELossTunnel()         const {return G4bool  (options.storeElossTunnel);}
-  inline G4bool   StoreELossTunnelHistograms() const {return G4bool (options.storeElossTunnelHistograms);}
+  inline G4bool   StoreELossTunnelHistograms()const{return G4bool  (options.storeElossTunnelHistograms);}
   inline G4bool   StoreELossWorld()          const {return G4bool  (options.storeElossWorld);}
   inline G4bool   StoreELossWorldContents()  const {return G4bool  (options.storeElossWorldContents);}
   inline G4bool   StoreELossTurn()           const {return G4bool  (options.storeElossTurn || options.circular);}
@@ -217,7 +219,7 @@ public:
   inline G4bool   StoreELossGlobal()         const {return G4bool  (options.storeElossGlobal);}
   inline G4bool   StoreELossTime()           const {return G4bool  (options.storeElossTime);}
   inline G4bool   StoreELossStepLength()     const {return G4bool  (options.storeElossStepLength);}
-  inline G4bool   StoreELossPreStepKineticEnergy() const {return G4bool  (options.storeElossPreStepKineticEnergy);}
+  inline G4bool   StoreELossPreStepKineticEnergy() const {return G4bool (options.storeElossPreStepKineticEnergy);}
   inline G4bool   StoreELossModelID()        const {return G4bool  (options.storeElossModelID);}
   inline G4bool   StoreGeant4Data()          const {return G4bool  (options.storeGeant4Data);}
   inline G4bool   StoreTrajectory()          const {return G4bool  (options.storeTrajectory);}
