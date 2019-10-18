5b4da7333b15099733b7f8af4e8f1cf304d9a1be
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2019-10-18 13:29:44 +0200

    AND filter for SDs.
    Former-commit-id: ccf64058ebf2d4616a6e3c46f4ffe457dec25939

---
+++ include/BDSSDFilterAnd.hh
@@ -0,0 +1,64 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2019.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef BDSSDFILTERAND_H
+#define BDSSDFILTERAND_H
+
+#include "G4String.hh"
+#include "G4Types.hh"
+#include "G4VSDFilter.hh"
+
+#include <vector>
+
+/**
+ * @brief Filter that applies AND to a vector of filters.
+ * 
+ * Can optionally own the filters. If the ownsTheFiltersIn argument
+ * is true, they'll be deleted afterwards.
+ * 
+ * @author Laurie Nevay
+ */
+
+class BDSSDFilterAnd: public G4VSDFilter
+{
+public:
+  explicit BDSSDFilterAnd(const G4String& name,
+			  G4bool ownsTheFiltersIn = false);
+  virtual ~BDSSDFilterAnd();
+
+  /// Return logical AND of all filters Accept(). If no filters are registered,
+  /// it will return false and not accept anything.
+  virtual G4bool Accept(const G4Step* step) const;
+
+  /// Register the filter.
+  inline void RegisterFilter(G4VSDFilter* filterIn) {filters.push_back(filterIn);}
+
+  /// Accessor.
+  inline const std::vector<G4VSDFilter*>& Filters() const {return filters;}
+
+private:
+  BDSSDFilterAnd() = delete;
+
+  /// Whether to delete the filters or not at the end.
+  G4bool ownsTheFilters;
+
+  /// Vector of all filters.
+  std::vector<G4VSDFilter*> filters;
+};
+
+#endif
---
+++ src/BDSSDFilterAnd.cc
@@ -0,0 +1,49 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2019.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "BDSSDFilterAnd.hh"
+
+#include "globals.hh"
+#include "G4Step.hh"
+#include "G4Track.hh"
+#include "G4VSDFilter.hh"
+
+#include <vector>
+
+BDSSDFilterAnd::BDSSDFilterAnd(const G4String& name,
+			       G4bool ownsTheFiltersIn):
+  G4VSDFilter(name),
+  ownsTheFilters(ownsTheFiltersIn)
+{;}
+
+BDSSDFilterAnd::~BDSSDFilterAnd()
+{
+  if (ownsTheFilters)
+    {
+      for (auto filter : filters)
+	{delete filter;}
+    }
+}
+
+G4bool BDSSDFilterAnd::Accept(const G4Step* step) const
+{
+  G4bool result = false;
+  for (const auto filter : filters)
+    {result &= filter->Accept(step);}
+  return result;
+}
