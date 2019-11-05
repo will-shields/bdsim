d8f23d9ae4e795e95eb38c8e8e0e385612e7b26f
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2019-11-05 14:32:48 +0100

    Protect against using gmad file as gdml file - common mistake.
    Former-commit-id: 5f796f9a531e21c908c33568da6c681c80ed264f

--- src/BDSGDMLPreprocessor.cc
+++ src/BDSGDMLPreprocessor.cc
@@ -49,6 +49,8 @@ G4String BDS::PreprocessGDML(const G4String& file,
 			     const G4String& prefix,
 			     G4bool          preprocessSchema)
 {
+  if (BDS::EndsWith(file, ".gmad"))
+    {throw BDSException(__METHOD_NAME__, "trying to read a GMAD file as a GDML file - check file or change extension.");}
   BDSGDMLPreprocessor processor;
   G4String processedFile = processor.PreprocessFile(file,
 						    prefix,
@@ -59,6 +61,8 @@ G4String BDS::PreprocessGDML(const G4String& file,
 G4String BDS::PreprocessGDMLSchemaOnly(const G4String& file)
 {
   // open file
+  if (BDS::EndsWith(file, ".gmad"))
+    {throw BDSException(__METHOD_NAME__, "trying to read a GMAD file as a GDML file - check file or change extension.");}
   std::ifstream inputFile;
   inputFile.open(file.c_str());
   if (!inputFile.is_open())
