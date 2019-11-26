f597122cd5a16a2551b92615def1c2b0d7021301
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2019-11-26 23:44:44 +0100

    Remove duplicate check of momentum.  Get momentum from particle definition class.
    Former-commit-id: ee06c5c36b88cc8d5b3aee0985d0203c8e518b1d

--- src/BDSBunch.cc
+++ src/BDSBunch.cc
@@ -98,13 +98,8 @@ void BDSBunch::SetOptions(const BDSParticleDefinition* beamParticle,
   finiteTilt   = BDS::IsFinite(tilt);
   finiteSigmaE = BDS::IsFinite(sigmaE);
   finiteSigmaT = BDS::IsFinite(sigmaT);
-  
-  // calculate momentum - used by some bunch distributions
-  G4double mass = beamParticle->Mass();
-  mass2 = std::pow(mass,2);
-  if (E0 <= mass)
-    {throw BDSException(__METHOD_NAME__, "E0 (central total energy) " + std::to_string(E0) + " MeV lower than mass of particle! " + std::to_string(mass) + " MeV");}
-  P0 = std::sqrt(std::pow(E0,2) - mass2); // E^2 = p^2 + m^2
+
+  P0 = particleDefinition->Momentum();
   sigmaP = (1./std::pow(beamParticle->Beta(),2)) * sigmaE; // dE/E = 1/(beta^2) dP/P
   if (finiteSigmaE)
     {G4cout << __METHOD_NAME__ << "sigmaE = " << sigmaE << " -> sigmaP = " << sigmaP << G4endl;}
