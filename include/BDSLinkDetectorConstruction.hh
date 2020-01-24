6ffd7766b7890b654dad065eda69bd9d2033957e
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2020-01-24 15:15:54 +0100

    Header fixes.
    Former-commit-id: 682a959655ad6f3b0e70dd2c74f779b8af037d86

--- include/BDSLinkDetectorConstruction.hh
+++ include/BDSLinkDetectorConstruction.hh
@@ -1,3 +1,21 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2020.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef BDSLINKDETECTORCONSTRUCTION_H
 #define BDSLINKDETECTORCONSTRUCTION_H
 
@@ -5,21 +23,14 @@
 
 class G4VPhysicalVolume;
 
-class BDSLinkDetectorConstruction : G4VUserDetectorConstruction
+class BDSLinkDetectorConstruction: public G4VUserDetectorConstruction
 {
 public:
-  //! Default constructor
-  DetectorConstruction(/* something. special GMAD syntax?*/);
+  /// Default constructor
+  BDSLinkDetectorConstruction(/* something. special GMAD syntax?*/);
 
   virtual ~BDSDetectorConstruction();
   virtual G4VPhysicalVolume* Construct();
-
-  
-protected:
-private:
-
 };
 
-// } // namespace BDSLINK
-
-#endif /* BDSLINKDETECTORCONSTRUCTION_H */
+#endif
--- include/BDSLinkOpaqueBox.hh
+++ include/BDSLinkOpaqueBox.hh
@@ -1,38 +1,55 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2020.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef LINKOPAQUEBOX_H
 #define LINKOPAQUEBOX_H
 
 #include "G4ThreeVector.hh"
-#include "G4types.hh"
+#include "G4Types.hh"
 
 #include "BDSExtent.hh"
 
 class BDSAcceleratorComponent;
 
-class LinkOpaqueBox : BDSGeometryComponent
+class BDSLinkOpaqueBox: public BDSGeometryComponent
 {
 public:
-  LinkOpaqueBox(BDSAcceleratorComponent*);
+  BDSLinkOpaqueBox(BDSAcceleratorComponent*);
 
-  BDSExtent getExtent();
+  BDSExtent GetExtent();
 
   //! Default constructor
-  LinkOpaqueBox() = delete;
+  BDSLinkOpaqueBox() = delete;
 
-  //! Copy constructor
-  LinkOpaqueBox(const LinkOpaqueBox &other) = default;
+  /// Copy constructor
+  BDSLinkOpaqueBox(const LinkOpaqueBox &other) = default;
 
-  //! Move constructor
-  LinkOpaqueBox(LinkOpaqueBox &&other) noexcept = default;
+  /// Move constructor
+  BDSLinkOpaqueBox(LinkOpaqueBox &&other) noexcept = default;
 
-  //! Destructor
+  /// Destructor
   virtual ~LinkOpaqueBox() noexcept = default;
 
-  //! Copy assignment operator
-  LinkOpaqueBox& operator=(const LinkOpaqueBox &other) = default;
+  /// Copy assignment operator
+  BDSLinkOpaqueBox& operator=(const BDSLinkOpaqueBox &other) = default;
 
-  //! Move assignment operator
-  LinkOpaqueBox& operator=(LinkOpaqueBox &&other) noexcept = default;
-
+  /// Move assignment operator
+  BDSLinkOpaqueBox& operator=(BDSLinkOpaqueBox &&other) noexcept = default;
 
 private:
   BDSAcceleratorComponent* component;
@@ -40,4 +57,4 @@ private:
   G4ThreeVector toStart;
 };
 
-#endif /* LINKOPAQUEBOX_H */
+#endif
