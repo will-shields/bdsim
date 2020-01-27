1543bb06ffeae6d8737db05ed2d8134a6026ddbe
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2020-01-27 17:29:15 +0100

    New primary generator action.
    Former-commit-id: 92f667eb00293ddd6fd5e15d3d3eca0f5287b520

---
+++ include/BDSPrimaryGeneratorActionLink.hh
@@ -0,0 +1,63 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2020.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef BDSPRIMARYGENERATORACTIONLINK_H
+#define BDSPRIMARYGENERATORACTIONLINK_H
+
+#include "BDSExtent.hh"
+
+#include "globals.hh"
+#include "G4VUserPrimaryGeneratorAction.hh"
+
+class BDSBunch;
+class BDSOutputLoader;
+class BDSPTCOneTurnMap;
+class G4Event;
+class G4ParticleGun;
+/**
+ * @brief Generates primary particle vertices using BDSBunch.
+ *
+ */
+
+class BDSPrimaryGeneratorActionLink: public G4VUserPrimaryGeneratorAction
+{
+public:
+  /// Bunch must have a valid particle definition (ie not nullptr).
+  BDSPrimaryGeneratorActionLink(BDSBunch* bunchIn);
+  virtual ~BDSPrimaryGeneratorActionLink();
+
+  /// Main interface for Geant4. Prepare primary(ies) for the event.
+  virtual void GeneratePrimaries(G4Event*);
+
+  /// Set the world extent that particle coordinates will be checked against.
+  inline void SetWorldExtent(const BDSExtent worldExtentIn) {worldExtent = worldExtentIn;}
+  
+private:  
+  
+  G4ParticleGun* particleGun;     ///< Geant4 particle gun that creates single particles.
+  BDSBunch* bunch;                ///< BDSIM particle generator.  
+  
+  /// World extent that particle coordinates are checked against to ensure they're inside it.
+  BDSExtent worldExtent;
+
+  /// Can only get a G4ParticleDefinition for an ion when primary generator is called
+  /// so cache the first time. This is the flag of that cache.
+  //G4bool ionCached;
+};
+
+#endif
---
+++ src/BDSPrimaryGeneratorActionLink.cc
@@ -0,0 +1,137 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2020.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "BDSBunch.hh"
+#include "BDSDebug.hh"
+#include "BDSEventInfo.hh"
+#include "BDSException.hh"
+#include "BDSExtent.hh"
+#include "BDSIonDefinition.hh"
+#include "BDSOutputLoader.hh"
+#include "BDSParticleDefinition.hh"
+#include "BDSPhysicsUtilities.hh"
+#include "BDSPrimaryGeneratorActionLink.hh"
+#include "BDSPrimaryVertexInformation.hh"
+#include "BDSRandom.hh"
+#include "BDSUtilities.hh"
+
+#include "CLHEP/Random/Random.h"
+
+#include "G4Event.hh"
+#include "G4IonTable.hh"
+#include "G4ParticleGun.hh"
+#include "G4ParticleDefinition.hh"
+#include "G4Types.hh"
+
+BDSPrimaryGeneratorActionLink::BDSPrimaryGeneratorActionLink(BDSBunch* bunchIn):
+  bunch(bunchIn)
+{
+  particleGun  = new G4ParticleGun(1); // 1-particle gun
+  
+  particleGun->SetParticleMomentumDirection(G4ThreeVector(0.,0.,1.));
+  particleGun->SetParticlePosition(G4ThreeVector());
+  particleGun->SetParticleTime(0);
+}
+
+BDSPrimaryGeneratorActionLink::~BDSPrimaryGeneratorActionLink()
+{
+  delete particleGun;
+}
+
+void BDSPrimaryGeneratorActionLink::GeneratePrimaries(G4Event* anEvent)
+{
+  // always save seed state in output
+  BDSEventInfo* eventInfo = new BDSEventInfo();
+  anEvent->SetUserInformation(eventInfo);
+  eventInfo->SetSeedStateAtStart(BDSRandom::GetSeedState());
+
+
+  // update particle definition in the special case of an ion - can only be done here
+  // and not before due to Geant4 ion information availability only at run time
+  /*if (ionPrimary && !ionCached)
+    {
+      bunch->UpdateIonDefinition();
+    }
+  */
+  // generate set of coordinates - internally the bunch may try many times to generate
+  // coordinates with total energy above the rest mass and may throw an exception if
+  // it can't
+  BDSParticleCoordsFullGlobal coords;
+  try
+    {coords = bunch->GetNextParticleValid();}
+  catch (const BDSException& exception)
+    {// we couldn't safely generate a particle -> abort
+      // could be because of user input file
+      anEvent->SetEventAborted();
+      G4cout << exception.what() << G4endl;
+      G4cout << "Aborting this event (#" << anEvent->GetEventID() << ")" << G4endl;
+      return;
+    }
+  
+  particleGun->SetParticleDefinition(bunch->ParticleDefinition()->ParticleDefinition());
+  
+  // always update the charge - ok for normal particles; fixes purposively specified ions.
+  particleGun->SetParticleCharge(bunch->ParticleDefinition()->Charge());
+
+  // check that kinetic energy is positive and finite anyway and abort if not.
+  // get the mass from the beamParticle as this takes into account any electrons
+  G4double EK = coords.local.totalEnergy - bunch->ParticleDefinition()->Mass();
+  if (EK <= 0)
+    {
+      G4cout << __METHOD_NAME__ << "Event #" << anEvent->GetEventID()
+	     << " - Particle kinetic energy smaller than 0! "
+	     << "This will not be tracked." << G4endl;
+      anEvent->SetEventAborted();
+      return;
+    }
+
+  // check the coordinates are valid
+  if (!worldExtent.Encompasses(coords.global))
+    {
+      G4cerr << __METHOD_NAME__ << "point: " << coords.global
+	     << "mm lies outside the world volume with extent ("
+	     << worldExtent << " - event aborted!" << G4endl << G4endl;
+      anEvent->SetEventAborted();
+    }
+
+#ifdef BDSDEBUG
+    G4cout << __METHOD_NAME__ << coords << G4endl;
+#endif
+
+  G4ThreeVector PartMomDir(coords.global.xp,coords.global.yp,coords.global.zp);
+  G4ThreeVector PartPosition(coords.global.x,coords.global.y,coords.global.z);
+
+  particleGun->SetParticlePosition(PartPosition);
+  particleGun->SetParticleEnergy(EK);
+  particleGun->SetParticleMomentumDirection(PartMomDir);
+  particleGun->SetParticleTime(coords.global.T);
+
+  particleGun->GeneratePrimaryVertex(anEvent);
+
+  // set the weight
+  auto vertex = anEvent->GetPrimaryVertex();
+  vertex->SetWeight(coords.local.weight);
+
+  // associate full set of coordinates with vertex for writing to output after event
+  //vertex->SetUserInformation(new BDSPrimaryVertexInformation(coords,
+  //							     bunch->ParticleDefinition()));
+
+#ifdef BDSDEBUG
+  vertex->Print();
+#endif
+}
