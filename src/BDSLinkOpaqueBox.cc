ec2aed08bbcb838ddc9985d2fa38336a47efc19b
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2020-01-27 15:00:14 +0100

    Register objects wtih BDSGeometryComponent for memory management.  Indentation.
    Former-commit-id: 4088fe19920c6a81cab9f73e9c50df6010f08740

--- src/BDSLinkOpaqueBox.cc
+++ src/BDSLinkOpaqueBox.cc
@@ -47,29 +47,29 @@ BDSLinkOpaqueBox::BDSLinkOpaqueBox(BDSAcceleratorComponent* acceleratorComponent
   G4double opaqueBoxThickness = 10*CLHEP::mm;
   G4String name = component->GetName();
 
-
   G4Box* terminatorBoxOuter = new G4Box(name + "_terminator_box_outer_solid",
-			 0.5 * extent.DX() + gap + opaqueBoxThickness,
-			 0.5 * extent.DY() + gap + opaqueBoxThickness,
-			 0.5 * extent.DZ() + gap + opaqueBoxThickness);
-
+					0.5 * extent.DX() + gap + opaqueBoxThickness,
+					0.5 * extent.DY() + gap + opaqueBoxThickness,
+					0.5 * extent.DZ() + gap + opaqueBoxThickness);
+  RegisterSolid(terminatorBoxOuter);
   G4Box* terminatorBoxInner = new G4Box(name + "_terminator_box_inner_solid",
-			 0.5 * extent.DX() + gap,
-			 0.5 * extent.DY() + gap,
-			 0.5 * extent.DZ() + gap);
-
-  auto opaqueBox = new G4SubtractionSolid(name + "_opaque_box_solid",
-					  terminatorBoxOuter,
-					  terminatorBoxInner);
-
-  G4LogicalVolume* opaqueBoxLV = new G4LogicalVolume(
-      opaqueBox, BDSMaterials::Instance()->GetMaterial("G4_Galactic"),
-      name + "_opaque_box_lv");
+					0.5 * extent.DX() + gap,
+					0.5 * extent.DY() + gap,
+					0.5 * extent.DZ() + gap);
+  RegisterSolid(terminatorBoxInner);
+  G4SubtractionSolid* opaqueBox = new G4SubtractionSolid(name + "_opaque_box_solid",
+							 terminatorBoxOuter,
+							 terminatorBoxInner);
+  RegisterSolid(opaqueBox);
+  G4LogicalVolume* opaqueBoxLV = new G4LogicalVolume(opaqueBox,
+						     BDSMaterials::Instance()->GetMaterial("G4_Galactic"),
+						     name + "_opaque_box_lv");
+  RegisterLogicalVolume(opaqueBoxLV);
 
   G4UserLimits* termUL = new G4UserLimits(std::numeric_limits<double>::max(),
-				 std::numeric_limits<double>::max(),
-				 std::numeric_limits<double>::max(),
-				 0., 0.);
+					  std::numeric_limits<double>::max(),
+					  std::numeric_limits<double>::max(),
+					  0., 0.);
   RegisterUserLimits(termUL);
   opaqueBoxLV->SetUserLimits(termUL);
   G4VisAttributes* obVis = new G4VisAttributes(*BDSColours::Instance()->GetColour("opaquebox"));
@@ -82,38 +82,36 @@ BDSLinkOpaqueBox::BDSLinkOpaqueBox(BDSAcceleratorComponent* acceleratorComponent
 			     0.5 * extent.DX() + gap + opaqueBoxThickness + ls,
 		       	     0.5 * extent.DY() + gap + opaqueBoxThickness + ls,
 			     0.5 * extent.DZ() + gap + opaqueBoxThickness + ls);
-
-  containerLogicalVolume = new G4LogicalVolume(
-      containerSolid, BDSMaterials::Instance()->GetMaterial("G4_Galactic"),
-      name + "_container_lv");
-
+  
+  containerLogicalVolume = new G4LogicalVolume(containerSolid,
+					       BDSMaterials::Instance()->GetMaterial("G4_Galactic"),
+					       name + "_container_lv");
+  containerLogicalVolume->SetVisAttributes(BDSGlobalConstants::Instance()->ContainerVisAttr());
+  
   // auto boxPlacement = 
-    new G4PVPlacement(nullptr,
-					G4ThreeVector(),
-					opaqueBoxLV,
-					name + "_opaque_box_pv",
-					containerLogicalVolume,
-					false,
-					1,
-					true);
+  new G4PVPlacement(nullptr,
+		    G4ThreeVector(),
+		    opaqueBoxLV,
+		    name + "_opaque_box_pv",
+		    containerLogicalVolume,
+		    false,
+		    1,
+		    true);
   
   // auto collimatorPlacement =
-    new G4PVPlacement(nullptr,
-					       G4ThreeVector(),
-					       component->GetContainerLogicalVolume(),
-					       component->GetName() + "_pv",
-					       containerLogicalVolume,
-					       false,
-					       1,
-					       true);
+  new G4PVPlacement(nullptr,
+		    G4ThreeVector(),
+		    component->GetContainerLogicalVolume(),
+		    component->GetName() + "_pv",
+		    containerLogicalVolume,
+		    false,
+		    1,
+		    true);
   
-					      
-
   outerExtent = BDSExtent(0.5 * extent.DX() + gap + opaqueBoxThickness + ls,
 			  0.5 * extent.DY() + gap + opaqueBoxThickness + ls,
 			  0.5 * extent.DZ() + gap + opaqueBoxThickness + ls);
-    
-
+  
   // build container geometry.
   // One box minus smaller box to make room for collimator.
 
@@ -125,6 +123,4 @@ BDSLinkOpaqueBox::BDSLinkOpaqueBox(BDSAcceleratorComponent* acceleratorComponent
   // Update containerSolid and containerLV of BDSGeometryComponent.
   // Update extents of BDSGeometryComponent.
   // register daughters BDSGeometryComponent.
-
-
 }
