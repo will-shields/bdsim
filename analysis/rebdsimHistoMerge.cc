7af220ad2806e7482fb07b954bb9d361cf0e4006
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2020-04-13 16:07:11 +0200

    Tidying.
    Former-commit-id: 185b8b9d4119134017acdf19fddf8d50a5dbaf41

--- analysis/rebdsimHistoMerge.cc
+++ analysis/rebdsimHistoMerge.cc
@@ -132,7 +132,7 @@ int main(int argc, char *argv[])
       // copy the model over and rename to avoid conflicts with Model directory
       auto modelTree = dl->GetModelTree();
       auto newTree = modelTree->CloneTree();
-      // unforunately we have a folder called Model in histogram output files
+      // unfortunately we have a folder called Model in histogram output files
       // avoid conflict when copying the model for plotting
       newTree->SetName("ModelTree");
       newTree->Write("", TObject::kOverwrite);
