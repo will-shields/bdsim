77986f16eb811b176207c6d26e5c7d87265d8fb5
Eliott Ramoisiaux <Eliott.ramoisiaux@ulb.be>
2020-06-05 15:56:02 +0200

    Add files
    Former-commit-id: 6428d638341703a748c9ccc83e9a06940375a422

---
+++ include/BDSBH4D.hh
@@ -0,0 +1,28 @@
+//
+// Created by Eliott Ramoisiaux on 05/06/2020.
+//
+
+#ifndef BDSIM_BDSBH4D_HH
+#define BDSIM_BDSBH4D_HH
+
+#include <boost/histogram.hpp>
+
+#include "Rtypes.h"
+#include "TObject.h"
+
+typedef boost::histogram::histogram<std::__1::tuple<boost::histogram::axis::regular<double, boost::use_default, boost::use_default, boost::use_default>, boost::histogram::axis::regular<double, boost::use_default, boost::use_default, boost::use_default>, boost::histogram::axis::regular<double, boost::use_default, boost::use_default, boost::use_default>, boost::histogram::axis::regular<double, boost::histogram::axis::transform::log, boost::use_default, boost::use_default> >, boost::histogram::unlimited_storage<std::__1::allocator<char> > > boost_histogram;
+
+
+class BDSBH4D : public TObject {
+
+public:
+    BDSBH4D();
+
+    boost_histogram h;
+
+ClassDef(BDSBH4D,1);
+
+};
+
+
+#endif //BDSIM_BDSBH4D_HH
--- include/BDSBH4DLinkDef.hh
+++ include/BDSBH4DLinkDef.hh
@@ -5,4 +5,7 @@
 #ifndef BDSIM_BDSBH4DLINKDEF_HH
 #define BDSIM_BDSBH4DLINKDEF_HH
 
+#pragma link C++ class boost::histogram::histogram<std::__1::tuple<boost::histogram::axis::regular<double, boost::use_default, boost::use_default, boost::use_default>, boost::histogram::axis::regular<double, boost::use_default, boost::use_default, boost::use_default>, boost::histogram::axis::regular<double, boost::use_default, boost::use_default, boost::use_default>, boost::histogram::axis::regular<double, boost::histogram::axis::transform::log, boost::use_default, boost::use_default> >, boost::histogram::unlimited_storage<std::__1::allocator<char> > >+;
+#pragma link C++ class BDSBH4D+;
+
 #endif //BDSIM_BDSBH4DLINKDEF_HH
