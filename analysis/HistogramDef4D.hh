9f1681a17452ab7cad0e29ee0b8727db3e02cef9
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2020-09-04 13:51:21 +0200

    Missing licenses.
    Former-commit-id: 6b4328dfae22b78c879af733a87c39b104933ba1

--- analysis/HistogramDef4D.hh
+++ analysis/HistogramDef4D.hh
@@ -1,10 +1,24 @@
-//
-// Created by Eliott Ramoisiaux on 14/07/2020.
-//
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2020.
 
-#ifndef BDSIM_HISTOGRAMDEF4D_HH
-#define BDSIM_HISTOGRAMDEF4D_HH
+This file is part of BDSIM.
 
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef BDSIM_HISTOGRAMDEF4D_H
+#define BDSIM_HISTOGRAMDEF4D_H
+#include "BinSpecification.hh"
 #include "HistogramDef3D.hh"
 
 #include <string>
