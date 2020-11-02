5074670cbd8c1c7c79d0b99d08020cebc17f3670
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2020-11-02 17:39:39 +0100

    Missing header.
    Former-commit-id: daa0be0ccd57d06621584d7df7ae1d2282108c1a

--- src/BDSBeamPipeFactoryBase.cc
+++ src/BDSBeamPipeFactoryBase.cc
@@ -31,6 +31,7 @@ along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
 #include "G4UserLimits.hh"
 #include "G4VisAttributes.hh"
 
+#include <limits>
 #include <set>
 
 BDSBeamPipeFactoryBase::BDSBeamPipeFactoryBase()
