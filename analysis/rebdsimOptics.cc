532bc3e784ca6be5c3abdec16ff17dd74b137028
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2020-11-19 12:54:41 +0100

    Fix segfault when the model tree wasn't saved and therefore no samplers are set up.
    Former-commit-id: 0813b385e34cd0f3e8dca7432316854ef13c18a9

--- analysis/rebdsimOptics.cc
+++ analysis/rebdsimOptics.cc
@@ -88,11 +88,18 @@ int main(int argc, char* argv[])
     {std::cerr << e << std::endl; exit(1);}
 
   // beam required to get the mass of the primary particle in EventAnalysis
-  Beam* beam = dl->GetBeam();
-  TChain*  beamTree = dl->GetBeamTree();
+  Beam*   beam     = dl->GetBeam();
+  TChain* beamTree = dl->GetBeamTree();
   BDSOutputROOTEventBeam* outputBeam = beam->beam;
   beamTree->GetEntry(0);
   const std::string& particleName = outputBeam->particle;
+  
+  TChain* modelTree = dl->GetModelTree();
+  if (modelTree->GetEntries() == 0)
+    {
+      std::cerr << "Data file written without Model tree that is required for optics analysis" << std::endl;
+      exit(1);
+    }
 
   EventAnalysis* evtAnalysis = new EventAnalysis(dl->GetEvent(), dl->GetEventTree(),
 						 false, true, false, -1, emittanceOnFly, 0, -1, particleName);
@@ -121,7 +128,6 @@ int main(int argc, char* argv[])
   if (!ob->generatePrimariesOnly)
     {
       // clone model tree for nice built in optics plotting
-      auto modelTree = dl->GetModelTree();
       auto newTree = modelTree->CloneTree();
       newTree->Write("", TObject::kOverwrite);
     }
