3ba107b75350f693ba06f55c886f7a3effb2dce0
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2021-02-08 18:41:36 +0000

    Introduce global field classes that look at the placement world using the different navigator.
    Former-commit-id: 604ea8473744d9317d42cb6206a532f29e1a464c

---
+++ src/BDSFieldEGlobalPlacement.cc
@@ -0,0 +1,55 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2021.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "BDSFieldE.hh"
+#include "BDSFieldEGlobalPlacement.hh"
+
+#include "globals.hh" // geant4 types / globals
+#include "G4ThreeVector.hh"
+
+BDSFieldEGlobalPlacement::BDSFieldEGlobalPlacement(BDSFieldE* fieldIn):
+  field(fieldIn)
+{
+  finiteStrength = field->FiniteStrength();
+}
+
+BDSFieldEGlobalPlacement::~BDSFieldEGlobalPlacement()
+{
+  delete field;
+}
+
+G4ThreeVector BDSFieldEGlobalPlacement::GetFieldTransformed(const G4ThreeVector& position,
+							    const G4double       t) const
+{
+  if (!finiteStrength)
+    {return G4ThreeVector();} // quicker than query
+  else
+    {return GetField(position, t);}
+}
+
+G4ThreeVector BDSFieldEGlobalPlacement::GetField(const G4ThreeVector& position,
+						 const G4double       t) const
+{
+  G4bool foundAPlacementVolume = false;
+  G4ThreeVector localPosition = ConvertToLocal(position, foundAPlacementVolume);
+  if (!foundAPlacementVolume)
+    {return G4ThreeVector();} // force to 0 outside range of coordinate transforms
+  G4ThreeVector localField    = field->GetFieldTransformed(localPosition,t);
+  G4ThreeVector globalField   = ConvertAxisToGlobal(localField);
+  return globalField;
+}
