89ad05bc27ee4d6d25ce1f49b3935f4fb3ca7e5b
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2021-03-06 16:52:19 +0000

    New bunch box distribution.
    Former-commit-id: d0ac1bdc8c2ddf4d3929fe86eba3c7e862b2f0a2

---
+++ include/BDSBunchBox.hh
@@ -0,0 +1,65 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2021.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef BDSBUNCHBOX_H
+#define BDSBUNCHBOX_H 
+
+#include "BDSBunch.hh"
+
+namespace CLHEP {
+  class RandFlat;
+}
+
+/**
+ * @brief A bunch distribution that produces an uncorrelated uniform
+ * random distribution within a 6D box.
+ * 
+ * @author Laurie Nevay
+ */
+
+class BDSBunchBox: public BDSBunch
+{ 
+public: 
+  BDSBunchBox(); 
+  virtual ~BDSBunchBox();
+  /// @{ Assignment and copy constructor not implemented nor used
+  BDSBunchBox& operator=(const BDSBunchBox&) = delete;
+  BDSBunchBox(BDSBunchBox&) = delete;
+  /// @}
+  virtual void SetOptions(const BDSParticleDefinition* beamParticle,
+			  const GMAD::Beam& beam,
+			  const BDSBunchType& distrType,
+			  G4Transform3D beamlineTransformIn = G4Transform3D::Identity,
+			  const G4double beamlineS = 0);
+  virtual void CheckParameters();
+  virtual BDSParticleCoordsFull GetNextParticleLocal();
+  
+protected:
+  G4double envelopeX;
+  G4double envelopeY;
+  G4double envelopeZ;
+  G4double envelopeXp;
+  G4double envelopeYp;
+  G4double envelopeZp;
+  G4double envelopeT;
+  G4double envelopeE;
+  
+  CLHEP::RandFlat* flatGen;
+};
+
+#endif
