1b72ba253a6cf1899e89383c35570e435b8ce8cc
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2021-03-14 11:15:20 +0000

    Define executable test.
    Former-commit-id: 81f3ff328078597a42b75317142d2f8ca188f405

---
+++ test/TrajectoryTest.cc
@@ -0,0 +1,83 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2021.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "BDSException.hh"
+#include "BDSOutputROOTEventTrajectory.hh"
+#include "BDSOutputROOTEventTrajectoryPoint.hh"
+#include "DataLoader.hh"
+#include "Event.hh"
+
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+
+int main(int argc, char** argv)
+{
+  if (argc < 2 || argc > 2)
+    {std::cout << "usage: BDSTrajectoryTest <datafile>" << std::endl;}
+
+  std::string dataFile = std::string(argv[1]);
+
+  // to simply syntax
+  typedef BDSOutputROOTEventTrajectoryPoint point;
+  typedef std::vector<BDSOutputROOTEventTrajectoryPoint> pointV;
+  
+  try
+    {
+      DataLoader* dl = new DataLoader(dataFile);
+      TTree* eventTree = dl->GetEventTree();
+      Event* event     = dl->GetEvent();
+      
+      eventTree->GetEntry(0);
+
+      // test each function in the trajectory class
+      // use the last trackID as our random starting particle to inspect
+      int lastTrackID = (int)event->Trajectory->trackID.back();
+
+      pointV trackInteractions = event->Trajectory->trackInteractions(lastTrackID);
+
+      point  primProcessPoint  = event->Trajectory->primaryProcessPoint(lastTrackID);
+
+      pointV processHistory    = event->Trajectory->processHistory(lastTrackID);
+
+      event->Trajectory->printTrajectoryInfo(lastTrackID);
+      bool parentIsPrimary = event->Trajectory->parentIsPrimary(lastTrackID);
+      std::string answer = parentIsPrimary ? "True" : "False";
+      std::cout << "Parent of trackID " << lastTrackID << " is a primary? : " << answer << std::endl;
+
+
+      // now test them with an invalid trackID to ensure no bad exception thrown
+      // trackID should be >= 1
+      event->Trajectory->trackInteractions(0);
+      event->Trajectory->primaryProcessPoint(0);
+      event->Trajectory->processHistory(0);
+      event->Trajectory->parentIsPrimary(0);
+
+      // now try with the primary which has no parent. The primary should by construction
+      // have trackID = 1
+      event->Trajectory->trackInteractions(1);
+      event->Trajectory->primaryProcessPoint(1);
+      event->Trajectory->processHistory(1);
+      event->Trajectory->parentIsPrimary(1);
+    }
+  catch (const BDSException& e)
+    {std::cout << e.what() << std::endl;}
+  catch (const std::exception& e)
+    {std::cout << e.what() << std::endl;}
+  return 0;
+}
