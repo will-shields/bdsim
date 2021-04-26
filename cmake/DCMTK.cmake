5527a51eaef2734c4d754be7f16df5145f0d4de0
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2021-04-26 18:50:56 +0200

    Consolidate cmake.
    Former-commit-id: 833430208eb69c13888253551b006fb86e1903a8

--- cmake/DCMTK.cmake
+++ cmake/DCMTK.cmake
@@ -1,15 +1,17 @@
 
-option(USE_DICOM "Use DCMTK C++ library." OFF)
-if(USE_DICOM)
-    message(STATUS "Using DCMTK")
-    FIND_PACKAGE(DCMTK REQUIRED CONFIG)
+option(USE_DICOM "Use DCMTK library for DICOM file handling" OFF)
+if (USE_DICOM)
+  message(STATUS "DICOM module ON")
 
-    if(DCMTK_FOUND)
-        message(STATUS "Found DCMTK ${DCMTK_VERSION} ${DCMKTK_INCLUDE_DIRS}")
-        include_directories(SYSTEM ${DCMKTK_INCLUDE_DIRS})
-        add_definitions("-DUSE_DICOM")
-    else()
-        message(FATAL_ERROR "USE_DICOM turned on, but no DCMTK library found.")
-    endif()
+  #find_package(DCMTK REQUIRED CONFIG)
+  find_package(DCMTK REQUIRED)
+
+  if (DCMTK_FOUND)
+     message(STATUS "Found DCMTK ${DCMTK_VERSION} ${DCMKTK_INCLUDE_DIRS}")
+     include_directories(SYSTEM ${DCMKTK_INCLUDE_DIRS})
+     add_definitions("-DUSE_DICOM")
+  else()
+     message(FATAL_ERROR "USE_DICOM turned on, but no DCMTK library found.")
+  endif()
 endif()
 
