51ed16dc958b4287af58b74c8672f6aedbf63109
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2021-09-21 12:10:41 +0200

    Example python versions equivalent of rebdsim.
    Former-commit-id: 8b51aa2a77cdc973d0b4deb936bf7826ad8c1934

---
+++ examples/features/analysis/pythonAnalysis/pyrebdsimLogBins.py
@@ -0,0 +1,50 @@
+import pybdsim
+import numpy as np
+import ROOT
+
+def AnalyseFile(filename, outfilename):
+    # load the data
+    d = pybdsim.Data.Load(filename)
+    # loading data in pybdsim forces the load of the rebdsim
+    # shared library and gives us all the rebdsim classes inside
+    # ROOT as python functions
+    
+    # explicitly specify the data type in the numpy array as 'd' for double
+    # for compatibility with root
+    logBins = np.logspace(-3,1,50,dtype='d')
+    hl = ROOT.TH1D("BaseEventDurationLog",
+                   "BaseEvent Durations",
+                   len(logBins)-1,# one less bin than n bin edges
+                   logBins)
+
+    # we now wrap our histograms in an 'accumulator' which
+    # builds the mean histogram across events
+    accHL = ROOT.HistogramAccumulator(hl,str(hl.GetName()).strip('Base'),str(hl.GetTitle()).strip('Base'))
+    # top tip - name the accumulators something different to avoid confusion
+
+    eventTree = d.GetEventTree()
+    for event in eventTree:
+        # the 'event' temporary object here, now looks exactly
+        # like the file layout you see in a TBrowser
+
+        # first of all we empty our base histogram for this event
+        hl.Reset()
+
+        # fill our histograms - could be some custom if else logic here
+        # or looping over N sampler hits or M trajectories
+        # this example just has one number per event we're filling
+        hl.Fill(event.Summary.durationCPU)
+
+        # after filling we accumulate the mean
+        accHL.Accumulate(hl)
+
+    # done with looping over events
+    # terminate mean histograms
+    resultHist = accHL.Terminate()
+    
+    outfile = pybdsim.Data.CreateEmptyRebdsimFile(outfilename, d.header.nOriginalEvents)
+    pybdsim.Data.WriteROOTHistogramsToDirectory(outfile, "Event/PerEntryHistograms", [resultHist])
+    outfile.Close()
+        
+if __name__ == "__main__":
+    AnalyseFile("../../data/sample*.root","pyrebdsimTestLog.root")
