407a5b1034893c2473e9d599827ec0c0693d406a
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2021-10-08 17:14:22 +0200

    Implement magnitude interpolation for linear field interpolation.  Use specialisation of template.
    Former-commit-id: 0d00fe6709dec4cc38ee2b42e66405f5f1bf5fd5

---
+++ include/BDSInterpolator3DLinearMag.hh
@@ -0,0 +1,59 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2021.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef BDSINTERPOLATE3DLINEARMAG_H
+#define BDSINTERPOLATE3DLINEARMAG_H
+
+#include "BDSFieldValue.hh"
+#include "BDSInterpolator3D.hh"
+
+#include "G4Types.hh"
+
+class BDSArray3DCoords;
+
+/** 
+ * @brief Linear interpolation over 3d array including magnitude interpolation.
+ * 
+ * Does not own array - so multiple interpolators could be used on same data.
+ * 
+ * This uses a trilinear algorithm to interpolate a 3d array and return
+ * the value at any arbitrary point. If the point lies outside the array
+ * the default value for the templated parameter is returned (typically 0).
+ * Therefore, the field drops to 0 outside the specified region.
+ *
+ * This is based on the following article:
+ * https://en.wikipedia.org/wiki/Trilinear_interpolation
+ * 
+ * @author Laurie Nevay
+ */
+
+class BDSInterpolator3DLinearMag: public BDSInterpolator3D
+{
+public:
+  explicit BDSInterpolator3DLinearMag(BDSArray3DCoords* arrayIn);
+  virtual ~BDSInterpolator3DLinearMag();
+
+protected:
+  virtual BDSFieldValue GetInterpolatedValueT(G4double x, G4double y, G4double z) const;
+
+private:
+  /// Private default constructor to force use of provided one.
+  BDSInterpolator3DLinearMag() = delete;
+};
+
+#endif
--- include/BDSInterpolatorRoutines.hh
+++ include/BDSInterpolatorRoutines.hh
@@ -21,12 +21,24 @@ along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "G4Types.hh"
 
+#include "BDSFieldValue.hh"
+
+#include <cmath>
+
 namespace BDS
 {
   // These routines are templated for any type. They can be used with a type
   // that provides both L and R operator+ operator- with their own type and
   // operator+ operator- operator* with a double.
-  
+
+  /// Define a magnitude function that will work for numeric types
+  /// but also for a field value (3 vector).
+  template<class T>
+  T mag(const T& v) {return std::abs(v);}
+  /// Specialisation for a 3-vector field value.
+  template<template<class> class BDSFieldValue, class V>
+  V mag(const BDSFieldValue<V>& v){return v.mag();}
+
   /// Linear interpolation in 1 dimension. The "p" is the real value to be
   /// interpolated. 'x' must be on the interval [0,1], ie normalised.
   template<class T>
@@ -125,6 +137,116 @@ namespace BDS
     arr[2] = BDS::Cubic3D<T>(p[2], y, z, t);
     arr[3] = BDS::Cubic3D<T>(p[3], y, z, t);
     return BDS::Cubic1D<T>(arr, x);
+  }
+
+  /// Linear interpolation of the magnitude in 1 dimension
+  template<class T>
+  double Linear1DMagOnly(const T p[2],
+			 G4double x)
+  {
+    return (double)(mag(p[0])*(1.-x) + mag(p[1])*x);
+  }
+
+  
+  /// Linear interpolation of the magnitude in 2 dimensions.
+  template<class T>
+  double Linear2DMagOnly(const T p[2][2],
+			 G4double x,
+			 G4double y)
+  {
+    double arr[2];
+    arr[0] = BDS::Linear1DMagOnly(p[0], y);
+    arr[1] = BDS::Linear1DMagOnly(p[1], y);
+    return BDS::Linear1DMagOnly(arr, x);
+  }
+
+  /// Linear interpolation of the magnitude in 3 dimensions.
+  template<class T>
+  double Linear3DMagOnly(const T p[2][2][2],
+			 G4double x,
+			 G4double y,
+			 G4double z)
+  {
+    double arr[2];
+    arr[0] = BDS::Linear2DMagOnly(p[0], y, z);
+    arr[1] = BDS::Linear2DMagOnly(p[1], y, z);
+    return BDS::Linear1DMagOnly(arr, x);
+  }
+
+  /// Linear interpolation of the magnitude in 4 dimensions.
+  template<class T>
+  double Linear4DMagOnly(const T p[2][2][2][2],
+			 G4double x,
+			 G4double y,
+			 G4double z,
+			 G4double t)
+  {
+    double arr[2];
+    arr[0] = BDS::Linear3DMagOnly(p[0], y, z, t);
+    arr[1] = BDS::Linear3DMagOnly(p[1], y, z, t);
+    return BDS::Linear1DMagOnly(arr, x);
+  }
+
+  /// Linear interpolation in 1 dimension including magnitude interpolation.
+  template<class T>
+  T Linear1DMag(const T p[2],
+		G4double x)
+  {
+    T v = BDS::Linear1D(p,x);
+    double vMag = Linear1DMagOnly(p, x);
+    double factor = vMag / mag(v);
+    if (std::isnan(factor))
+      {factor = 1.0;}
+    v *= factor;
+    return v;
+  }
+
+  /// Linear interpolation in 2 dimensions including magnitude interpolation.
+  template<class T>
+  T Linear2DMag(const T p[2][2],
+		G4double x,
+		G4double y)
+  {
+    T v = BDS::Linear2D(p, x, y);
+    double vMag = BDS::Linear2DMagOnly(p, x, y);
+    double factor = vMag / mag(v);
+    if (std::isnan(factor))
+      {factor = 1.0;}
+    v *= factor;
+    return v;
+  }
+
+   /// Linear interpolation in 3 dimensions including magnitude interpolation.
+  template<class T>
+  T Linear3DMag(const T p[2][2][2],
+		G4double x,
+		G4double y,
+		G4double z)
+  {
+    T v = BDS::Linear3D(p, x, y, z);
+    double vMag = BDS::Linear3DMagOnly(p, x, y, z);
+    double factor = vMag / mag(v);
+    if (std::isnan(factor))
+      {factor = 1.0;}
+    v *= factor;
+    return v;
+  }
+
+  /// Linear interpolation in 4 dimensions including magnitude interpolation.
+  template<class T>
+  T Linear4DMag(const T p[2][2][2][2],
+		G4double x,
+		G4double y,
+		G4double z,
+		G4double t)
+  {
+    T v = BDS::Linear4D(p, x, y, z, t);
+    double vMag = BDS::Linear4DMagOnly(p, x, y, z, t);
+    double factor = vMag / mag(v);
+    if (std::isnan(factor))
+      {factor = 1.0;}
+    v *= factor;
+    return v;
   }
 }
 
