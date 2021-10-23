225e07ae19eff899d39d35b1d8c23a60793bed88
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2021-10-23 17:42:15 +0200

    Copy and rename script for manual pdfs.
    Former-commit-id: 2139a1a2db0a075b8788bf9cf9ba7ae306c4e166

---
+++ examples/features/fields/maps_bdsim/CopyFieldComparisonsToManual.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+MANDIR=~/physics/reps/bdsim
+DESDIR=$MANDIR/manual/source/dev_figures
+
+
+cp original_data.pdf              $DESDIR/field_raw.pdf
+cp original.png                   $DESDIR/field_raw2d.png
+cp nearest.png                    $DESDIR/field_nearest2d.png
+cp linear.png                     $DESDIR/field_linear2d.png
+cp cubic.png                      $DESDIR/field_cubic.png
+cp 1d_nearest_xyz.pdf             $DESDIR/field_linear2d.png
+cp 1d_linear_xyz.pdf              $DESDIR/field_linear.pdf
+cp 1d_linear_mag_xyz.pdf          $DESDIR/field_linear_mag.pdf
+cp 1d_cubic_xyz.pdf               $DESDIR/field_cubic.pdf
+cp 1d_linear_mag_comparison.pdf   $DESDIR/field_linear_comparison.pdf
