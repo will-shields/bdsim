1f4530a6045f90bc486e7a110d4078411b0c0c9d
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2021-10-23 18:40:01 +0200

    Fix manual plots.
    Former-commit-id: cdf9ace7c64a7c338b5faa4c32bf7f97a8d3ec53

--- examples/features/fields/maps_bdsim/Compare2D.py
+++ examples/features/fields/maps_bdsim/Compare2D.py
@@ -1,17 +1,41 @@
+import matplotlib.pyplot as _plt
 import pybdsim
 
 def Compare2D(original, nearest, linear, linearmag, cubic):
     kws = {'vmin':-4, 'vmax':4}
     pybdsim.Field.Plot2DXYFxFyFz(original, 'Original', **kws)
+    # for this one we shrink the limits
+    axs = _plt.gcf().get_axes()
+    for i in range(3):
+        axs[i].set_ylim(-50,50)
+    Save(original)
+    
     pybdsim.Field.Plot2DXYFxFyFz(nearest, 'Nearest', **kws)
+    Save(nearest)
+    
     pybdsim.Field.Plot2DXYFxFyFz(linear, 'Linear', **kws)
+    Save(linear)
+    
     pybdsim.Field.Plot2DXYFxFyFz(linearmag, 'Linear Mag', **kws)
+    Save(linearmag)
+    
     pybdsim.Field.Plot2DXYFxFyFz(cubic, 'Cubic', **kws)
-    return
+    Save(cubic)
 
-if __name__ == "__main__":
+def _FN(filename):
+    fn = filename.split('.')[0]+".png"
+    print(fn)
+    return fn
+
+def Save(filename):
+    _plt.savefig(_FN(filename), dpi=400)
+
+def DoIt():
     Compare2D("2dexample.dat.gz",
               "2d_interpolated_nearest.dat",
               "2d_interpolated_linear.dat",
               "2d_interpolated_linear_mag.dat",
               "2d_interpolated_cubic.dat")
+
+if __name__ == "__main__":
+    DoIt()
--- examples/features/fields/maps_bdsim/CopyFieldComparisonsToManual.sh
+++ examples/features/fields/maps_bdsim/CopyFieldComparisonsToManual.sh
@@ -6,10 +6,11 @@ DESDIR=$MANDIR/manual/source/dev_figures
 set -x
 
 cp original_data.pdf              $DESDIR/field_raw.pdf
-cp original.png                   $DESDIR/field_raw2d.png
-cp nearest.png                    $DESDIR/field_nearest2d.png
-cp linear.png                     $DESDIR/field_linear2d.png
-cp cubic.png                      $DESDIR/field_cubic2d.png
+cp 2dexample.png                  $DESDIR/field_raw2d.png
+cp 2d_interpolated_nearest.png    $DESDIR/field_nearest2d.png
+cp 2d_interpolated_linear.png     $DESDIR/field_linear2d.png
+cp 2d_interpolated_linear_mag.png $DESDIR/field_linear_mag2d.png
+cp 2d_interpolated_cubic.png      $DESDIR/field_cubic2d.png
 cp 1d_nearest_xyz.pdf             $DESDIR/field_linear2d.png
 cp 1d_linear_xyz.pdf              $DESDIR/field_linear.pdf
 cp 1d_linear_mag_xyz.pdf          $DESDIR/field_linear_mag.pdf
