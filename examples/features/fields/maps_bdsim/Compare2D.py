ca0863611ac40bc1ae7766d8a29d2b80b3acc81c
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2021-10-23 18:21:19 +0200

    Update manual figures for field map interpolation comparison and documentation.
    Former-commit-id: f78668d5731c35b507c3af1f9baf8bc80326a99b

--- examples/features/fields/maps_bdsim/Compare2D.py
+++ examples/features/fields/maps_bdsim/Compare2D.py
@@ -1,55 +1,13 @@
 import pybdsim
-import matplotlib.pyplot as plt
-import numpy as np
 
 def Compare2D(original, nearest, linear, linearmag, cubic):
-    a = pybdsim.Field.Load(original).data
-    n = pybdsim.Field.Load(nearest).data
-    l = pybdsim.Field.Load(linear).data
-    lm = pybdsim.Field.Load(linearmag).data
-    c = pybdsim.Field.Load(cubic).data
-    _Plot(a, 'original', 'Original')
-    _Plot(n, 'nearest',  'Nearest')
-    _Plot(l, 'linear',   'Linear')
-    _Plot(lm, 'linear_mag',   'Linear Mag')
-    _Plot(c, 'cubic',    'Cubic')
-
-def _Plot(a, filename, title):
-    f   = plt.figure(figsize=(7.5,4))
-    ax  = f.add_subplot(131)
-    ax2 = f.add_subplot(132)
-    ax3 = f.add_subplot(133)
-
-    xmin = np.min(a[:,:,0])
-    xmax = np.max(a[:,:,0])
-    ymin = np.min(a[:,:,1])
-    ymax = np.max(a[:,:,1])
-    ax.imshow(a[:,:,2], interpolation='None',extent=(xmin,xmax,ymin,ymax), vmin=-4, vmax=4, origin='lower')
-    ax.set_xlabel('X (cm)')
-    ax.set_ylabel('Y (cm)')
-    ax.set_title('X-Component',size='medium')
-    
-    im = ax2.imshow(a[:,:,3], interpolation='None',extent=(xmin,xmax,ymin,ymax), vmin=-4, vmax=4, origin='lower')
-    ax2.set_xlabel('X (cm)')
-    ax2.set_title('Y-Component',size='medium')
-    ax2.get_yaxis().set_ticks([])
-
-    im = ax3.imshow(a[:,:,4], interpolation='None',extent=(xmin,xmax,ymin,ymax), vmin=-4, vmax=4, origin='lower')
-    ax3.set_xlabel('X (cm)')
-    ax3.set_title('Z-Component',size='medium')
-    ax3.get_yaxis().set_ticks([])
-
-    f.subplots_adjust(left=0.09, right=0.85, top=0.86, bottom=0.12, wspace=0.02)
-    cbar_ax = f.add_axes([0.88, 0.15, 0.05, 0.7])
-    f.colorbar(im, cax=cbar_ax)
-
-    plt.suptitle(title,size='x-large')
-
-    #pdf seems to force interpolation
-    plt.savefig(filename+'.png',dpi=400)
-    
-    plt.draw()
-
+    kws = {'vmin':-4, 'vmax':4}
+    pybdsim.Field.Plot2DXYFxFyFz(original, 'Original', **kws)
+    pybdsim.Field.Plot2DXYFxFyFz(nearest, 'Nearest', **kws)
+    pybdsim.Field.Plot2DXYFxFyFz(linear, 'Linear', **kws)
+    pybdsim.Field.Plot2DXYFxFyFz(linearmag, 'Linear Mag', **kws)
+    pybdsim.Field.Plot2DXYFxFyFz(cubic, 'Cubic', **kws)
+    return
 
 if __name__ == "__main__":
     Compare2D("2dexample.dat.gz",
--- examples/features/fields/maps_bdsim/CopyFieldComparisonsToManual.sh
+++ examples/features/fields/maps_bdsim/CopyFieldComparisonsToManual.sh
@@ -3,12 +3,13 @@
 MANDIR=~/physics/reps/bdsim
 DESDIR=$MANDIR/manual/source/dev_figures
 
+set -x
 
 cp original_data.pdf              $DESDIR/field_raw.pdf
 cp original.png                   $DESDIR/field_raw2d.png
 cp nearest.png                    $DESDIR/field_nearest2d.png
 cp linear.png                     $DESDIR/field_linear2d.png
-cp cubic.png                      $DESDIR/field_cubic.png
+cp cubic.png                      $DESDIR/field_cubic2d.png
 cp 1d_nearest_xyz.pdf             $DESDIR/field_linear2d.png
 cp 1d_linear_xyz.pdf              $DESDIR/field_linear.pdf
 cp 1d_linear_mag_xyz.pdf          $DESDIR/field_linear_mag.pdf
