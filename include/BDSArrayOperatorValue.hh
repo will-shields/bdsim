0ed92b553ebd4220a291d35364dbf70d14798f8c
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2021-10-31 10:28:10 +0100

    Introduce array value operator.
    Former-commit-id: 0d8af45ea607d305c8f55bbdb3b7c45db8b66c35

---
+++ include/BDSArrayOperatorValue.hh
@@ -0,0 +1,47 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2021.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef BDSARRAYOPERATORVALUE_H
+#define BDSARRAYOPERATORVALUE_H
+#include "G4Types.hh"
+
+/**
+ * @brief Interface for modifying by reference spatial coordinates.
+ *
+ * Apply() method is pure virtual.
+ * 
+ * @author Laurie Nevay
+ */
+
+class BDSArrayOperatorValue
+{
+public:
+  BDSArrayOperatorValue(){;}
+  virtual ~BDSArrayOperatorValue(){;}
+  
+  /// Return a name of the operator for feedback to the user in print out.
+  virtual G4String Name() const = 0;
+  
+  virtual BDSFieldValue Apply(BDSFieldValue v,
+                              G4int xInd,
+                              G4int yInd = 0,
+                              G4int zInd = 0,
+                              G4int tInd = 0) const = 0;
+};
+
+#endif
