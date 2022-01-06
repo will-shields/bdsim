57da6b5a7f7acb1d9485d4a8a4b9b0c943016d8c
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2022-01-06 16:02:53 +0000

    Start of extra value relfection class.
    Former-commit-id: 9005800268e69784bbe8c210c7e236faa7c096eb

---
+++ include/BDSArrayOperatorValueReflectXZDipole.hh
@@ -0,0 +1,80 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2021.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef BDSARRAYOPERATORVALUEREFLECTYDIPOLE_H
+#define BDSARRAYOPERATORVALUEREFLECTYZDIPOLE_H
+#include "BDSArrayOperatorValue.hh"
+#include "BDSFieldValue.hh"
+
+#include "G4String.hh"
+#include "G4Types.hh"
+
+/**
+ * @brief Reflect field values for a dipolar field in the positive quadrant.
+ *
+ * Note this must work in combination with a coordinate relfection in x and y.
+ * Ultimately, the goal is this:
+ *
+ * \verbatim
+ *       y
+ *       ∧
+ *       |
+ *    ∧  |  ∧
+ *    |  |  | original
+ * <---  |  --->
+ *       |
+ * -------------------> x
+ * ∧     |     ∧
+ * |     |     |
+ * --->  |  <---
+ *       |
+ *       |
+ * \endverbatim
+ *
+ * 
+ * @author Laurie Nevay
+ */
+
+class BDSArrayOperatorValueReflectDipoleY: public BDSArrayOperatorValue
+{
+public:
+  BDSArrayOperatorValueReflectDipoleXY()
+  {;}
+  virtual ~BDSArrayOperatorValueReflectDipoleXY(){;}
+  
+  /// Return a name of the operator for feedback to the user in print out.
+  virtual G4String Name() const {return name;}
+  
+  virtual BDSFieldValue Apply(BDSFieldValue v,
+                              G4int xInd,
+                              G4int yInd = 0,
+                              G4int zInd = 0,
+                              G4int tInd = 0) const
+  {
+    zInd = 3; tInd = 4;// to retain default values and prevent compiler warnings
+    // only top left or bottom right quadrant need the x-component flipped
+    if ( (xInd < 0 && yInd >= 0) || (xInd >= 0 && yInd < 0) )
+      {v[0] *= -1.0;}
+    return v;
+  }
+  
+private:
+  G4String name;
+};
+
+#endif
