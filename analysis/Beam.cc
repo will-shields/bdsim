1380bc420ff9b9f55b4f160b4f185598edfe6208
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2022-03-04 19:23:59 +0000

    Don't follow ROOT's bad example of using an int as a bool.
    Former-commit-id: 88b339e7bbe154b633245d05917354c2e9ca26ed

--- analysis/Beam.cc
+++ analysis/Beam.cc
@@ -46,10 +46,10 @@ void Beam::SetBranchAddress(TTree *t,
 			    const RBDS::VectorString* branchesToTurnOn)
 {
   // turn off all branches by default.
-  t->SetBranchStatus("*", 0);
+  t->SetBranchStatus("*", false);
 
   if (allBranchesOn)
-    {t->SetBranchStatus("*", 1);}
+    {t->SetBranchStatus("*", true);}
   else if (branchesToTurnOn)
     {
       for (auto name : *branchesToTurnOn)
@@ -57,7 +57,7 @@ void Beam::SetBranchAddress(TTree *t,
 	  std::string nameStar = name + ".*"; // necessary because of the splitting
 	  if (debug)
 	    {std::cout << "Turning on branch \"" << nameStar << "\"" << std::endl;}
-	  t->SetBranchStatus(nameStar.c_str(), 1);
+	  t->SetBranchStatus(nameStar.c_str(), true);
 	}
     }
   
--- analysis/Model.cc
+++ analysis/Model.cc
@@ -52,18 +52,18 @@ void Model::SetBranchAddress(TTree* t,
 			     const RBDS::VectorString* branchesToTurnOn)
 {
   // turn off all branches by default.
-  t->SetBranchStatus("*", 0);
+  t->SetBranchStatus("*", false);
 
   if (allBranchesOn)
-    {t->SetBranchStatus("*", 1);}
+    {t->SetBranchStatus("*", true);}
   else if (branchesToTurnOn)
     {
-      for (auto name : *branchesToTurnOn)
+      for (const auto& name : *branchesToTurnOn)
 	{
 	  std::string nameStar = name + ".*"; // necessary because of the splitting
 	  if (debug)
 	    {std::cout << "Turning on branch \"" << nameStar << "\"" << std::endl;}
-	  t->SetBranchStatus(nameStar.c_str(), 1);
+	  t->SetBranchStatus(nameStar.c_str(), true);
 	}
     }
   
