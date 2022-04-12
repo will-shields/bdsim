97d06c2fccc7be03cf9679c45050ea8b10bb9002
William Shields <william.shields@rhul.ac.uk>
2022-04-12 16:25:09 +0100

    Set cylindrical mesh size according to Geant4 version.
    Former-commit-id: d5b9610b7349a02c8df0ce4e72c155d1e359e2d8

--- src/BDSScoringMeshCylinder.cc
+++ src/BDSScoringMeshCylinder.cc
@@ -24,6 +24,7 @@ along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "globals.hh"
 #include "G4RotationMatrix.hh"
+#include "G4Version.hh"
 
 BDSScoringMeshCylinder::BDSScoringMeshCylinder(const G4String&          name,
                                      const BDSScorerMeshInfo& recipe,
@@ -31,9 +32,14 @@ BDSScoringMeshCylinder::BDSScoringMeshCylinder(const G4String&          name,
     BDSScoringMeshCylinder(name, placementTransform.getTranslation(), placementTransform.getRotation())
 {
   // size of the scoring mesh
+#if G4VERSION_NUMBER < 1100
+  scorerSize[0] = recipe.ScoringMeshR(); // RMax
+  scorerSize[1] = recipe.ScoringMeshZ(); // ZSize
+#else
   scorerSize[0] = 0;  // RMin
   scorerSize[1] = recipe.ScoringMeshR(); // RMax
   scorerSize[2] = recipe.ScoringMeshZ(); // ZSize
+#endif
   SetSize(scorerSize);
 
   // divisions of the scoring mesh
