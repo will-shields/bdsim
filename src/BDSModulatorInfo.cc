d2f787ef67155a5d342c24d50dc89aed92d0b74d
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2022-08-29 11:59:02 +0100

    Modulator recipe class.
    Former-commit-id: 100c924d558195c17ab8f9f2e766fced7fff5484

---
+++ src/BDSModulatorInfo.cc
@@ -0,0 +1,33 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2022.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "BDSModulatorInfo.hh"
+#include "BDSModulatorType.hh"
+
+
+BDSModulatorInfo::BDSModulatorInfo(BDSModulatorType modulatorTypeIn,
+				   G4double         frequencyIn,
+				   G4double         phaseIn,
+				   G4double         scaleIn,
+				   G4double         amplitudeOffsetIn):
+  modulatorType(modulatorTypeIn),
+  frequency(frequencyIn),
+  phase(phaseIn),
+  scale(scaleIn),
+  amplitudeOffset(amplitudeOffsetIn)
+{;}
