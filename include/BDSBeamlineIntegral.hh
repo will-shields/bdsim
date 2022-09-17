9f67c4fe43f94af09b86b875b87f13ac5ba87963
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2022-09-17 13:56:13 +0100

    Start of beam line integral class to get all phases and times correct as well as scaling rigitidy.
    Former-commit-id: aca23c28d39023dd8a734c18aed19562af3010a7

---
+++ include/BDSBeamlineIntegral.hh
@@ -0,0 +1,60 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2022.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef BDSBEAMLINEINTEGRAL_H
+#define BDSBEAMLINEINTEGRAL_H
+
+#include "BDSParticleDefinition.hh"
+
+#include "G4Types.hh"
+
+namespace GMAD
+{
+  class Element;
+}
+
+/**
+ * @brief A class that holds the current integrated quantities along a beam line.
+ *
+ * Used to hold accumulated quantities like the current total arc length, synchronous
+ * global time and current particle rigidity / momentum along the beam line.
+ * 
+ * @author Laurie Nevay
+ */
+
+class BDSBeamlineIntegral
+{
+public:
+  BDSBeamlineIntegral() = delete;
+  explicit BDSBeamlineIntegral(const BDSParticleDefinition& incomingParticle,
+			       G4double T0In = 0,
+			       G4double integratedArcLength = 0);
+
+  ~BDSBeamlineIntegral();
+
+  /// Increment all quantities in this class by adding this element
+  /// onto the beam line.
+  void Integrate(const GMAD::Element& componentAsDefined);
+  
+  G4double synchronousTAtEnd;
+  G4double synchronousTAtMiddleOfLastElement;
+  G4double arcLength;
+  BDSParticleDefinition designParticle;
+};
+
+#endif
---
+++ src/BDSBeamlineIntegral.cc
@@ -0,0 +1,111 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2022.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "BDSBeamlineIntegral.hh"
+#include "BDSCavityFieldType.hh"
+#include "BDSComponentFactory.hh"
+#include "BDSDebug.hh"
+#include "BDSException.hh"
+#include "BDSParticleDefinition.hh"
+#include "BDSUtilities.hh"
+
+#include "G4Types.hh"
+
+#include "parser/element.h"
+#include "parser/elementtype.h"
+
+#include "CLHEP/Units/PhysicalConstants.h"
+#include "CLHEP/Units/SystemOfUnits.h"
+
+#include <cmath>
+
+
+BDSBeamlineIntegral::BDSBeamlineIntegral(const BDSParticleDefinition& incomingParticle,
+					 G4double T0In,
+					 G4double integratedArcLength):
+  synchronousTAtEnd(T0In),
+  synchronousTAtMiddleOfLastElement(T0In),
+  arcLength(integratedArcLength),
+  designParticle(incomingParticle)
+{;}
+
+BDSBeamlineIntegral::~BDSBeamlineIntegral()
+{;}
+
+void BDSBeamlineIntegral::Integrate(const GMAD::Element& componentAsDefined)
+{
+  // length
+  G4double thisComponentArcLength = componentAsDefined.l*CLHEP::m;
+  arcLength += thisComponentArcLength;
+  
+  G4double v0 = designParticle.Velocity(); // velocity at entrance of element
+
+  // calculate change in velocity and kinetic energy
+  G4double dEk = 0;
+  
+  G4double particleCharge = designParticle.Charge();
+  G4double phase = componentAsDefined.phase * CLHEP::rad;
+  G4double cosPhase = std::cos(phase);
+  G4double frequency = componentAsDefined.frequency * CLHEP::hertz;
+  
+  switch (componentAsDefined.type)
+    {
+    case GMAD::ElementType::_RF:
+      {
+        // field model
+        BDSCavityFieldType tp = BDS::DetermineCavityFieldType(componentAsDefined.cavityFieldType);
+        G4double eField = BDSComponentFactory::EFieldFromElement(&componentAsDefined, thisComponentArcLength);
+        switch (tp.underlying())
+	  {
+          case BDSCavityFieldType::constantintz:
+	    {
+	      dEk = particleCharge * eField * thisComponentArcLength * cosPhase;
+	      break;
+	    }
+          case BDSCavityFieldType::pillbox:
+	    {
+	      if (!BDS::IsFinite(frequency)) // protect against zero division
+		{throw BDSException(__METHOD_NAME__, "for a pillbox cavity field, the frequency must be non-zero");}
+	      G4double rfWavelength = CLHEP::c_light / frequency;
+	      G4double piGOverBetaLambda = CLHEP::pi * thisComponentArcLength / designParticle.Beta() * rfWavelength;
+	      G4double transitTimeFactor = std::sin(piGOverBetaLambda) / piGOverBetaLambda;
+	      dEk = particleCharge * eField * thisComponentArcLength * transitTimeFactor * cosPhase;
+	      break;
+	    }
+          default:
+	    {break;}
+	  }
+        break;
+      }
+    default:
+      {break;} // no action
+    }
+  
+  // momentum and therefore BRho
+  designParticle.ApplyChangeInKineticEnergy(dEk);
+
+  G4double v1 = designParticle.Velocity(); // velocity at the end
+  G4double vMean = (v1 + v0) / 2.0;
+  G4double dT = thisComponentArcLength / vMean;
+
+  G4double dTMiddle = thisComponentArcLength / ( (0.5*(v1-v0) + v0 ) / 2.0);
+  synchronousTAtMiddleOfLastElement = synchronousTAtEnd + dTMiddle;
+  
+  // time - now at the start of the next component / end of this component
+  synchronousTAtEnd += dT;
+}
