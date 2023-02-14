4bf891e46cde37a935f3c663633beec9fea2f265
Laurie Nevay <laurie.nevay@cern.ch>
2023-02-14 21:08:30 +0100

    Remove debug print out.
    Former-commit-id: b925b5d14985439c601631a735eb804502b73811

--- src/BDSAcceleratorComponent.cc
+++ src/BDSAcceleratorComponent.cc
@@ -68,9 +68,6 @@ BDSAcceleratorComponent::BDSAcceleratorComponent(const G4String&      nameIn,
   inputFaceNormal(inputFaceNormalIn),
   outputFaceNormal(outputFaceNormalIn)
 {
-#ifdef BDSDEBUG
-  G4cout << __METHOD_NAME__ << "(" << name << ")" << G4endl;
-#endif
   // initialise static members
   if (!emptyMaterial)
     {
@@ -117,9 +114,7 @@ void BDSAcceleratorComponent::Initialise()
 {
   if (initialised)
     {return;} // protect against duplicated initialisation and memory leaks
-#ifdef BDSDEBUG
-  G4cout << __METHOD_NAME__ << G4endl;
-#endif
+
   Build();
 
   // field construction must be done after all the geometry is constructed if the
