f95f336c0cbcf3af4fc92b3440d5622b97599ca0
Laurie Nevay <laurie.nevay@cern.ch>
2023-04-16 22:36:19 +0200

    Fixes to docker files again.
    Former-commit-id: 60c95e6783f6e885c8ef817cd91ace4ad7b18442

--- building/docker/tag-upload-testing-environments.sh
+++ building/docker/tag-upload-testing-environments.sh
@@ -1,24 +1,20 @@
 
-docker image tag centos7-base-environment jairhul/centos7-base-environment:latest
-docker image tag centos7-base-environment-dt9 jairhul/centos7-base-environment-dt9:latest
-#docker image tag centos7-geant4v10.1.3-environment jairhul/centos7-geant4v10.1.3-environment:latest
-#docker image tag centos7-geant4v10.2.3-environment jairhul/centos7-geant4v10.2.3-environment:latest
-#docker image tag centos7-geant4v10.3.3-environment jairhul/centos7-geant4v10.3.3-environment:latest
-#docker image tag centos7-geant4v10.4.3-environment jairhul/centos7-geant4v10.4.3-environment:latest
-#docker image tag centos7-geant4v10.5.1-environment jairhul/centos7-geant4v10.5.1-environment:latest
-#docker image tag centos7-geant4v10.6.3-environment jairhul/centos7-geant4v10.6.3-environment:latest
-#docker image tag centos7-geant4v10.7.4-environment jairhul/centos7-geant4v10.7.4-environment:latest
+docker image tag centos7-geant4v10.1.3-environment jairhul/centos7-geant4v10.1.3-environment:latest
+docker image tag centos7-geant4v10.2.3-environment jairhul/centos7-geant4v10.2.3-environment:latest
+docker image tag centos7-geant4v10.3.3-environment jairhul/centos7-geant4v10.3.3-environment:latest
+docker image tag centos7-geant4v10.4.3-environment jairhul/centos7-geant4v10.4.3-environment:latest
+docker image tag centos7-geant4v10.5.1-environment jairhul/centos7-geant4v10.5.1-environment:latest
+docker image tag centos7-geant4v10.6.3-environment jairhul/centos7-geant4v10.6.3-environment:latest
+docker image tag centos7-geant4v10.7.4-environment jairhul/centos7-geant4v10.7.4-environment:latest
 docker image tag centos7-geant4v11.0.4-environment jairhul/centos7-geant4v11.0.4-environment:latest
 docker image tag centos7-geant4v11.1.1-environment jairhul/centos7-geant4v11.1.1-environment:latest
 
-#docker image push jairhul/centos7-base-environment:latest
-docker image push jairhul/centos7-base-environment-dt9:latest
-#docker image push jairhul/centos7-geant4v10.1.3-environment:latest
-#docker image push jairhul/centos7-geant4v10.2.3-environment:latest
-#docker image push jairhul/centos7-geant4v10.3.3-environment:latest
-#docker image push jairhul/centos7-geant4v10.4.3-environment:latest
-#docker image push jairhul/centos7-geant4v10.5.1-environment:latest
-#docker image push jairhul/centos7-geant4v10.6.3-environment:latest
-#docker image push jairhul/centos7-geant4v10.7.4-environment:latest
+docker image push jairhul/centos7-geant4v10.1.3-environment:latest
+docker image push jairhul/centos7-geant4v10.2.3-environment:latest
+docker image push jairhul/centos7-geant4v10.3.3-environment:latest
+docker image push jairhul/centos7-geant4v10.4.3-environment:latest
+docker image push jairhul/centos7-geant4v10.5.1-environment:latest
+docker image push jairhul/centos7-geant4v10.6.3-environment:latest
+docker image push jairhul/centos7-geant4v10.7.4-environment:latest
 docker image push jairhul/centos7-geant4v11.0.4-environment:latest
 docker image push jairhul/centos7-geant4v11.1.1-environment:latest
