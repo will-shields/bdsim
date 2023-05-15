6ba5db31f5cf0e89b8ed0c5bc3b694324ab22e93
Laurie Nevay <laurie.nevay@cern.ch>
2023-05-15 11:43:11 +0200

    Update docker build scripts again. Finally for bdsim itself. Use the same name but with a tag for the version number.  Start of separate root build for modern Geant4. Work in progress.
    Former-commit-id: 170c22dbf74beb085f70cd4034deacb70fc385b4

--- building/docker/tag-upload-testing-environments.sh
+++ building/docker/tag-upload-testing-environments.sh
@@ -6,6 +6,7 @@ docker image tag centos7-geant4v10.4.3-environment jairhul/centos7-geant4v10.4.3
 docker image tag centos7-geant4v10.5.1-environment jairhul/centos7-geant4v10.5.1-environment:latest
 docker image tag centos7-geant4v10.6.3-environment jairhul/centos7-geant4v10.6.3-environment:latest
 docker image tag centos7-geant4v10.7.4-environment jairhul/centos7-geant4v10.7.4-environment:latest
+docker image tag centos7-geant4v10.7.2.3-jai-environment jairhul/centos7-geant4v10.7.2.3-jai-environment:latest
 docker image tag centos7-geant4v11.0.4-environment jairhul/centos7-geant4v11.0.4-environment:latest
 docker image tag centos7-geant4v11.1.1-environment jairhul/centos7-geant4v11.1.1-environment:latest
 
@@ -16,5 +17,6 @@ docker image push jairhul/centos7-geant4v10.4.3-environment:latest
 docker image push jairhul/centos7-geant4v10.5.1-environment:latest
 docker image push jairhul/centos7-geant4v10.6.3-environment:latest
 docker image push jairhul/centos7-geant4v10.7.4-environment:latest
+docker image push jairhul/centos7-geant4v10.7.2.3-jai-environment:latest
 docker image push jairhul/centos7-geant4v11.0.4-environment:latest
 docker image push jairhul/centos7-geant4v11.1.1-environment:latest
