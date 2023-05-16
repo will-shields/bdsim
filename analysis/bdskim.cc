697a9c7c055bf0f17ae65133c7786f25c2e5c0cb
Laurie Nevay <laurie.nevay@cern.ch>
2023-05-16 12:08:06 +0200

    In bdskim, copy the number of events from the 2nd header entry rather than inspect the tree. This also copies all the other variables too.
    Former-commit-id: d6758517a8019291507ac3bd556aaab1841b6490

--- analysis/bdskim.cc
+++ analysis/bdskim.cc
@@ -77,7 +77,9 @@ int main(int argc, char* argv[])
     {std::cerr << "Error with header" << std::endl; return 1;}
   Header* headerLocal = new Header();
   headerLocal->SetBranchAddress(headerTree);
-  headerTree->GetEntry(0);
+  Long64_t nEntriesHeader = headerTree->GetEntries();
+  headerTree->GetEntry(nEntriesHeader - 1); // get the last entry (2nd is more up to date if it exists)
+  // We also want to explicitly copy the skim variables that might only be known in the 2nd instance.
   BDSOutputROOTEventHeader* headerOut = new BDSOutputROOTEventHeader(*(headerLocal->header));
   headerOut->skimmedFile = true;
   
@@ -112,9 +114,7 @@ int main(int argc, char* argv[])
     }
   TTree* selectEvents = allEvents->CopyTree(selection.c_str());
   selectEvents->Write();
-  
-  headerOut->nOriginalEvents = allEvents->GetEntries(); // update original number of entries
-  outputHeaderTree->Fill();
+
   output->Write(nullptr,TObject::kOverwrite);
   delete output;
   delete input;
