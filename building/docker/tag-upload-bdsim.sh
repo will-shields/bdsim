33e88c1f0b2600f5bf56fef64c0c4789d382309c
Laurie Nevay <laurie.nevay@cern.ch>
2023-07-13 23:51:52 +0200

    Update docker tag.
    Former-commit-id: ae62b8e4ddd17ac5d6823bb6eb7ca3bd51583bac

--- building/docker/tag-upload-bdsim.sh
+++ building/docker/tag-upload-bdsim.sh
@@ -2,9 +2,9 @@
 docker image tag centos7-bdsim-geant4v10.7.2.3-jai-v1.7.0 jairhul/centos7-bdsim-geant4v10.7.2.3-jai:v1.7.0
 docker image tag centos7-bdsim-geant4v10.7.2.3-jai-develop jairhul/centos7-bdsim-geant4v10.7.2.3-jai-develop:latest
 
-docker image tag centos7-bdsim-geant4v10.7.2.3-jai-v1.7.0 jairhul/bdsim:v1.7.0-rc
+#docker image tag centos7-bdsim-geant4v10.7.2.3-jai-v1.7.0 jairhul/bdsim:v1.7.0-rc
 
 docker image push jairhul/centos7-bdsim-geant4v10.7.2.3-jai:v1.7.0
 docker image push jairhul/centos7-bdsim-geant4v10.7.2.3-jai-develop:latest
 
-docker image push jairhul/bdsim:v1.7.0-rc
+docker image push jairhul/bdsim:v1.7.0
